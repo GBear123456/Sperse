b86b8fdbaeb543d7be332cca0c5e68fd381ebbaf
Volodymyr Peren <vp@sperse.com>
2017-09-26 19:23:58 +0300

    [SC-183] - Implemented address add/edit/delete logic

--- src/app/admin/clients/details/addresses/addresses.component.ts
+++ src/app/admin/clients/details/addresses/addresses.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, Injector } from '@angular/core';
+import { EditAddressDialog } from '../edit-address-dialog/edit-address-dialog.component';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { MdDialog, MdDialogRef } from '@angular/material';
-import { CustomersServiceProxy, CustomerInfoDto } from '@shared/service-proxies/service-proxies';
+import { CustomersServiceProxy, ContactAddressServiceProxy, CustomerInfoDto, 
+  ContactAddressDto, UpdateContactAddressInput, CreateContactAddressInput } from '@shared/service-proxies/service-proxies';
+
+import * as _ from 'underscore';
 
 @Component({
   selector: 'addresses',
@@ -9,13 +13,62 @@ import { CustomersServiceProxy, CustomerInfoDto } from '@shared/service-proxies/
   styleUrls: ['./addresses.component.less']
 })
 export class AddressesComponent extends AppComponentBase implements OnInit {
-  data: any[] = [];
+  data: {
+    customerInfo: CustomerInfoDto
+  };  
 
   constructor(
     injector: Injector,
-    private _customerService: CustomersServiceProxy
+    public dialog: MdDialog,
+    private _customerService: CustomersServiceProxy,
+    private _addressService: ContactAddressServiceProxy
   ) { 
     super(injector);
+  }
+
+  editAddress(address) {
+    let dialogData = _.pick(address || {}, 'id', 'city', 
+      'comment', 'country', 'isActive', 'isConfirmed', 
+      'state', 'streetAddress', 'usageTypeId', 'zip');
+    dialogData.contactId = this.data
+      .customerInfo.primaryContactInfo.id;
+    this.dialog.open(EditAddressDialog, {
+      data: dialogData
+    }).afterClosed().subscribe(result => {
+      if (result) {
+        this._addressService
+          [(address ? 'update': 'create') + 'ContactAddress'](              
+            (address ? UpdateContactAddressInput: CreateContactAddressInput).fromJS(dialogData)
+          ).subscribe(result => { 
+            if (!result && address) {
+              address.city = dialogData.city;
+              address.country = dialogData.country;
+              address.isActive = dialogData.isActive;
+              address.isConfirmed = dialogData.isConfirmed;
+              address.state = dialogData.state;
+              address.streetAddress = dialogData.streetAddress;
+              address.comment = dialogData.comment;
+              address.usageTypeId = dialogData.usageTypeId;
+            } else if (result.id) {
+              dialogData.id = result.id;                  
+              this.data.customerInfo.primaryContactInfo.addresses
+                .push(ContactAddressDto.fromJS(dialogData));
+            }
+            
+          });
+      }                                                                                                                                                            
+    });
+  }
+
+  deleteAddress(address, event, index){
+    console.log(this.data.customerInfo.primaryContactInfo.id, address.id);
+    this._addressService.deleteContactAddress(
+      this.data.customerInfo.primaryContactInfo.id, address.id).subscribe(result => {
+        console.log(result);
+        this.data.customerInfo.primaryContactInfo.addresses.splice(1, 1);
+      });
+
+    event.stopPropagation();
   }
 
   ngOnInit() {
---
+++ src/app/admin/clients/details/edit-address-dialog/edit-address-dialog.component.ts
@@ -0,0 +1,84 @@
+import { AppConsts } from '@shared/AppConsts';
+import { Component, Inject, Injector } from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { MdDialog, MdDialogRef, MD_DIALOG_DATA } from '@angular/material';
+
+import { 
+  ContactAddressServiceProxy,
+  MemberServiceProxy,
+  CountryStateDto
+} from '@shared/service-proxies/service-proxies';
+
+import * as _ from 'underscore';
+
+@Component({
+  selector: 'edit-address-dialog',
+  templateUrl: 'edit-address-dialog.html',
+  styleUrls: ['edit-address-dialog.less']
+})
+export class EditAddressDialog extends AppComponentBase {
+  types: any[] = [];
+  validator: any;
+  action: string;
+  address: any;
+
+  states: CountryStateDto[];
+  options = {
+    types: ['address'],
+    componentRestrictions: {
+      country: 'US'
+    }
+  };
+
+  constructor(
+    injector: Injector,
+    @Inject(MD_DIALOG_DATA) public data: any,
+    public dialogRef: MdDialogRef<EditAddressDialog>,
+    private _contactAddressService: ContactAddressServiceProxy,
+    private _memberService: MemberServiceProxy
+  ) { 
+    super(injector, AppConsts.localization.CRMLocalizationSourceName);
+        
+    if (data.city) {
+      this.action = this.l('Edit');
+      this.address = [
+        data.streetAddress,
+        data.city,
+        data.state,
+        data.country
+      ].join(',');
+    } else
+      this.action = this.l('Create');
+
+    this.addressTypesLoad();
+    this.addressStatesLoad();
+  }
+
+  addressStatesLoad(): void {
+    this._memberService
+      .getCountryStates(this.options.componentRestrictions.country)
+      .subscribe(result => {
+        this.states = result;
+      });
+  }
+
+  addressTypesLoad() {
+    this._contactAddressService.getAddressUsageTypes().subscribe(result => {      
+      this.types = result.items;
+    });
+  }
+
+  onSave(event) {
+    if (!this.data.streetAddress)
+      this.data.streetAddress = this.address;
+    if (this.validator.validate().isValid && this.data.streetAddress) {
+      this.data.countryId = this.options.componentRestrictions.country;
+      this.data.stateId = _.findWhere(this.states, {name: this.data.state})['code'];
+      this.dialogRef.close(true);
+    }
+  }
+
+  initValidationGroup(event){
+    this.validator = event.component;
+  }
+}
