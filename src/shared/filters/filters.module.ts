a540b506f0eb99d90914d37d4894871b17094c45
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2017-11-03 18:29:16 +0200

    Fixes

--- src/shared/filters/filters.module.ts
+++ src/shared/filters/filters.module.ts
@@ -19,37 +19,40 @@ import { FilterMultiselectDropDownComponent } from './multiselect-dropdown/filte
 import { DxCheckBoxModule, DxSelectBoxModule, DxTextBoxModule, DxDateBoxModule, DxDropDownBoxModule, DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
-  imports: [
-  		ngCommon.CommonModule,
-  		FormsModule,
-  		DxCheckBoxModule,
-      DxSelectBoxModule,
-      DxTextBoxModule,
-      DxDateBoxModule,
-      CommonModule
-	],
-  declarations: [
-      FilterManagerComponent,
-      FilterStatesComponent,
-      FilterDropDownComponent,
-      FilterMultiselectDropDownComponent,
-      FilterInputsComponent,
-      FilterCBoxesComponent,
-      FilterDatesComponent,
-      FilterCalendarComponent,
-      AdDirective
-  ],
-  entryComponents: [
-      FilterStatesComponent,
-      FilterDropDownComponent,
-      FilterCBoxesComponent,
-      FilterInputsComponent,
-      FilterDatesComponent,
-      FilterCalendarComponent
-  ],
-  exports: [
-		  FilterManagerComponent
-  ]
+    imports: [
+        ngCommon.CommonModule,
+        FormsModule,
+        DxCheckBoxModule,
+        DxSelectBoxModule,
+        DxTextBoxModule,
+        DxDateBoxModule,
+        DxDropDownBoxModule,
+        DxDataGridModule,
+        CommonModule
+    ],
+    declarations: [
+        FilterManagerComponent,
+        FilterStatesComponent,
+        FilterDropDownComponent,
+        FilterMultiselectDropDownComponent,
+        FilterInputsComponent,
+        FilterCBoxesComponent,
+        FilterDatesComponent,
+        FilterCalendarComponent,
+        AdDirective
+    ],
+    entryComponents: [
+        FilterStatesComponent,
+        FilterDropDownComponent,
+        FilterMultiselectDropDownComponent,
+        FilterCBoxesComponent,
+        FilterInputsComponent,
+        FilterDatesComponent,
+        FilterCalendarComponent
+    ],
+    exports: [
+        FilterManagerComponent
+    ]
 })
 export class FiltersModule {
     static forRoot(): ModuleWithProviders {
