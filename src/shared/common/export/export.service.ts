3deaa600c58b98c84b19a9d1b3ee9400969e1993
Yulia Shkliarenko <ys@sperse.com>
2017-11-14 15:48:52 +0200

    [SC-751] [SC-752] Changed time format in excel documents

--- src/shared/common/export/export.service.ts
+++ src/shared/common/export/export.service.ts
@@ -4,16 +4,15 @@ import { AppConsts } from '@shared/AppConsts';
 import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 import { capitalize } from 'underscore.string';
+import * as moment from "moment";
 
 @Injectable()
 export class ExportService {
   constructor() {}
 
   getFileName() {
-    var date = new Date();
-    return capitalize(location.href.split('/').pop()) +
-      "_" + date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() +
-      "_" + date.getHours() + "" + date.getMinutes() + "" + date.getSeconds();
+      var date = new Date();
+      return capitalize(location.href.split('/').pop()) + "_" + moment().local().format('YYYY-MM-DD_hhmmss_a');
   }
 
   saveAsCSV(data: any, name?: string) {
