2adfa287b2a11417d8bc45d2b797f1933a871fb1
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2017-11-16 12:52:38 +0200

    reformatted some code

--- src/app/crm/clients/details/client-scores/client-scores.component.ts
+++ src/app/crm/clients/details/client-scores/client-scores.component.ts
@@ -1,34 +1,34 @@
 ﻿import { Component, OnInit } from '@angular/core';
+
 //import { FinancialService } from '../../services/financial/financial.service';
 
 @Component({
-  selector: 'client-scores',
-  templateUrl: './client-scores.component.html',
-  styleUrls: ['./client-scores.component.less']
+    selector: 'client-scores',
+    templateUrl: './client-scores.component.html',
+    styleUrls: ['./client-scores.component.less']
 })
 export class ClientScoresComponent implements OnInit {
 
-  scores = {
-    experian_score: {
-      'amount' : 803,
-      'label' : 'Experian Score'
-    },
-    equifax_score: {
-      'amount' : 809,
-      'label' : 'Equifax Score'
-    },
-    transunion_score: {
-      'amount' : 799,
-      'label'  : 'TransUnion Score'
-    }
-  };
+    scores = {
+        experian_score: {
+            'amount': 803,
+            'label': 'Experian Score'
+        },
+        equifax_score: {
+            'amount': 809,
+            'label': 'Equifax Score'
+        },
+        transunion_score: {
+            'amount': 799,
+            'label': 'TransUnion Score'
+        }
+    };
 
-  constructor(
-    //private FinancialService: FinancialService
-  ) {}
-  
-  ngOnInit() { 
-    //this.scores = this.FinancialService.getPersonScores();
-  }
-  
+    constructor(//private FinancialService: FinancialService
+    ) {}
+
+    ngOnInit() {
+        //this.scores = this.FinancialService.getPersonScores();
+    }
+
 }
--- src/shared/common/widgets/calendar/calendar.component.ts
+++ src/shared/common/widgets/calendar/calendar.component.ts
@@ -1,61 +1,58 @@
-import { AppConsts } from '@shared/AppConsts';
 import { Component, AfterViewInit, OnDestroy, Injector, Input, Output, EventEmitter } from '@angular/core';
 import { AppComponentBase } from '../../app-component-base';
 
-var JQCalendarInit = require('jquery-calendar');
+let JQCalendarInit = require('jquery-calendar');
 
 @Component({
-  selector: 'calendar',
-  templateUrl: 'calendar.component.html',
-  styleUrls: ['calendar.component.less']
+    selector: 'calendar',
+    templateUrl: 'calendar.component.html',
+    styleUrls: ['calendar.component.less']
 })
 export class CalendarComponent extends AppComponentBase implements AfterViewInit, OnDestroy {
-  UID: String = Math.random().toString(36).substring(2);
-  calendar: any;
+    UID: String = Math.random().toString(36).substring(2);
+    calendar: any;
 
-  private _values: any;
-  @Input() 
-  set values(values: any){
-    this._values = values;
-    this.setDateRageValues();
-  } 
-
-	@Output() onChange = new EventEmitter();
-
-  constructor(
-    injector: Injector
-  ) { 
-    super(injector);
-  }
-
-  private setDateRageValues() {
-    if (this.calendar) {
-      let dateRange = this.calendar.data('dateRangePicker');
-      if (this._values.from)
-        dateRange.setStart(this._values.from);
-      if (this._values.to)
-        dateRange.setEnd(this._values.to);
+    private _values: any;
+    @Input()
+    set values(values: any) {
+        this._values = values;
+        this.setDateRageValues();
     }
-  }
 
-  ngAfterViewInit() {
-    this.calendar = JQCalendarInit(".calendar#" + this.UID, true);
-    this.calendar.on('datepicker-first-date-selected', 
-      (event, obj) => {
-        this._values.from = obj.date1;
-      }
-    ).on('datepicker-change', 
-      (event, obj) => {
-        this._values.from = obj.date1;
-        this._values.to = obj.date2;
-      }
-    );
-    this.setDateRageValues();
-  }
+    @Output() onChange = new EventEmitter();
 
-  ngOnDestroy() {
-    this.calendar.off('datepicker-change');
-    this.calendar.off('datepicker-first-date-selected');
-    this.calendar.data('dateRangePicker').destroy();
-  }
+    constructor(injector: Injector) {
+        super(injector);
+    }
+
+    private setDateRageValues() {
+        if (this.calendar) {
+            let dateRange = this.calendar.data('dateRangePicker');
+            if (this._values.from)
+                dateRange.setStart(this._values.from);
+            if (this._values.to)
+                dateRange.setEnd(this._values.to);
+        }
+    }
+
+    ngAfterViewInit() {
+        this.calendar = JQCalendarInit('.calendar#' + this.UID, true);
+        this.calendar.on('datepicker-first-date-selected',
+            (event, obj) => {
+                this._values.from = obj.date1;
+            }
+        ).on('datepicker-change',
+            (event, obj) => {
+                this._values.from = obj.date1;
+                this._values.to = obj.date2;
+            }
+        );
+        this.setDateRageValues();
+    }
+
+    ngOnDestroy() {
+        this.calendar.off('datepicker-change');
+        this.calendar.off('datepicker-first-date-selected');
+        this.calendar.data('dateRangePicker').destroy();
+    }
 }
