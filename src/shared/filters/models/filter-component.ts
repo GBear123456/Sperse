ba19d3e2e89daf5031fcb89a5e35817e95d5c197
Volodymyr Peren <vp@sperse.com>
2017-11-28 16:43:19 +0200

    [SC-907, SC-863] - Date Range filter updated and fullscreen fixes

--- src/shared/filters/models/filter-component.ts
+++ src/shared/filters/models/filter-component.ts
@@ -2,6 +2,7 @@ import { FilterItemModel } from './filter-item.model';
 
 export interface FilterComponent {
     items?: { [item: string]: FilterItemModel; };
+    options?: any;
     apply: (event) => void;
     localizationSourceName: string;
 }
