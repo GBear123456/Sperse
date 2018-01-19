f2345970e9a0d08e2bc51f57c4e1f73fc341f0ad
DenysTkachuk <denys.tkachuk@sperse.com>
2018-01-19 20:18:39 +0200

    [SC-1507] used API method for getting data, added navigate route

--- src/app/cfo/start/dashboard/dashboard.component.ts
+++ src/app/cfo/start/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { CFOComponentBase } from '@app/cfo/shared/common/cfo-component-base';
 import { Component, OnInit, Injector } from '@angular/core';
 import { AppConsts } from 'shared/AppConsts';
 import { appModuleAnimation } from 'shared/animations/routerTransition';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,7 +14,10 @@ import { appModuleAnimation } from 'shared/animations/routerTransition';
 export class DashboardComponent extends CFOComponentBase implements OnInit {
     public headlineConfig;
 
-    constructor(injector: Injector) {
+    constructor(
+        injector: Injector,
+        private _router: Router
+    ) {
         super(injector);
     }
 
@@ -24,6 +28,10 @@ export class DashboardComponent extends CFOComponentBase implements OnInit {
             names: [this.l('Dashboard_Title')],
             iconSrc: 'assets/common/icons/pie-chart.svg',
             buttons: []
-        }
+        };
+    }
+
+    navigateTo() {
+        this._router.navigate(['app/cfo/' + this.instanceType.toLowerCase() + '/bank-accounts']);
     }
 }
