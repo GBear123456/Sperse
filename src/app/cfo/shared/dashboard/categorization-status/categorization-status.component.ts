bc74e3a4e145b224ed60ca142f6c05d118625a2b
DenysTkachuk <denys.tkachuk@sperse.com>
2018-01-22 11:43:23 +0200

    [SC-1509] added method for get synch data, updated value formatting

--- src/app/cfo/shared/dashboard/categorization-status/categorization-status.component.ts
+++ src/app/cfo/shared/dashboard/categorization-status/categorization-status.component.ts
@@ -1,21 +1,38 @@
 import {Component, Injector, OnInit} from '@angular/core';
 import {CFOComponentBase} from '@app/cfo/shared/common/cfo-component-base';
+import {DashboardServiceProxy, InstanceType} from '@shared/service-proxies/service-proxies';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-categorization-status',
     templateUrl: './categorization-status.component.html',
-    styleUrls: ['./categorization-status.component.less']
+    styleUrls: ['./categorization-status.component.less'],
+    providers: [DashboardServiceProxy]
 })
 export class CategorizationStatusComponent extends CFOComponentBase implements OnInit {
-    private categorySynchData = [
-        {transactionType: 'Classified transactions', count: 15125, percents: 75},
-        {transactionType: 'Unclassified transactions', count: 5125, percents: 25}
-    ];
-    constructor(injector: Injector) {
+    private categorySynchData: any;
+    constructor(
+        injector: Injector,
+        private _dashboardService: DashboardServiceProxy,
+        private _router: Router
+    ) {
         super(injector);
     }
 
     ngOnInit() {
+        this.getCategorizationStatus();
+    }
+
+    getCategorizationStatus(): void {
+        this._dashboardService.getCategorizationStatus(InstanceType[this.instanceType], this.instanceId)
+            .subscribe((result) => {
+                this.categorySynchData = result;
+                this.categorySynchData.totalCount = this.categorySynchData.classifiedTransactionCount + this.categorySynchData.unclassifiedTransactionCount;
+            });
+    }
+    
+    navigateTo() {
+        this._router.navigate(['app/cfo/' + this.instanceType.toLowerCase() + '/transactions']);
     }
 
 }
