77272f142f5f6afb94721b8348cb4c99fa027f58
Volodymyr Peren <vp@sperse.com>
2018-01-29 14:47:44 +0200

    [SC-1633] - Toolbar on Dashboard is not fixed once scrolling

--- src/app/cfo/start/dashboard/dashboard.component.ts
+++ src/app/cfo/start/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CFOComponentBase } from '@app/cfo/shared/common/cfo-component-base';
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, OnDestroy, Injector } from '@angular/core';
 import { AppConsts } from 'shared/AppConsts';
 import { appModuleAnimation } from 'shared/animations/routerTransition';
 import { Router } from '@angular/router';
@@ -11,8 +11,9 @@ import { Router } from '@angular/router';
     styleUrls: ['./dashboard.component.less'],
     animations: [appModuleAnimation()]
 })
-export class DashboardComponent extends CFOComponentBase implements OnInit {
+export class DashboardComponent extends CFOComponentBase implements OnInit, OnDestroy {
     public headlineConfig;
+    private rootComponent: any;
 
     constructor(
         injector: Injector,
@@ -20,16 +21,24 @@ export class DashboardComponent extends CFOComponentBase implements OnInit {
     ) {
         super(injector);
         this.localizationSourceName = AppConsts.localization.CFOLocalizationSourceName;
+
+        this.rootComponent = this.getRootComponent();
     }
 
     ngOnInit(): void {
         super.ngOnInit();
+      
+        this.rootComponent.overflowHidden(true);
 
         this.headlineConfig = {
             names: [this.l('Dashboard_Title')],
             iconSrc: 'assets/common/icons/pie-chart.svg',
             buttons: []
         };
+    }
+
+    ngOnDestroy(): void {
+        this.rootComponent.overflowHidden();
     }
 
     navigateTo() {
