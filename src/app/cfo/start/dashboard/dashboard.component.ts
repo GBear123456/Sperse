436eef4dfaaa31debe089893c82c28cdc4391751
Denys Tkachuk <denys.tkachuk@sperse.com>
2018-01-31 10:08:25 +0200

    [SC-1666] changed position of widgets, need change colors and update some styles according to mockups

--- src/app/cfo/start/dashboard/dashboard.component.ts
+++ src/app/cfo/start/dashboard/dashboard.component.ts
@@ -14,6 +14,11 @@ import { Router } from '@angular/router';
 export class DashboardComponent extends CFOComponentBase implements OnInit, OnDestroy {
     public headlineConfig;
     private rootComponent: any;
+    linksTo = [
+        {name: 'View_Cash_Flow_Report', route: 'cashflow'},
+        {name: 'View_Transaction_Details', route: 'transactions'},
+        {name: 'View_Financial_Statistics', route: 'stats'},
+    ];
 
     constructor(
         injector: Injector,
@@ -27,9 +32,7 @@ export class DashboardComponent extends CFOComponentBase implements OnInit, OnDe
 
     ngOnInit(): void {
         super.ngOnInit();
-      
         this.rootComponent.overflowHidden(true);
-
         this.headlineConfig = {
             names: [this.l('Dashboard_Title')],
             iconSrc: 'assets/common/icons/pie-chart.svg',
