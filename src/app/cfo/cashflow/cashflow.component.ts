cabb05eab92f7d005250bad32c7bc1804f72728d
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2018-02-12 15:00:33 +0200

    [SC-1810] - added updating of the forecasts in transactions details modal, regenerated service proxy

--- src/app/cfo/cashflow/cashflow.component.ts
+++ src/app/cfo/cashflow/cashflow.component.ts
@@ -2281,7 +2281,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
                 });
             } else {
                 if (this.clickedRowResult && this.clickedRowResult.forecastId) {
-                    forecastModel = new UpdateForecastInput({
+                    forecastModel = UpdateForecastInput.fromJS({
                         id: this.clickedRowResult.forecastId,
                         amount: newValue
                     });
@@ -2925,6 +2925,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         }
     }
 
+    /** If date is lower then the current date - return false */
     validateForecastDate(e) {
         return e.value.toLocaleDateString() >= new Date().toLocaleDateString();
     }
@@ -2939,8 +2940,47 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         }
     }
 
-    onDetailsRowUpdated(e) {
-        console.log(e);
+    onDetailsRowUpdating(e) {
+        /** Send request for updating the row */
+        let paramName = Object.keys(e.newData)[0];
+        /** add minus sign for debit values */
+        let paramValue = paramName === 'debit' ? -e.newData[paramName] : e.newData[paramName];
+        if (e.newData[paramName] !== null) {
+            let paramNameForUpdateInput = this.mapParamNameToUpdateParam(paramName);
+            let data = {
+                id: e.key.id,
+                [paramNameForUpdateInput]: paramValue
+            };
+            this._cashFlowForecastServiceProxy
+                .updateForecast(
+                    InstanceType10[this.instanceType],
+                    this.instanceId,
+                    UpdateForecastInput.fromJS(data)
+                )
+                .subscribe();
+
+            /** Remove opposite cell */
+            if (paramName === 'debit' || paramName === 'credit') {
+                let oppositeParamName = paramName === 'debit' ? 'credit' : 'debit';
+                if (e.oldData[oppositeParamName] !== null) {
+                    let rowKey = this.cashFlowGrid.instance.getRowIndexByKey(e.key);
+                    /** remove the value of opposite cell */
+                    this.cashFlowGrid.instance.cellValue(rowKey, oppositeParamName, null);
+                }
+            }
+        }
     }
+
+    mapParamNameToUpdateParam(paramName) {
+        let detailsParamsToUpdateParams = {
+            'forecastDate': 'date',
+            'credit': 'amount',
+            'debit': 'amount',
+            'description': 'transactionDescriptor'
+        };
+
+        return detailsParamsToUpdateParams[paramName];
+    }
+
 
 }
