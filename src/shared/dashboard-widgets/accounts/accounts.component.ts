eede0145eebb4924880ae4e5d8ea90214bbc4dc5
Volodymyr Peren <vp@sperse.com>
2018-03-06 19:13:22 +0200

    Added loading spiner for balance box

--- src/shared/dashboard-widgets/accounts/accounts.component.ts
+++ src/shared/dashboard-widgets/accounts/accounts.component.ts
@@ -60,11 +60,13 @@ export class AccountsComponent extends CFOComponentBase implements OnInit {
     }
 
     getDailyStats(): void {
+        this.startLoading();
         this._dashboardProxy.getDailyBalanceStats(InstanceType[this.instanceType], this.instanceId, this.bankAccountIds, this.startDate, this.endDate)
             .subscribe(result => {
                 this.dailyStatsData = result;
                 this.setDailyStatsAmount();
-            });
+            }, () => this.finishLoading(),
+               () => this.finishLoading());
     }
 
     navigateTo() {
