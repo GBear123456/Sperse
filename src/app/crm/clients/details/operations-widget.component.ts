c802e3a416cdd44de469d0ed7e1cfa9d200447aa
Roman Mudryk <roman.mudryk@sperse.com>
2018-03-15 13:24:28 +0200

    [SC-2352] Removed unnecessary buttons

--- src/app/crm/clients/details/operations-widget.component.ts
+++ src/app/crm/clients/details/operations-widget.component.ts
@@ -15,52 +15,10 @@ export class OperationsWidgetComponent implements OnInit {
 
   toolbarConfig = [
     {location: 'before', items: [
-      {name: 'back'}
-    ]},
-    {location: 'before', items: [
       {name: 'assign'}, {name: 'status'}, {name: 'delete'}
     ]},
     {location: 'center', items: [
       {name: 'folder'}, {name: 'pen'}
-    ]},
-    {location: 'center', items: [
-      {name: 'more'}
-    ]},
-    {                
-      location: 'after', 
-      areItemsDependent: true,
-      items: [
-          { 
-              name: 'box',
-              action: this.toggleDataLayout.bind(this, DataLayoutType.Box),
-              options: {
-                  checkPressed: () => {
-                      return (this.dataLayoutType == DataLayoutType.Box);
-                  },
-              }
-          },
-          { 
-              name: 'pipeline', 
-              action: this.toggleDataLayout.bind(this, DataLayoutType.Pipeline),
-              options: {
-                  checkPressed: () => {
-                      return (this.dataLayoutType == DataLayoutType.Pipeline);
-                  },
-              }
-          },
-          { 
-              name: 'grid', 
-              action: this.toggleDataLayout.bind(this, DataLayoutType.Grid),
-              options: {
-                  checkPressed: () => {
-                      return (this.dataLayoutType == DataLayoutType.Grid);
-                  },
-              } 
-          }
-      ]
-    },
-    {location: 'after', items: [
-      {name: 'prev'}, {name: 'next'}
     ]}
   ];
 
