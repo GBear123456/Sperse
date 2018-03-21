191b67f99b91901fcfc60cef00c7ebd32505f0bf
Yulia Shkliarenko <ys@sperse.com>
2018-03-21 16:45:41 +0200

    [SC-2316] Added alter routes for top bar component

--- src/app/shared/layout/panel-menu-item.ts
+++ src/app/shared/layout/panel-menu-item.ts
@@ -4,11 +4,12 @@ export class PanelMenuItem {
     featureName: string = '';
     icon: string = '';
     route: string = '';
+    alterRoutes: string[] = [];
     visible: boolean = true;
     disabled: boolean = false;
     items: PanelMenuItem[];
 
-    constructor(text: string, permissionName: string, icon: string, route: string, featureName: string, items?: PanelMenuItem[]) {
+    constructor(text: string, permissionName: string, icon: string, route: string, featureName: string, alterRoutes?: string[], items?: PanelMenuItem[]) {
         this.text = text;
         this.permissionName = permissionName;
         this.featureName = featureName;
@@ -16,11 +17,17 @@ export class PanelMenuItem {
         this.route = route;
         this.disabled = !route;
         this.visible = Boolean(text);
-
+        
         if (items === undefined) {
             this.items = [];
         } else {
             this.items = items;
         }
+
+        if (alterRoutes === undefined) {
+            this.alterRoutes = [];
+        } else {
+            this.alterRoutes = alterRoutes;
+        }        
     }
 }
