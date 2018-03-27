e646093920cce06902d04600858fc6f94b5d4fa7
Yulia Shkliarenko <ys@sperse.com>
2018-03-27 14:57:20 +0300

    [SC-2367] Added  the number of selected accounts on Cashflow toolbar

--- src/app/cfo/cashflow/operations/operations.component.ts
+++ src/app/cfo/cashflow/operations/operations.component.ts
@@ -28,7 +28,11 @@ export class OperationsComponent extends AppComponentBase implements OnDestroy {
         clearTimeout(this.initSelectedBankAccountsTimeout);
         this.initSelectedBankAccountsTimeout = setTimeout(() => {
             this.bankAccountSelector.setSelectedBankAccounts(selectedBankAccounts);
-            this.bankAccountCount = selectedBankAccounts.length;
+            if (!selectedBankAccounts.length)
+                this.bankAccountCount = '';
+            else
+                this.bankAccountCount = selectedBankAccounts.length;
+            this.initToolbarConfig();
         }, 300);
     }
     @Output() repaintCashflow: EventEmitter<any> = new EventEmitter();
@@ -42,7 +46,7 @@ export class OperationsComponent extends AppComponentBase implements OnDestroy {
     @Output() onReportPeriodChange: EventEmitter<any> = new EventEmitter();
     @Output() onSelectedBankAccountsChange: EventEmitter<any> = new EventEmitter();
 
-    bankAccountCount: any;
+    bankAccountCount = '';
     reportPeriodTooltipVisible: boolean = false;
     sliderReportPeriod = { start: 2000, end: 2028 };
     totalCount = 3;
@@ -137,6 +141,10 @@ export class OperationsComponent extends AppComponentBase implements OnDestroy {
                             id: 'bankAccountSelect',
                             text: this.l('Accounts'),
                             icon: 'assets/common/icons/accounts.svg'
+                        },
+                        attr: {
+                            'custaccesskey': 'bankAccountSelect',
+                            'accountCount': this.bankAccountCount
                         }
                     },
                 ]
