0db5ae548643f41cd01d02cd3f4a0bc07d94f3c8
DenysTkachuk <denys.tkachuk@sperse.com>
2018-03-28 14:52:25 +0300

    [SC-1997] clean from unused code, small improvements

--- src/mobile/cfo/start/setup/setup.component.ts
+++ src/mobile/cfo/start/setup/setup.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Injector, ViewChild } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppConsts } from '@shared/AppConsts';
 import { InstanceServiceProxy, InstanceType } from 'shared/service-proxies/service-proxies';
 import { CFOComponentBase } from '../../shared/common/cfo-component-base';
 
