f6b6bb3557ea6b2021f0202d349a7db40e3ca868
Volodymyr Peren <vp@sperse.com>
2018-04-07 14:55:24 +0300

    [SC-2743] - Add 'Questions or feedback' widget to CFO Welcome page

--- src/app/cfo/start/start.component.ts
+++ src/app/cfo/start/start.component.ts
@@ -1,8 +1,10 @@
 import { CFOComponentBase } from '@shared/cfo/cfo-component-base';
-import { Injector, Component } from '@angular/core';
+import { Injector, Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { SetupComponent } from './setup/setup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { appModuleAnimation } from '@shared/animations/routerTransition';
+
+import { ngxZendeskWebwidgetService } from 'ngx-zendesk-webwidget';
 
 @Component({
     selector: 'start',
@@ -10,8 +12,18 @@ import { appModuleAnimation } from '@shared/animations/routerTransition';
     styleUrls: ['./start.component.less'],
     animations: [appModuleAnimation()]
 })
-export class StartComponent extends CFOComponentBase {
-    constructor(injector: Injector) {
+export class StartComponent extends CFOComponentBase implements AfterViewInit, OnDestroy {
+    constructor(injector: Injector,
+        private _ngxZendeskWebwidgetService: ngxZendeskWebwidgetService
+    ) {
         super(injector);
+    }
+
+    ngAfterViewInit(): void {
+        CFOComponentBase.zendeskWebwidgetShow(this._ngxZendeskWebwidgetService);
+    }
+
+    ngOnDestroy(): void {
+        CFOComponentBase.zendeskWebwidgetHide(this._ngxZendeskWebwidgetService);
     }
 }
