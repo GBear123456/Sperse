00cb4f67e244e7ee3cfa7064520d90174de93022
Roman Mudryk <roman.mudryk@sperse.com>
2018-04-16 20:00:23 +0300

    [SC-2858] Implemented API method to get list of note types

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -10554,6 +10554,56 @@ export class NotesServiceProxy {
         }
         return Observable.of<void>(<any>null);
     }
+
+    /**
+     * @return Success
+     */
+    getNoteTypes(): Observable<NoteTypeInfoDto[]> {
+        let url_ = this.baseUrl + "/api/services/CRM/Notes/GetNoteTypes";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            method: "get",
+            headers: new Headers({
+                "Content-Type": "application/json", 
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request(url_, options_).flatMap((response_ : any) => {
+            return this.processGetNoteTypes(response_);
+        }).catch((response_: any) => {
+            if (response_ instanceof Response) {
+                try {
+                    return this.processGetNoteTypes(response_);
+                } catch (e) {
+                    return <Observable<NoteTypeInfoDto[]>><any>Observable.throw(e);
+                }
+            } else
+                return <Observable<NoteTypeInfoDto[]>><any>Observable.throw(response_);
+        });
+    }
+
+    protected processGetNoteTypes(response: Response): Observable<NoteTypeInfoDto[]> {
+        const status = response.status; 
+
+        let _headers: any = response.headers ? response.headers.toJSON() : {};
+        if (status === 200) {
+            const _responseText = response.text();
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            if (resultData200 && resultData200.constructor === Array) {
+                result200 = [];
+                for (let item of resultData200)
+                    result200.push(NoteTypeInfoDto.fromJS(item));
+            }
+            return Observable.of(result200);
+        } else if (status !== 200 && status !== 204) {
+            const _responseText = response.text();
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+        }
+        return Observable.of<NoteTypeInfoDto[]>(<any>null);
+    }
 }
 
 @Injectable()
@@ -33848,6 +33898,45 @@ export interface IUpdateNoteInput {
     followUpDateTime: moment.Moment;
     dateTime: moment.Moment;
     addedByUserId: number;
+}
+
+export class NoteTypeInfoDto implements INoteTypeInfoDto {
+    id: string;
+    name: string;
+
+    constructor(data?: INoteTypeInfoDto) {
+        if (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property))
+                    (<any>this)[property] = (<any>data)[property];
+            }
+        }
+    }
+
+    init(data?: any) {
+        if (data) {
+            this.id = data["id"];
+            this.name = data["name"];
+        }
+    }
+
+    static fromJS(data: any): NoteTypeInfoDto {
+        let result = new NoteTypeInfoDto();
+        result.init(data);
+        return result;
+    }
+
+    toJSON(data?: any) {
+        data = typeof data === 'object' ? data : {};
+        data["id"] = this.id;
+        data["name"] = this.name;
+        return data; 
+    }
+}
+
+export interface INoteTypeInfoDto {
+    id: string;
+    name: string;
 }
 
 export class GetNotificationsOutput implements IGetNotificationsOutput {
