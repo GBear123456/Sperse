0998e9c830a8a60fb6b7dcdf8c5cec0b48b9cf58
Oksana Khoma <oksana.khoma@sperse.com>
2018-04-25 17:30:49 +0300

    [SC-2988] Used Quovo Connect for CFO Get Started workflow

--- src/app/cfo/shared/common/quovo/QuovoService.ts
+++ src/app/cfo/shared/common/quovo/QuovoService.ts
@@ -6,6 +6,8 @@ declare const Quovo: any;
 export class QuovoHandler {
     handler: any;
     isLoaded = false;
+    isOpened = false;
+    addedConnections = [];
 }
 
 @Injectable()
@@ -14,19 +16,26 @@ export class QuovoService {
         private _syncServiceProxy: SyncServiceProxy
     ) {}
 
-    getQuovoHandler(instanceType: string, instanceId: number) {
+    getQuovoHandler(instanceType: string, instanceId: number, onClose?: Function) {
         let quovoHandler = new QuovoHandler();
         jQuery.getScript('https://app.quovo.com/ui.js', () => {
             this._syncServiceProxy.createProviderUIToken(InstanceType[instanceType], instanceId)
                 .subscribe((data) => {
-                    quovoHandler.handler = this.createQuovoHandler(data.token, () => quovoHandler.isLoaded = true);
+                    quovoHandler.handler = this.createQuovoHandler(data.token,
+                        () => quovoHandler.isLoaded = true,
+                        () => quovoHandler.isOpened = true,
+                        () => {
+                            quovoHandler.isOpened = false;
+                            if (onClose) onClose();
+                        },
+                        (id) => quovoHandler.addedConnections.push(id));
                 });
         });
 
         return quovoHandler;
     }
 
-    private createQuovoHandler(token, onLoad) {
+    private createQuovoHandler(token, onLoad, onOpen, onClose, onAdd) {
         return Quovo.create({
             token: token,
             search: {
@@ -34,10 +43,12 @@ export class QuovoService {
             },
             onLoad: () => { onLoad(); console.log('loaded'); },
             onAdd: function (err, event) {
+                onAdd();
                 if (!err) {
                     console.log('Connection', event.connection.id, 'added!');
                 }
-            }
+            },
+            onClose: () => onClose()
         });
     }
 
