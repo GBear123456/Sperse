5e77d0283440f5fb2ff4a10b43fd3024262efd78
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2018-05-02 18:10:48 +0300

    Revert "[SC-3141] - changed calling of few daily requests to the new dailyPeriods param for getStats method, changed creating of stub periods according to that"
    This reverts commit f398462
    Created separate branch SC-3141

--- src/app/cfo/cashflow/cashflow.component.ts
+++ src/app/cfo/cashflow/cashflow.component.ts
@@ -25,8 +25,7 @@ import {
     TransactionStatsDtoAdjustmentType,
     DiscardDiscrepanciesInput,
     CreateForecastModelInput,
-    CashFlowStatsDetailDto,
-    DailyPeriod
+    CashFlowStatsDetailDto
 } from '@shared/service-proxies/service-proxies';
 import { UserPreferencesService } from './preferences-dialog/preferences.service';
 import { RuleDialogComponent } from '../rules/rule-edit-dialog/rule-edit-dialog.component';
@@ -908,6 +907,8 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
     initFiltering() {
         this._filtersService.apply(() => {
             for (let filter of this.filters) {
+                /** Reset cached days months to apply the date filter again */
+                /** @todo reset only for the month for which the filter changed */
                 if (filter.caption.toLowerCase() === 'date') {
                     this.monthsDaysLoadedPathes = [];
                     if (filter.items.from.value)
@@ -1277,9 +1278,9 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
     loadGridDataSource() {
         this.startLoading();
         this.requestFilter.forecastModelId = this.selectedForecastModel.id;
-        this.requestFilter.dailyPeriods = this.getDailyPeriods();
 
-        /** Clear cache of tree paths */
+        /** Clear cache of loaded days */
+        this.monthsDaysLoadedPathes = [];
         this.treePathes = {};
 
         /** Clear cache for rows sparklines */
@@ -1287,13 +1288,38 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
             sparkLine.dispose();
         });
         this.cachedRowsSparkLines.clear();
-        this._cashflowServiceProxy.getStats(InstanceType[this.instanceType], this.instanceId, this.requestFilter)
-            .pluck('transactionStats')
-            .subscribe(transactions => {
+
+        /** Monthly cashflow data observer */
+        let monthlyStatsObservers = [this._cashflowServiceProxy.getStats(InstanceType[this.instanceType], this.instanceId, this.requestFilter)];
+
+        /** If we have some expanded months in state - we should also load daily data */
+        let dailyStatsObservers = [];
+        let dailyStatsFilters = [];
+        let state = this.pivotGrid ? this.pivotGrid.instance.getDataSource().state() : this.stateLoad();
+        let monthIndex = this.getAreaIndexByCaption('month');
+        if (state && state.columnExpandedPaths) {
+            state.columnExpandedPaths.forEach(columnPath => {
+                if (columnPath.length === monthIndex + 1) {
+                    this.monthsDaysLoadedPathes.push(columnPath);
+                    let requestFilter = this.getRequestFilterFromPath(columnPath);
+                    if (requestFilter) {
+                        dailyStatsFilters.push(requestFilter);
+                        dailyStatsObservers.push(this._cashflowServiceProxy.getStats(InstanceType[this.instanceType], this.instanceId, requestFilter));
+                    }
+                }
+            });
+        }
+
+        let getStatsObservervables = monthlyStatsObservers.concat(dailyStatsObservers);
+        Observable.forkJoin(...getStatsObservervables)
+            .subscribe((result: any)  => {
                 this.startDataLoading = true;
-                this.handleMonthlyCashflowData(transactions);
+                this.handleMonthlyCashflowData(result[0].transactionStats);
                 /** override cashflow data push method to add totals and net change automatically after adding of cashflow */
                 this.overrideCashflowDataPushMethod();
+                for (let i = 1; i < result.length; i++) {
+                    this.handleDailyCashflowData(result[i].transactionStats, dailyStatsFilters[i - 1].startDate, dailyStatsFilters[i - 1].endDate);
+                }
             },
             e => {},
             () => {
@@ -1691,22 +1717,11 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         let stubsInterval = this.getStubsInterval(minDate, maxDate, existingPeriods, periodFormat);
         this.yearsAmount = stubsInterval.endDate.diff(stubsInterval.startDate, 'years') + 1;
 
+
         /** cycle from started date to ended date */
         /** added fake data for each date that is not already exists in cashflow data */
         let accountId = cashflowData[0] ? cashflowData[0].accountId : this.bankAccounts[0].id;
         let stubCashflowData = this.createStubsForPeriod(stubsInterval.startDate, stubsInterval.endDate, 'month', accountId, existingPeriods);
-
-        if (this.requestFilter.dailyPeriods.length) {
-            this.requestFilter.dailyPeriods.forEach(dailyPeriod => {
-                let filterStart = this.requestFilter.startDate ? moment(this.requestFilter.startDate) : null;
-                let filterEnd = this.requestFilter.endDate ? moment(this.requestFilter.endDate) : null;
-                let start = filterStart && dailyPeriod.start.isBefore(filterStart) ? filterStart : dailyPeriod.start.utc();
-                let end = filterEnd && dailyPeriod.end.isAfter(filterEnd) ? filterEnd : dailyPeriod.end.utc();
-                let dailyStubs = this.createStubsForPeriod(start, end, 'day', accountId, []);
-                stubCashflowData = stubCashflowData.concat(dailyStubs);
-            });
-        }
-
         return stubCashflowData;
     }
 
@@ -1751,7 +1766,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         return { startDate: moment.utc(minDate), endDate: moment.utc(maxDate) };
     }
 
-    createStubsForPeriod(startDate, endDate, period, bankAccountId, existingPeriods = []): TransactionStatsDtoExtended[] {
+    createStubsForPeriod(startDate, endDate, period, bankAccountId, existingPeriods = []) {
         let stubs = [];
         let startDateCopy = moment(startDate),
             endDateCopy = moment(endDate);
@@ -2240,9 +2255,9 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
 
     getYearHistoricalSelectorWithCurrent(): any {
         return data => {
-            let currentYear = moment().year();
-            let itemYear = data.initialDate.year();
-            let result = Periods.Historical;
+            let currentYear = moment().year(),
+                itemYear = data.initialDate.year(),
+                result = Periods.Historical;
             if (currentYear < itemYear) {
                 result = Periods.Forecast;
             } else if (currentYear === itemYear) {
@@ -3474,23 +3489,6 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         requestFilter.endDate = this.requestFilter.endDate && moment(this.requestFilter.endDate).utc().isBefore(datePeriod.endDate ) ? moment(this.requestFilter.endDate).utc() : datePeriod.endDate;
         requestFilter.calculateStartingBalance = false;
         return requestFilter;
-    }
-
-    getDailyPeriods() {
-        let dailyPeriods = [];
-        let state = this.pivotGrid ? this.pivotGrid.instance.getDataSource().state() : this.stateLoad();
-        if (state && state.columnExpandedPaths) {
-            let monthIndex = this.getAreaIndexByCaption('month');
-            state.columnExpandedPaths.forEach(columnPath => {
-                if (columnPath.length === monthIndex + 1) {
-                    this.monthsDaysLoadedPathes.push(columnPath);
-                    let datePeriod = this.formattingDate(columnPath);
-                    let dailyPeriod: DailyPeriod = DailyPeriod.fromJS({ start: datePeriod.startDate, end: datePeriod.endDate });
-                    dailyPeriods.push(dailyPeriod);
-                }
-            });
-        }
-        return dailyPeriods;
     }
 
     isProjectedCellOfCurrentMonth(cellObj) {
