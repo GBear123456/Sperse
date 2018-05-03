40d6afb8f7b9ad54f1b57e5e09778f7db756470c
Yulia Shkliarenko <ys@sperse.com>
2018-05-03 14:16:24 +0300

    [SC-3180] initiated sync progress after adding/updating account

--- src/app/cfo/shared/common/synch-progress/synch-progress.service.ts
+++ src/app/cfo/shared/common/synch-progress/synch-progress.service.ts
@@ -7,9 +7,16 @@ export class SynchProgressService {
     private syncCompleted = new Subject<string>();
     syncCompleted$ = this.syncCompleted.asObservable();
 
+    private needRefreshSync = new Subject<string>();
+    needRefreshSync$ = this.needRefreshSync.asObservable();
+
     constructor() { }
 
     syncProgressCompleted() {
         this.syncCompleted.next();
+    }
+
+    refreshSyncComponent() {
+        this.needRefreshSync.next();
     }
 }
