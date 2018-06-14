535f66aa219be52d7d6af6e584309452f7b45c71
Volodymyr Peren <vp@sperse.com>
2018-06-14 12:52:49 +0300

    [SC-3459] - Error ActivatedRouteSnapshot when switching modules (after session expires)

--- src/root-routing.module.ts
+++ src/root-routing.module.ts
@@ -12,8 +12,15 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
     private getKey(route: ActivatedRouteSnapshot) {
         return route && route.routeConfig.path;
     }
+
+    private checkSameRoute(route, handle) {
+        return handle && (handle.route.value.snapshot.routeConfig == route.routeConfig);
+    }
     
     shouldDetach(route: ActivatedRouteSnapshot): boolean {
+         if (!route.routeConfig || route.routeConfig.loadChildren)
+            return false;
+
         return route.routeConfig.data && route.routeConfig.data.reuse;
     }
 
@@ -24,17 +31,20 @@ export class CustomReuseStrategy implements RouteReuseStrategy {
     }
 
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
-        return Boolean(this.handlers[this.getKey(route)]);
+        return this.checkSameRoute(route, <any>this.handlers[this.getKey(route)]);
     }
 
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+         if (!route.routeConfig || route.routeConfig.loadChildren)
+            return false;
+
         let handle = <any>this.handlers[this.getKey(route)];
         if (handle && handle.componentRef.instance.activate) {
             clearTimeout(this.activateTimeout);
             this.activateTimeout = setTimeout(() => 
                 handle.componentRef.instance.activate());
         }
-        return handle;
+        return (this.checkSameRoute(route, handle) ? handle: null);
     }
 
     shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
