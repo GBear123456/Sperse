f7ff17822842842bdad0fa901ba631984046b930
Volodymyr Peren <vp@sperse.com>
2018-07-18 12:34:53 +0300

    [SC-4233] - Default user is not marked in the toolbar on import Lead wizard

--- src/app/crm/import-leads/import-leads.component.ts
+++ src/app/crm/import-leads/import-leads.component.ts
@@ -569,6 +569,7 @@ export class ImportLeadsComponent extends AppComponentBase implements AfterViewI
     clearToolbarSelectedItems() {
         this.stagesComponent.selectedItems = [];
         this.starsListComponent.selectedItemKey = undefined;
+        this.userAssignmentComponent.selectedItemKey = this.userId;
         this.userAssignmentComponent.selectedKeys = [this.userId];
         this.listsComponent.reset();
         this.tagsComponent.reset();
