a21d847d421578ea6baf836e30158a3653c84f10
Volodymyr Peren <vp@sperse.com>
2018-07-23 18:37:35 +0300

    Fixed build issue

--- src/app/shared/common/import-wizard/import-progress-bar/import-progress-bar.component.ts
+++ src/app/shared/common/import-wizard/import-progress-bar/import-progress-bar.component.ts
@@ -29,7 +29,7 @@ export class ImportProgressBarComponent extends AppComponentBase implements OnIn
          //!! check import progress
     }
 
-    showStatus = () => () {
+    showStatus = () => {
         return this.progress + '% ' + this.l('Import Progress');
     }
 
