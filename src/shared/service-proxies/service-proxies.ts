ada8e2fda1946887510aaddab6509744e0b0cafb
Roman Mudryk <roman.mudryk@sperse.com>
2018-08-20 19:58:22 +0300

    [SC-4675] Implemented API method to delete Partner Type

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -16774,6 +16774,57 @@ export class PartnerTypeServiceProxy {
         }
         return _observableOf<void>(<any>null);
     }
+
+    /**
+     * @id (optional) 
+     * @return Success
+     */
+    delete(id: number | null | undefined): Observable<void> {
+        let url_ = this.baseUrl + "/api/services/CRM/PartnerType/Delete?";
+        if (id !== undefined)
+            url_ += "id=" + encodeURIComponent("" + id) + "&"; 
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+            })
+        };
+
+        return this.http.request("delete", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processDelete(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processDelete(<any>response_);
+                } catch (e) {
+                    return <Observable<void>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<void>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processDelete(response: HttpResponseBase): Observable<void> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return _observableOf<void>(<any>null);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<void>(<any>null);
+    }
 }
 
 @Injectable()
@@ -46679,7 +46730,7 @@ export interface IPackageDto {
 }
 
 export class PartnerInfoDto implements IPartnerInfoDto {
-    typeId!: string | undefined;
+    typeId!: number | undefined;
 
     constructor(data?: IPartnerInfoDto) {
         if (data) {
@@ -46711,7 +46762,7 @@ export class PartnerInfoDto implements IPartnerInfoDto {
 }
 
 export interface IPartnerInfoDto {
-    typeId: string | undefined;
+    typeId: number | undefined;
 }
 
 export class UpdatePartnerTypeInput implements IUpdatePartnerTypeInput {
