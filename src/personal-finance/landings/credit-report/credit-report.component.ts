e805e99956ad87217e2a77d16a67ef3e9c5fa9c0
Oksana Khoma <oksana.khoma@sperse.com>
2018-08-27 16:46:32 +0300

    [SC-4753] Renamed credit-reports folder to personal-finance

---
+++ src/personal-finance/landings/credit-report/credit-report.component.ts
@@ -0,0 +1,24 @@
+import {Component, Injector, OnInit} from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: 'credit-report.component.html',
+    styleUrls: ['credit-report.component.less']
+})
+
+export class CreditReportComponent extends AppComponentBase implements OnInit {
+    constructor(
+        injector: Injector,
+        private _router: Router
+    ) {
+        super(injector);
+
+        if (abp.session.userId) {
+            this._router.navigate(['personal-finance/member-area']);
+        }
+    }
+
+    ngOnInit() {    }
+}
