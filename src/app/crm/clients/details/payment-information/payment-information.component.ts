296e2ce14f22c812562c996db50a06a345da3557
Yulia Shkliarenko <ys@sperse.com>
2018-08-29 17:22:08 +0300

    [SC-4348] Extended Subscriptions section on client details to support tenancies

--- src/app/crm/clients/details/payment-information/payment-information.component.ts
+++ src/app/crm/clients/details/payment-information/payment-information.component.ts
@@ -1,8 +1,14 @@
-import { Component, OnInit, Injector } from '@angular/core';
+import { Component, OnInit, Injector, ViewChild } from '@angular/core';
 
 import { AppConsts } from '@shared/AppConsts';
 import { AppComponentBase } from '@shared/common/app-component-base';
-import { ContactGroupServiceProxy, OrderSubscriptionServiceProxy, OrderSubscriptionDto, ContactGroupInfoDto } from 'shared/service-proxies/service-proxies';
+import {
+    ContactGroupServiceProxy, OrderSubscriptionServiceProxy, OrderSubscriptionDto, ContactGroupInfoDto,
+    NameValueDto,
+    FindUsersInput,
+    CommonLookupServiceProxy} from 'shared/service-proxies/service-proxies';
+import { CommonLookupModalComponent } from '@app/shared/common/lookup/common-lookup-modal.component';
+import { ImpersonationService } from '@app/admin/users/impersonation.service';
 
 @Component({
     selector: 'payment-information',
@@ -11,6 +17,8 @@ import { ContactGroupServiceProxy, OrderSubscriptionServiceProxy, OrderSubscript
     providers: [OrderSubscriptionServiceProxy]
 })
 export class PaymentInformationComponent extends AppComponentBase implements OnInit {
+    @ViewChild('impersonateUserLookupModal') impersonateUserLookupModal: CommonLookupModalComponent;
+
     public data: {
         customerInfo: ContactGroupInfoDto
     };
@@ -20,6 +28,8 @@ export class PaymentInformationComponent extends AppComponentBase implements OnI
         injector: Injector,
         private _customerService: ContactGroupServiceProxy,
         private _orderSubscriptionService: OrderSubscriptionServiceProxy,
+        private _commonLookupService: CommonLookupServiceProxy,
+        private _impersonationService: ImpersonationService
     ) {
         super(injector, AppConsts.localization.CRMLocalizationSourceName);
     }
@@ -27,6 +37,18 @@ export class PaymentInformationComponent extends AppComponentBase implements OnI
     ngOnInit() {
         this.data = this._customerService['data'];
         this.refreshData();
+
+        this.impersonateUserLookupModal.configure({
+            title: this.l('SelectAUser'),
+            dataSource: (skipCount: number, maxResultCount: number, filter: string, tenantId?: number) => {
+                let input = new FindUsersInput();
+                input.filter = filter;
+                input.maxResultCount = maxResultCount;
+                input.skipCount = skipCount;
+                input.tenantId = tenantId;
+                return this._commonLookupService.findUsers(input);
+            }
+        });
     }
 
     refreshData() {
@@ -48,5 +70,20 @@ export class PaymentInformationComponent extends AppComponentBase implements OnI
                     });
             }
         });
+    }
+
+    showUserImpersonateLookUpModal(record: any): void {
+        console.log(record);
+        console.log(record.tenantId);
+        this.impersonateUserLookupModal.tenantId = record.tenantId;
+        this.impersonateUserLookupModal.show();
+    }
+
+    impersonateUser(item: NameValueDto): void {
+        this._impersonationService
+            .impersonate(
+                parseInt(item.value),
+                this.impersonateUserLookupModal.tenantId
+            );
     }
 }
