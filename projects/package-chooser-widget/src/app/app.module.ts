361d74b0ff8a3d633d89db616c40dffa819cc4e5
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2018-10-01 14:20:52 +0300

    [SC-4970] - added missing files

---
+++ projects/package-chooser-widget/src/app/app.module.ts
@@ -0,0 +1,69 @@
+/** Application imports */
+import { NgModule, Injector, APP_INITIALIZER } from '@angular/core';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClient } from '@angular/common/http';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+/** Third party imports */
+import { GestureConfig } from '@angular/material';
+import { MatSlider, MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggle, MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+/** Application imports */
+import { PackageChooserComponent } from '../../../../src/app/shared/common/payment-wizard/package-chooser/package-chooser.component';
+import { PackageCardComponent } from '@app/shared/common/payment-wizard/package-chooser/package-card/package-card.component';
+import { AbpModule } from '@abp/abp.module';
+import { AppHttpInterceptor } from '@shared/http/appHttpInterceptor';
+import { API_BASE_URL, PackageServiceProxy } from '@shared/service-proxies/service-proxies';
+import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
+import { AppHttpConfiguration } from '@shared/http/appHttpConfiguration';
+import { PackageChooserWidgetComponent } from './package-chooser-widget-component';
+import { AppConsts } from '@shared/AppConsts';
+import { WidgetsService } from '../../../widgets.service';
+
+export function getRemoteUrl() {
+    return AppConsts.remoteServiceBaseUrl;
+}
+
+export function initialize(widgetsService: WidgetsService, injector: Injector, http: HttpClient) {
+    return widgetsService.initialize(injector, http);
+}
+
+@NgModule({
+    declarations: [
+        PackageChooserComponent,
+        PackageChooserWidgetComponent,
+        PackageCardComponent
+    ],
+    imports: [
+        AbpModule,
+        BrowserModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        HttpClientModule
+    ],
+    providers: [
+        AppLocalizationService,
+        AppHttpConfiguration,
+        PackageServiceProxy,
+        WidgetsService,
+        { provide: HTTP_INTERCEPTORS, useClass: AppHttpInterceptor, multi: true },
+        { provide: API_BASE_URL, useFactory: getRemoteUrl },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: initialize,
+            deps: [ WidgetsService, Injector, HttpClient ],
+            multi: true
+        },
+        {
+            provide: HAMMER_GESTURE_CONFIG,
+            useClass: GestureConfig
+        }
+    ],
+    entryComponents: [ MatSlider, MatSlideToggle, PackageChooserWidgetComponent, PackageCardComponent ],
+    bootstrap: []
+})
+export class AppModule {
+    constructor() {}
+
+    ngDoBootstrap() {}
+}
