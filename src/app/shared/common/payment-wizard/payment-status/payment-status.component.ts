d2ccbec71a541dbe85fa6525062aa57801c6108f
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2018-10-08 18:08:15 +0300

    [SC-5162] - added method of separation of words

--- src/app/shared/common/payment-wizard/payment-status/payment-status.component.ts
+++ src/app/shared/common/payment-wizard/payment-status/payment-status.component.ts
@@ -18,4 +18,17 @@ export class PaymentStatusComponent extends AppComponentBase implements OnInit {
 
     ngOnInit() {}
 
+    separateToFewLines(string: string) {
+        const commaIndex = string.indexOf(',');
+        let result;
+        /** If comma exists in text - separate by comma */
+        if (commaIndex !== -1) {
+            result = string.slice(0, commaIndex + 1) + '\n' + string.slice(commaIndex + 1, string.length);
+        /** Else - separate by first two words and else */
+        } else {
+            const words = string.split(' ');
+            result = words.slice(0, 2).join(' ') + '\n' + words.slice(2, words.length).join(' ');
+        }
+        return result;
+    }
 }
