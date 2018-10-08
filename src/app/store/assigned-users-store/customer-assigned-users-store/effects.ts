e3afac9fa59e863804384f7ea21e0b04df673f98
Yulia Shkliarenko <ys@sperse.com>
2018-10-08 16:02:59 +0300

    [SC-4998] Applied review comments

--- src/app/store/assigned-users-store/customer-assigned-users-store/effects.ts
+++ src/app/store/assigned-users-store/customer-assigned-users-store/effects.ts
@@ -9,13 +9,13 @@ import { catchError, exhaustMap, map, startWith, withLatestFrom } from 'rxjs/ope
 
 /** Application imports */
 import * as assignedUsersActions from './actions';
-import { ContactGroupServiceProxy, UserInfoDto } from 'shared/service-proxies/service-proxies';
+import { CustomerServiceProxy, UserInfoDto } from 'shared/service-proxies/service-proxies';
 import { State } from './state';
 import { getLoaded } from './selectors';
 
 @Injectable()
 export class CustomerAssignedUsersStoreEffects {
-    constructor(private _contactGroupService: ContactGroupServiceProxy,
+    constructor(private _customerService: CustomerServiceProxy,
                 private actions$: Actions,
                 private store$: Store<State>) {}
 
@@ -29,7 +29,7 @@ export class CustomerAssignedUsersStoreEffects {
                 return empty();
             }
 
-            return this._contactGroupService.getAllowedAssignableUsers(true, undefined, undefined)
+            return this._customerService.getAllowedAssignableUsers(true, undefined, undefined)
                 .pipe(
                     map((users: UserInfoDto[]) => {
                         return new assignedUsersActions.LoadSuccessAction(users);
