35934a82e12d03796d58d68fcc54ea376aceda13
Roman Bilyak <roman.bilyak@sperse.com>
2018-10-10 13:46:08 +0300

    [SC-4940] added script to clone deployed files into current container

--- deploy/create-container.ps1
+++ deploy/create-container.ps1
@@ -3,6 +3,9 @@ param
 (
     [string] [Parameter(Mandatory = $true)]
     $StorageName,
+    
+    [string] [Parameter(Mandatory = $true)]
+    $StorageKey,
 
     [string] [Parameter(Mandatory = $true)]
     $ContainerName,
@@ -13,20 +16,13 @@ param
 
 try 
 {
-    Write-Verbose "Getting Resource group name"
-    $ResourceGroupName = Get-AzureRmStorageAccount | Where-Object {$_.StorageAccountName -eq $StorageName} | Select-Object -ExpandProperty ResourceGroupName
-    Write-Verbose "Resource group name: $ResourceGroupName"
-    
-    Write-Verbose "Getting Storage key for storage $StorageName and resource group $ResourceGroupName"
-    $StorageKey = (Get-AzureRmStorageAccountKey -AccountName $StorageName -ResourceGroupName $ResourceGroupName ).Value[0]
-    Write-Verbose "Storage key: $StorageKey"
-    
-    Write-Verbose "Getting Storage context..."
+    Write-Host "Getting context for '$StorageName' storage ..."
     $StorageContext = New-AzureStorageContext -StorageAccountName $StorageName -StorageAccountKey $StorageKey
     
-    Write-Host "Creating container $ContainerName in storage $StorageName"
+    Write-Host "Creating container '$ContainerName' in storage '$StorageName' ..."
     New-AzureStorageContainer -Name $ContainerName -Context $StorageContext -Permission $ContainerPublicAccessLevel -ErrorAction Stop
-    Write-Host "Succesfully created container $ContainerName in storage account $StorageName"
+    
+    Write-Host "Done"
 }
 catch 
 {
