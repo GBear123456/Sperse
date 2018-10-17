358583b41e07db4b2c91ffb06a7f611c4164636f
Roman Bilyak <roman.bilyak@sperse.com>
2018-10-17 13:36:06 +0300

    fixed path to blob storage for beta and prod environments

--- src/environments/environment.beta.ts
+++ src/environments/environment.beta.ts
@@ -4,5 +4,5 @@ export const environment = {
     hmr: false,
     appConfig: 'appconfig.beta.json',
     appBaseUrl: 'https://beta.sperse.com',
-    appBaseHref: 'https://testui.blob.core.windows.net/current/'
+    appBaseHref: 'https://sperseui.blob.core.windows.net/current/'
 };
