a72f6e8a9f2167266302b47accb5326bfa1349e6
Yulia Shkliarenko <ys@sperse.com>
2018-11-14 15:34:24 +0200

    [SC-5712] Added Quovo widgets on Accounts section

--- src/personal-finance/member-area/member-area.module.ts
+++ src/personal-finance/member-area/member-area.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
+import { StickyModule } from 'ng2-sticky-kit/ng2-sticky-kit';
+
 import { BankAccountsCommonModule } from '@shared/cfo/bank-accounts/bank-accounts-common.module'
 import { MemberAreaRoutingModule } from './member-area-routing.module';
 import { CreditReportModule } from './credit-report/credit-report.module';
@@ -26,7 +28,8 @@ import { InstanceServiceProxy } from '@shared/service-proxies/service-proxies';
         MemberAreaRoutingModule,
         CreditSimulatorModule,
         CreditResourcesModule,
-        BankAccountsCommonModule.forRoot()
+        BankAccountsCommonModule.forRoot(),
+        StickyModule
     ],
     declarations: [
         MemberAreaComponent,
