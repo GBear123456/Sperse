0455bf20b86f73a9b2c1f4805e0ee1390312332d
Denys Tkachuk <denys.tkachuk@sperse.com>
2018-11-22 16:38:04 +0200

    [SC-5823] apply review comments

--- src/personal-finance/landings/lend-space-dark/lend-space-dark.component.ts
+++ src/personal-finance/landings/lend-space-dark/lend-space-dark.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
     selector: 'app-lend-space-dark',
     templateUrl: './lend-space-dark.component.html',
-    styleUrls: ['./lend-space-dark.component.less']
+    styleUrls: ['./lend-space-dark.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LendSpaceDarkComponent implements OnInit {
     faq = [
@@ -14,6 +15,43 @@ export class LendSpaceDarkComponent implements OnInit {
         {title: 'Are there any hidden fees associated with applying online at CreditSoup®?', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'},
         {title: 'Will the lender require any documentation prior to closing my loan?', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'},
         {title: 'What is the interest rate?', text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'}
+    ];
+    buttons = [
+        {title: 'Credit Card', description: 'I want to get my'},
+        {title: 'Personal Loan', description: 'I want to apply for a'},
+        {title: 'Credit Score', description: 'I want to get my free'}
+    ];
+    features = [
+        {
+            imgSrc: './assets/common/images/lend-space-dark/credit-score.png', 
+            title: 'Credit Scores',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        },
+        {
+            imgSrc: './assets/common/images/lend-space-dark/credit-cards.png',
+            title: 'Credit Cards',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        },
+        {
+            imgSrc: './assets/common/images/lend-space-dark/retirement-planning.png',
+            title: 'Retirement Planning',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        },
+        {
+            imgSrc: './assets/common/images/lend-space-dark/personal-loans.png',
+            title: 'Personal Loans',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        },
+        {
+            imgSrc: './assets/common/images/lend-space-dark/business-loans.png',
+            title: 'Business Loans',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        },
+        {
+            imgSrc: './assets/common/images/lend-space-dark/get-debt-free.png',
+            title: 'Get Debt Free',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse.'
+        }
     ];
 
     constructor() {
