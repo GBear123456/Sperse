417cf251108c74f2e10abcf08b640e46c4f5faec
Oleh Stakhiv <veishen19@gmail.com>
2018-12-17 13:08:27 +0200

    merge fix

--- src/app/crm/contacts/details-header.component.ts
+++ src/app/crm/contacts/details-header.component.ts
@@ -24,6 +24,7 @@ import {
     OrganizationInfoDto,
     OrganizationContactServiceProxy,
     PersonContactServiceProxy,
+    PersonOrgRelationServiceProxy,
     UpdateJobTitleInput,
     UpdateOrganizationInfoInput,
     UpdatePersonInfoInput
