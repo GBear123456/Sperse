4f668c50db6e14c8e45c9ff73fb302e17e3d43a0
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-01-10 16:22:21 +0200

    [SC-6424] added sorting of offers groups as in ItemOfOfferCollection enum

--- src/personal-finance/shared/offers/credit-cards/credit-cards.component.ts
+++ src/personal-finance/shared/offers/credit-cards/credit-cards.component.ts
@@ -60,7 +60,10 @@ export class CreditCardsComponent implements OnInit {
             this.selectedOfferGroup = _.first(list.filter(item => this.route.snapshot.params.group == item.offerCollection));
             this.creditCardCollection = list.filter(item => !_.contains(this.creditScoreNames, item.offerCollection));
             this.bestCardsByScore = list.filter(item => _.contains(this.creditScoreNames, item.offerCollection));
-            this.filteredGroup = _.uniq(this.creditCardCollection, 'offerCollection');
+            const itemOfOfferCollections = _.values(ItemOfOfferCollection);
+            this.filteredGroup = _.uniq(this.creditCardCollection, 'offerCollection').sort((a, b) => {
+                return itemOfOfferCollections.indexOf(a['offerCollection']) > itemOfOfferCollections.indexOf(b['offerCollection']) ? 1 : -1;
+            });
         });
     }
 
