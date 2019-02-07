48865f36abb0dd9da327132f4f17b3df57898f41
Yulia Shkliarenko <ys@sperse.com>
2019-02-07 17:39:56 +0200

    [SC-6481] Created PFM Admin module and Offers section

---
+++ src/app/pfm/pfm.module.ts
@@ -0,0 +1,38 @@
+/** Core imports */
+import { NgModule } from '@angular/core';
+import * as ngCommon from '@angular/common';
+
+/** Third party imports */
+import { DxDataGridModule, DxTooltipModule, DxListModule, DxTextBoxModule } from '../../node_modules/devextreme-angular';
+
+/** Application imports */
+import { AppCommonModule } from '../shared/common/app-common.module';
+import { CommonModule } from '@shared/common/common.module';
+import { OffersComponent } from '@app/pfm/offers/offers.component';
+import { PfmRoutingModule } from '@app/pfm/pfm-routing.module';
+import { OfferCategoriesComponent } from '@app/pfm/shared/offer-categories/offer-categories.component';
+import { OffersService } from 'personal-finance/shared/offers/offers.service';
+import { OfferServiceProxy } from '@shared/service-proxies/service-proxies';
+
+@NgModule({
+    imports: [
+        PfmRoutingModule,
+        ngCommon.CommonModule,
+        CommonModule,
+        AppCommonModule,
+        DxDataGridModule,
+        DxTooltipModule,
+        DxListModule,
+        DxTextBoxModule
+    ],
+    declarations: [
+        OfferCategoriesComponent,
+        OffersComponent
+    ],
+    entryComponents: [
+    ],
+    providers: [
+    ]
+})
+
+export class PfmModule { }
