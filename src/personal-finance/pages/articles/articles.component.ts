254034649671ab0cdb8f998250e4c4f0cedc7936
Denys Tkachuk <denys.tkachuk@sperse.com>
2019-02-12 14:59:57 +0200

    [SC-6830] call SubmitRequest API with pre-pop params on app Resources page

--- src/personal-finance/pages/articles/articles.component.ts
+++ src/personal-finance/pages/articles/articles.component.ts
@@ -1,9 +1,12 @@
-import {Component, Injector, OnInit} from '@angular/core';
-import {AppComponentBase} from '@shared/common/app-component-base';
+import { Component, Injector, OnInit } from '@angular/core';
+import { AppComponentBase } from '@shared/common/app-component-base';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { environment } from 'environments/environment';
+
 import { Observable, from } from 'rxjs';
 import { map } from 'rxjs/operators';
+
+import { OffersService } from '@root/personal-finance/shared/offers/offers.service';
+import { AppConsts } from '@shared/AppConsts';
 
 @Component({
     selector: 'articles',
@@ -14,24 +17,34 @@ export class ArticlesComponent extends AppComponentBase implements OnInit {
     articles$: Observable<SafeHtml>;
 
     constructor(injector: Injector,
-        private sanitizer: DomSanitizer
+        private sanitizer: DomSanitizer,
+        private offersService: OffersService
     ) {
         super(injector);
     }
 
     ngOnInit() {
         this.startLoading(true);
-        this.articles$ = from(
-            $.ajax({
-                url: environment.LENDSPACE_DOMAIN + '/documents/articles.html',
-                method: 'GET'
-            })
-        ).pipe(
-            map((html) => {
-                this.finishLoading(true);
-                return this.sanitizer.bypassSecurityTrustHtml(html);
-            })
+        this.offersService.memberInfo$.subscribe(
+            () => {
+                this.articles$ = from(
+                    $.ajax({
+                        url: './assets/articles.html',
+                        method: 'GET'
+                    })
+                ).pipe(
+                    map((html) => {
+                        html = html.replace('var applyOfferParams', 'var applyOfferParams = "' + this.offersService.memberInfoApplyOfferParams + '"');
+                        html = html.replace('var apiUiOrigin', 'var apiUiOrigin = "' + AppConsts.remoteServiceBaseUrl + '"');
+                        eval(html.match(/<script>(.+?)<\/script>/s).pop());
+                        return html;
+                    }),
+                    map((html) => {
+                        this.finishLoading(true);
+                        return this.sanitizer.bypassSecurityTrustHtml(html);
+                    })
+                );
+            }
         );
     }
-
 }
