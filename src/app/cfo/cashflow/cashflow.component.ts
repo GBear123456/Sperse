490b6f6cedc2b0311afc459ea71e102edfee8a03
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-04-01 14:38:27 +0300

    [SC-7343] added reloading of category tree for refresh and category change, fixed bug with starting editing after white space click

--- src/app/cfo/cashflow/cashflow.component.ts
+++ src/app/cfo/cashflow/cashflow.component.ts
@@ -1285,7 +1285,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
      * Handle get categories result
      * @param getCategoriesResult
      */
-    handleGetCategoryTreeResult(getCategoriesResult) {
+    handleGetCategoryTreeResult(getCategoriesResult: GetCategoryTreeOutput) {
         this.categoryTree = getCategoriesResult;
         /** Add starting balance, ending balance, netchange and balance discrepancy */
         for (let type in this.cashflowTypes) {
@@ -1926,6 +1926,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
         this.getNewTextWidth.cache = {};
         this.monthsDaysLoadedPathes = [];
         this.anotherPeriodAccountsValues.clear();
+        this.reloadCategoryTree();
         this._bankAccountsService.load().subscribe(() => {
             this.setBankAccountsFilter(true);
             this.finishLoading();
@@ -4009,7 +4010,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
     bindCollapseActionOnWhiteSpaceColumn(cellObj) {
         let rowSpan = cellObj.cellElement.rowSpan || 1;
         let totalCell = $(cellObj.cellElement).parent().nextAll().eq(rowSpan - 1).first().find('td.dx-total');
-        totalCell.trigger('click');
+        setTimeout(() => totalCell.trigger('click'));
     }
 
     getRequestFilterFromPath(path) {
@@ -4193,11 +4194,19 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
                 this.cashflowService.removeModifyingCellInput();
                 this.cashflowService.valueIsChanging = false;
             })
-        ).subscribe(x => {
+        ).subscribe(() => {
+            this.reloadCategoryTree();
             this.categoryTree.categories[id].name = e.value;
             this.cashflowService.modifyingInputObj.cell.text = e.value;
             this.pivotGrid.instance.getDataSource().reload();
         });
+    }
+
+    private reloadCategoryTree() {
+        this._categoryTreeServiceProxy.get(InstanceType[this.instanceType], this.instanceId, false)
+            .subscribe((categoryTreeResult: GetCategoryTreeOutput) => {
+                this.handleGetCategoryTreeResult(categoryTreeResult);
+            });
     }
 
     projectedFieldIsVisible() {
