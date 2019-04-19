8cf3223325223f6022ec955609e8fe3c13cd9946
Volodymyr Peren <vp@sperse.com>
2019-04-19 12:40:34 +0300

    [SC-7643] - Browser title to show Client Name in CFOMembers layout

--- src/app/shared/common/contact-info-panel/contact-info-panel.component.ts
+++ src/app/shared/common/contact-info-panel/contact-info-panel.component.ts
@@ -1,17 +1,35 @@
 import { Component, Injector, Input } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
+import { TenantLoginInfoDtoCustomLayoutType } from '@shared/service-proxies/service-proxies';
+import { AppSessionService } from '@shared/common/session/app-session.service';
 
 @Component({
-  selector: 'contact-info-panel',
-  templateUrl: './contact-info-panel.component.html',
-  styleUrls: ['./contact-info-panel.component.less']
+    selector: 'contact-info-panel',
+    templateUrl: './contact-info-panel.component.html',
+    styleUrls: ['./contact-info-panel.component.less']
 })
 export class ContactInfoPanelComponent extends AppComponentBase {
-  @Input() data: any;
+    private _data: any;
 
-  constructor(
-    injector: Injector
-  ) {
-    super(injector);
-  }
+    @Input() 
+    set data(value) {
+        this._data = value;
+        if (this.data && this.checkCFOMember())
+            this.setTitle(this.data.fullName);
+    }
+    get data(): any { 
+        return this._data;
+    }
+
+    constructor(
+        injector: Injector,
+        private _appSession: AppSessionService
+    ) {
+        super(injector);
+    }
+
+    checkCFOMember() {
+        let tenant = this._appSession.tenant;
+        return tenant && tenant.customLayoutType == TenantLoginInfoDtoCustomLayoutType.CFOMembers;
+    }
 }
