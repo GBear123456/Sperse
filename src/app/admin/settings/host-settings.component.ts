ddd40788cc7032849f6b137fa2737cd400307665
Denys Tkachuk <denys.tkachuk@sperse.com>
2019-05-29 18:06:35 +0300

    [SC-7984] fixed links to style

--- src/app/admin/settings/host-settings.component.ts
+++ src/app/admin/settings/host-settings.component.ts
@@ -18,7 +18,7 @@ import {
 @Component({
     templateUrl: './host-settings.component.html',
     animations: [appModuleAnimation()],
-    styleUrls: ['../../shared/_checkbox-radio', './host-settings.component.less'],
+    styleUrls: ['../../shared/_checkbox-radio.less', './host-settings.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     providers: [TenantPaymentSettingsServiceProxy]
 })
