941968a97aa7c7a0744edc22785762905f7b2499
Yulia Shkliarenko <ys@sperse.com>
2019-06-10 19:24:57 +0300

    [SC-8278] Changed CoAId Type

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -34243,7 +34243,7 @@ export interface IAccountingTypeDto {
 export class CategoryDto implements ICategoryDto {
     accountingTypeId!: number | undefined;
     parentId!: number | undefined;
-    coAID!: number | undefined;
+    coAID!: string | undefined;
     name!: string | undefined;
     isActive!: boolean | undefined;
 
@@ -34287,7 +34287,7 @@ export class CategoryDto implements ICategoryDto {
 export interface ICategoryDto {
     accountingTypeId: number | undefined;
     parentId: number | undefined;
-    coAID: number | undefined;
+    coAID: string | undefined;
     name: string | undefined;
     isActive: boolean | undefined;
 }
@@ -34295,7 +34295,7 @@ export interface ICategoryDto {
 export class CreateCategoryInput implements ICreateCategoryInput {
     accountingTypeId!: number;
     parentId!: number | undefined;
-    coAID!: number | undefined;
+    coAID!: string | undefined;
     name!: string;
 
     constructor(data?: ICreateCategoryInput) {
@@ -34336,13 +34336,13 @@ export class CreateCategoryInput implements ICreateCategoryInput {
 export interface ICreateCategoryInput {
     accountingTypeId: number;
     parentId: number | undefined;
-    coAID: number | undefined;
+    coAID: string | undefined;
     name: string;
 }
 
 export class UpdateCategoryInput implements IUpdateCategoryInput {
     id!: number;
-    coAID!: number | undefined;
+    coAID!: string | undefined;
     name!: string;
     accountingTypeId!: number;
     parentId!: number | undefined;
@@ -34386,7 +34386,7 @@ export class UpdateCategoryInput implements IUpdateCategoryInput {
 
 export interface IUpdateCategoryInput {
     id: number;
-    coAID: number | undefined;
+    coAID: string | undefined;
     name: string;
     accountingTypeId: number;
     parentId: number | undefined;
@@ -34557,7 +34557,7 @@ export interface ISyncDto {
 }
 
 export class AccountingCategoryDto implements IAccountingCategoryDto {
-    coAID!: number | undefined;
+    coAID!: string | undefined;
     cashType!: string | undefined;
     accountingType!: string;
     category!: string;
@@ -34610,7 +34610,7 @@ export class AccountingCategoryDto implements IAccountingCategoryDto {
 }
 
 export interface IAccountingCategoryDto {
-    coAID: number | undefined;
+    coAID: string | undefined;
     cashType: string | undefined;
     accountingType: string;
     category: string;
