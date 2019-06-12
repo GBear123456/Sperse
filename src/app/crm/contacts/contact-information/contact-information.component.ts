f64075e3783c71769e41d87a8cac88bfe7b0fbec
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-06-12 19:41:23 +0300

    [SC-8143] created offer-notify-dialog instead of swal small dialog, added toggle button and select box to choose announcement service and choose target list, moved toggle button styles to common less file, added its importing in package selector and offer-notify dialog

--- src/app/crm/contacts/contact-information/contact-information.component.ts
+++ src/app/crm/contacts/contact-information/contact-information.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { ContactServiceProxy, ContactInfoDto } from '@shared/service-proxies/service-proxies';
 import { AppComponentBase } from '@shared/common/app-component-base';
-import { AppConsts } from '@shared/AppConsts';
 
 @Component({
     selector: 'contact-information',
