27dbf09252a5bb04b9dccb1d777e24ad71a584d0
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-06-26 11:12:46 +0300

    [SC-8421] added default date to the filter, removed extends AppComponentBase for FilterCalendarComponent

--- src/shared/filters/calendar/filter-calendar.component.ts
+++ src/shared/filters/calendar/filter-calendar.component.ts
@@ -1,20 +1,15 @@
-import { Component, Injector } from '@angular/core';
-import { AppComponentBase } from '@shared/common/app-component-base';
+import { Component } from '@angular/core';
 import { FilterComponent } from '../models/filter-component';
 
 @Component({
     templateUrl: './filter-calendar.component.html',
     styleUrls: ['./filter-calendar.component.less']
 })
-export class FilterCalendarComponent extends AppComponentBase implements FilterComponent {
+export class FilterCalendarComponent implements FilterComponent {
     options: any;
     items: {
         from: any,
         to: any
     };
     apply: (event) => void;
-
-    constructor(injector: Injector) {
-        super(injector);
-    }
 }
