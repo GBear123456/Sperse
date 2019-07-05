67a4fd0ce9e6aee778c8a794727360ca62f025b6
Denys Tkachuk <denys.tkachuk@sperse.com>
2019-07-05 12:57:23 +0300

    [SC-8490] updated tabs and some styles

--- src/shared/bank-code-wizzard/bank-code-wizzard-tabs/bank-code-wizzard-tabs.component.ts
+++ src/shared/bank-code-wizzard/bank-code-wizzard-tabs/bank-code-wizzard-tabs.component.ts
@@ -6,20 +6,10 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./bank-code-wizzard-tabs.component.less']
 })
 export class BankCodeWizzardTabsComponent implements OnInit {
-    tabLoadTimes: Date[] = [];
-    
-    getTimeLoaded(index: number) {
-        if (!this.tabLoadTimes[index]) {
-            this.tabLoadTimes[index] = new Date();
-        }
-        
-        return this.tabLoadTimes[index];
-    }
-    
     constructor() {
     }
-    
+
     ngOnInit() {
     }
-    
+
 }
