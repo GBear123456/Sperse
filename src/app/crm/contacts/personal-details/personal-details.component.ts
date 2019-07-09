14b4f8a41f17944660964424776161277ff7bebe
Volodymyr Peren <vp@sperse.com>
2019-07-09 16:00:36 +0300

    [SC-8526] - Connect GetInterests on Personal Details

--- src/app/crm/contacts/personal-details/personal-details.component.ts
+++ src/app/crm/contacts/personal-details/personal-details.component.ts
@@ -16,7 +16,7 @@ import { CountriesStoreActions, CountriesStoreSelectors } from '@app/store';
 import { RootStore, StatesStoreActions, StatesStoreSelectors } from '@root/store';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { PersonContactServiceProxy, UpdatePersonInfoInput, UpdatePersonInfoInputMaritalStatus, UpdatePersonInfoInputGender, PersonInfoDtoMaritalStatus,
-    PersonInfoDtoGender, PersonInfoDto, UpdatePersonInfoInputPreferredToD, TimingServiceProxy } from 'shared/service-proxies/service-proxies';
+    DictionaryServiceProxy, PersonInfoDtoGender, PersonInfoDto, UpdatePersonInfoInputPreferredToD, TimingServiceProxy } from 'shared/service-proxies/service-proxies';
 import { ContactsService } from '../contacts.service';
 
 @Component({
@@ -29,28 +29,28 @@ export class PersonalDetailsComponent implements OnDestroy {
     person: PersonInfoDto;
     isEditAllowed = false;
     startCase = startCase;
-
+    interests$ = this._dictionaryProxy.getInterests();
     accessConfidentialData = this._permission.isGranted('Pages.CRM.AccessConfidentialData');
 
     columns = [
         [
-            { name: 'citizenship',              type: 'select'                         },
-            { name: 'timeZone',                 type: 'select'                         },
-            { name: 'dob',                      type: 'date'                           },
-            { name: 'drivingLicense',           type: 'string',     confidential: true },
-            { name: 'drivingLicenseState',      type: 'select'                         },
-            { name: 'experience',               type: 'string',     multiline:    true },
-            { name: 'gender',                   type: 'select'                         },
-            { name: 'interests',                type: 'list',       multiline:    true },
+            { name: 'citizenship',          type: 'select'                                              },
+            { name: 'timeZone',             type: 'select'                                              },
+            { name: 'dob',                  type: 'date'                                                },
+            { name: 'drivingLicense',       type: 'string', confidential: true                          },
+            { name: 'drivingLicenseState',  type: 'select'                                              },
+            { name: 'experience',           type: 'string', multiline:    true                          },
+            { name: 'gender',               type: 'select'                                              },
+            { name: 'interests',            type: 'list',   multiline:    true, source: this.interests$ },
         ], [
-            { name: 'isActiveMilitaryDuty',     type: 'bool'                           },
-            { name: 'isUSCitizen',              type: 'bool'                           },
-            { name: 'maritalStatus',            type: 'select'                         },
-            { name: 'marriageDate',             type: 'date'                           },
-            { name: 'divorceDate',              type: 'date'                           },
-            { name: 'profileSummary',           type: 'string',     multiline:    true },
-            { name: 'preferredToD',             type: 'select'                         },
-            { name: 'ssn',                      type: 'string',     confidential: true }
+            { name: 'isActiveMilitaryDuty', type: 'bool'                                                },
+            { name: 'isUSCitizen',          type: 'bool'                                                },
+            { name: 'maritalStatus',        type: 'select'                                              },
+            { name: 'marriageDate',         type: 'date'                                                },
+            { name: 'divorceDate',          type: 'date'                                                },
+            { name: 'profileSummary',       type: 'string', multiline:    true                          },
+            { name: 'preferredToD',         type: 'select'                                              },
+            { name: 'ssn',                  type: 'string', confidential: true                          }
         ]
     ];
 
@@ -78,8 +78,9 @@ export class PersonalDetailsComponent implements OnDestroy {
         private _changeDetector: ChangeDetectorRef,
         private _permission: AppPermissionService,
         private _personContactService: PersonContactServiceProxy,
+        private _dictionaryProxy: DictionaryServiceProxy,
         private _asyncPipe: AsyncPipe
-    ) {
+    ) {        
         this._contactsService.contactInfoSubscribe((contactInfo) => {
             this.person = contactInfo.personContactInfo.person;
             this.isEditAllowed = this._contactsService.checkCGPermission(contactInfo.groupId);
