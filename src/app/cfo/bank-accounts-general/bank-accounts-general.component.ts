c9e9a61e44485b19422241059852947f948c8fcc
Volodymyr Peren <vp@sperse.com>
2019-07-11 14:42:34 +0300

    Fixed build issue

--- src/app/cfo/bank-accounts-general/bank-accounts-general.component.ts
+++ src/app/cfo/bank-accounts-general/bank-accounts-general.component.ts
@@ -12,7 +12,7 @@ import { CFOComponentBase } from '@shared/cfo/cfo-component-base';
 import { SynchProgressComponent } from '@shared/cfo/bank-accounts/synch-progress/synch-progress.component';
 import { SynchProgressService } from '@shared/cfo/bank-accounts/helpers/synch-progress.service';
 import { BankAccountsGeneralService } from '@shared/cfo/bank-accounts/helpers/bank-accounts-general.service';
-import { SyncAccountServiceProxy, InstanceType95 } from '@shared/service-proxies/service-proxies';
+import { SyncAccountServiceProxy, InstanceType94 } from '@shared/service-proxies/service-proxies';
 import { CfoPreferencesService } from '@app/cfo/cfo-preferences.service';
 import { CfoStore, CurrenciesStoreSelectors } from '@app/cfo/store';
 import { LifecycleSubjectsService } from '@shared/common/lifecycle-subjects/lifecycle-subjects.service';
@@ -44,7 +44,7 @@ export class BankAccountsGeneralComponent extends CFOComponentBase implements On
         this._synchProgress.needRefreshSync$.subscribe(() => {
             this._synchProgress.startSynchronization();
         });
-        this._syncAccountServiceProxy.createIsAllowed(this._cfoService.instanceType as InstanceType95, this.instanceId)
+        this._syncAccountServiceProxy.createIsAllowed(this._cfoService.instanceType as InstanceType94, this.instanceId)
             .subscribe((result) => {
                 this.createAccountAvailable = result;
                 this.initHeadlineConfig();
--- src/shared/common/account-connector-dialog/xero-login/xero-login.component.ts
+++ src/shared/common/account-connector-dialog/xero-login/xero-login.component.ts
@@ -19,7 +19,7 @@ import {
     CategoryTreeServiceProxy,
     InstanceType43,
     InstanceType91,
-    InstanceType92,
+    InstanceType90,
     UpdateSyncAccountInput
 } from 'shared/service-proxies/service-proxies';
 import { AppConsts } from '@shared/AppConsts';
@@ -76,7 +76,7 @@ export class XeroLoginComponent {
     connectToXero(e) {
         abp.ui.setBusy(this._elementRef.nativeElement);
         this._syncAccountServiceProxy.create(
-            this._cfoService.instanceType as InstanceType91,
+            this._cfoService.instanceType as InstanceType90,
             this._cfoService.instanceId,
             new CreateSyncAccountInput({
                 typeId: SyncTypeIds.Xero,
@@ -114,7 +114,7 @@ export class XeroLoginComponent {
     updateSyncAccount() {
         abp.ui.setBusy(this._elementRef.nativeElement);
         const accountId = this.accountId;
-        this._syncAccountServiceProxy.update(this._cfoService.instanceType as InstanceType92, this._cfoService.instanceId,
+        this._syncAccountServiceProxy.update(this._cfoService.instanceType as InstanceType91, this._cfoService.instanceId,
             new UpdateSyncAccountInput({
                 id: accountId,
                 consumerKey: this.consumerKey,
