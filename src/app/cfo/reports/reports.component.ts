7da6b9ad1b6319d7bcc45721df0415dd46b459a1
Volodymyr Peren <vp@sperse.com>
2019-07-11 12:32:20 +0300

    [SC-8513] - Implementing reports section

--- src/app/cfo/reports/reports.component.ts
+++ src/app/cfo/reports/reports.component.ts
@@ -1,22 +1,27 @@
 /** Core imports */
-import { Component, Injector, OnInit, AfterViewInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, Injector, OnInit, AfterViewInit, OnDestroy, ViewChild, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { CFOComponentBase } from '@shared/cfo/cfo-component-base';
 
 /** Third party imports */
 import { MatDialog } from '@angular/material';
 import { Observable, Subject, of } from 'rxjs';
+import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
 
 /** Application imports */
 import { AppService } from '@app/app.service';
 import { AppConsts } from '@shared/AppConsts';
-import { ReportsServiceProxy } from '@shared/service-proxies/service-proxies';
+import { FileSizePipe } from '@shared/common/pipes/file-size.pipe';
+import { ReportsServiceProxy, InstanceType78 } from '@shared/service-proxies/service-proxies';
 
 @Component({
     templateUrl: './reports.component.html',
     styleUrls: ['./reports.component.less'],
-    providers: [ ReportsServiceProxy ]
+    providers: [ ReportsServiceProxy, FileSizePipe ],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReportsComponent extends CFOComponentBase implements OnInit, AfterViewInit, OnDestroy {
+    @ViewChild(DxDataGridComponent) dataGrid: DxDataGridComponent;
+
     headlineConfig;
     menuItems = [
          {
@@ -30,13 +35,17 @@ export class ReportsComponent extends CFOComponentBase implements OnInit, AfterV
          }
      ];
 
+    formatting = AppConsts.formatting;
+    reports$ = this.reportsProxy.getAll(InstanceType78[this.instanceType], this.instanceId);
+
     constructor(
         private injector: Injector,
         private _appService: AppService,
+        private _fileSizePipe: FileSizePipe,
+        private _changeDetector: ChangeDetectorRef,
         public reportsProxy: ReportsServiceProxy
     ) {
-        super(injector);
-        
+        super(injector);        
     }
 
     ngOnInit(): void {
@@ -51,9 +60,24 @@ export class ReportsComponent extends CFOComponentBase implements OnInit, AfterV
         this.headlineConfig = {
             names: [this.l('REPORTS')],
             onRefresh: () => {
+                this.dataGrid.instance.refresh();
             },
             iconSrc: './assets/common/icons/credit-card-icon.svg'
         };
+    }
+
+    calculateFileSizeValue = (data) => this._fileSizePipe.transform(data.size);
+    numerizeFileSizeSortValue = (data) => +data.size;
+
+    onDataGridInit(event) {
+        
+    }
+
+    onCellClick(event) {
+    }
+
+    onContentReady() {
+        this.setGridDataLoaded();
     }
 
     onMenuClick(item) {
