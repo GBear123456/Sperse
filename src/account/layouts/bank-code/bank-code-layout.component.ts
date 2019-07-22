771b1a9d7887fb966cacc86c83b2dd5d0681511d
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-07-22 14:20:19 +0300

    [SC-8564] removed extending of AppComponentBase for BankCodeLayoutComponent component

--- src/account/layouts/bank-code/bank-code-layout.component.ts
+++ src/account/layouts/bank-code/bank-code-layout.component.ts
@@ -1,9 +1,8 @@
 /** Core imports */
-import { Component, Injector, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 /** Application imports */
 import { AppConsts } from '@shared/AppConsts';
-import { AppComponentBase } from '@shared/common/app-component-base';
 import { AppSessionService } from '@shared/common/session/app-session.service';
 import * as moment from 'moment';
 
@@ -13,18 +12,15 @@ import * as moment from 'moment';
         './bank-code-layout.component.less'
     ]
 })
-export class BankCodeLayoutComponent extends AppComponentBase implements OnInit {
+export class BankCodeLayoutComponent implements OnInit {
     currentYear: number = moment().year();
     tenantName = AppConsts.defaultTenantName;
     remoteServiceBaseUrl = AppConsts.remoteServiceBaseUrl;
     originUrl = location.origin;
 
     constructor(
-        injector: Injector,
         private _appSession: AppSessionService
-    ) {
-        super(injector);
-    }
+    ) {}
 
     ngOnInit(): void {
         let tenant = this._appSession.tenant;
