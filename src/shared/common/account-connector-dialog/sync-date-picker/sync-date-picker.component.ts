b8a3cd76d904c9dfb14f8473c875e28a2fe97e1f
Denys Tkachuk <denys.tkachuk@sperse.com>
2019-07-23 16:18:00 +0300

    [SC-8608] applied test comments, fixed some formatting

--- src/shared/common/account-connector-dialog/sync-date-picker/sync-date-picker.component.ts
+++ src/shared/common/account-connector-dialog/sync-date-picker/sync-date-picker.component.ts
@@ -1,5 +1,5 @@
 /** Core imports */
-import {Component, Injector, ViewChild, OnInit, EventEmitter, Output, HostListener} from '@angular/core';
+import {Component, Injector, ViewChild, HostListener} from '@angular/core';
 
 /** Third party imports */
 import { DxDateBoxComponent } from 'devextreme-angular/ui/date-box';
@@ -8,25 +8,26 @@ import * as moment from 'moment-timezone';
 /** Application imports */
 import { SyncDatePickerService } from './sync-date-picker.service';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
+import { CFOComponentBase } from '@shared/cfo/cfo-component-base';
+import { CFOService } from '@shared/cfo/cfo.service';
 
 @Component({
   selector: 'sync-date-picker',
   templateUrl: './sync-date-picker.component.html',
   styleUrls: ['./sync-date-picker.component.less']
 })
-export class SyncDatePickerComponent {
+export class SyncDatePickerComponent extends CFOComponentBase {
     @ViewChild(DxDateBoxComponent) dateBox: DxDateBoxComponent;
-
+    maxDate = moment();
     @HostListener('click') onClick() {
         this.dateBox.instance.open();
     }
 
-    maxDate = moment();
-
     constructor(
-        public syncService: SyncDatePickerService,
-        public ls: AppLocalizationService
+        injector: Injector,
+        public syncService: SyncDatePickerService
     ) {
+        super(injector);
     }
 
     apply(event) {
