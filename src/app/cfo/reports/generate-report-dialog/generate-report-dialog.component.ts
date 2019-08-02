51742a1110ed7d5cadc700d1a32be054340ad9f6
Yulia Shkliarenko <ys@sperse.com>
2019-08-02 13:00:43 +0300

    [SC-8760] Added 3rd step to report generation wizard

--- src/app/cfo/reports/generate-report-dialog/generate-report-dialog.component.ts
+++ src/app/cfo/reports/generate-report-dialog/generate-report-dialog.component.ts
@@ -7,7 +7,8 @@ import moment from 'moment-timezone';
 import * as _ from 'underscore';
 import { NotifyService } from 'abp-ng2-module/dist/src/notify/notify.service';
 import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
-import { first, switchMap, tap } from 'rxjs/operators';
+import { first, switchMap, tap, map } from 'rxjs/operators';
+import { from, forkJoin } from 'rxjs';
 import { select, Store } from '@ngrx/store';
 
 /** Application imports */
@@ -21,15 +22,16 @@ import { GenerateReportStep } from './generate-report-step.enum';
 import { CfoStore, CurrenciesStoreSelectors } from '@app/cfo/store';
 
 @Component({
-  templateUrl: 'generate-report-dialog.component.html',
-  styleUrls: ['generate-report-dialog.component.less'],
-  providers: [ ReportsServiceProxy ]
+    templateUrl: 'generate-report-dialog.component.html',
+    styleUrls: ['generate-report-dialog.component.less'],
+    providers: [ReportsServiceProxy]
 })
 export class GenerateReportDialogComponent implements OnInit {
     @ViewChild(DxDataGridComponent) dataGrid: DxDataGridComponent;
     @ViewChild(ModalDialogComponent) modalDialog: ModalDialogComponent;
 
     title = this.ls.l('SelectBusinessEntity');
+    initButtons: IDialogButton[];
     buttons: IDialogButton[];
     currentStep = GenerateReportStep.Step1;
     generateReportSteps = GenerateReportStep;
@@ -45,6 +47,11 @@ export class GenerateReportDialogComponent implements OnInit {
             showAllAvailableDatesButton: false
         }
     };
+    radioGroupGrouping = [
+        { text: 'Separate', value: true },
+        { text: 'Combined', value: false }
+    ];
+    isSeparateGrouping = true;
 
     private readonly BACK_BTN_INDEX = 0;
     private readonly NEXT_BTN_INDEX = 1;
@@ -62,18 +69,12 @@ export class GenerateReportDialogComponent implements OnInit {
     }
 
     ngOnInit() {
-        this.buttons = [
+        this.initButtons = [
             {
                 title: this.ls.l('Back'),
                 class: 'default',
                 disabled: true,
-                action: () => {
-                    this.currentStep--;
-                    this.title = this.ls.l('SelectBusinessEntity');
-                    this.buttons[this.NEXT_BTN_INDEX].title = this.ls.l('Next');
-                    this.buttons[this.NEXT_BTN_INDEX].action = this.next.bind(this);
-                    this.buttons[this.BACK_BTN_INDEX].disabled = true;
-                }
+                action: this.prev.bind(this)
             },
             {
                 id: 'next',
@@ -83,17 +84,53 @@ export class GenerateReportDialogComponent implements OnInit {
                 disabled: true
             }
         ];
+        this.buttons = this.initButtons;
         this.bankAccountsService.load();
     }
 
-    next() {
-        this.applyBusinessEntity();
+    prev() {
+        this.currentStep--;
+        this.processStep();
+    }
 
+    next() {
         this.currentStep++;
-        this.title = this.ls.l('SelectDateRange');
-        this.buttons[this.NEXT_BTN_INDEX].title = this.ls.l('Generate');
-        this.buttons[this.NEXT_BTN_INDEX].action = this.generateReport.bind(this);
-        this.buttons[this.BACK_BTN_INDEX].disabled = false;
+        if (this.currentStep == GenerateReportStep.Step2) {
+            this.applyBusinessEntity();
+        }
+        if (this.currentStep == GenerateReportStep.Step3) {
+            this.applyDateRange();
+        }
+        this.processStep();
+    }
+
+    processStep() {
+        this.buttons = this.initButtons;
+        if (this.currentStep == GenerateReportStep.Step1) {
+            this.title = this.ls.l('SelectBusinessEntity');
+            this.buttons[this.NEXT_BTN_INDEX].title = this.ls.l('Next');
+            this.buttons[this.NEXT_BTN_INDEX].action = this.next.bind(this);
+            this.buttons[this.BACK_BTN_INDEX].disabled = true;
+        }
+        if (this.currentStep == GenerateReportStep.Step2) {
+            this.title = this.ls.l('SelectDateRange');
+            this.buttons[this.NEXT_BTN_INDEX].title = this.ls.l('Next');
+            this.buttons[this.NEXT_BTN_INDEX].action = this.next.bind(this);
+            this.buttons[this.BACK_BTN_INDEX].disabled = false;
+        }
+        if (this.currentStep == GenerateReportStep.Step3) {
+            this.buttons = null;
+        }
+    }
+
+    editDate() {
+        this.currentStep = GenerateReportStep.Step2;
+        this.processStep();
+    }
+
+    editBusinessEntities() {
+        this.currentStep = GenerateReportStep.Step1;
+        this.processStep();
     }
 
     applyBusinessEntity() {
@@ -103,26 +140,37 @@ export class GenerateReportDialogComponent implements OnInit {
     }
 
     generateReport() {
-        this.applyDateRange();
+        this.modalDialog.startLoading();
         this.store$.pipe(
             select(CurrenciesStoreSelectors.getSelectedCurrencyId),
             first(),
             tap(() => this.notify.info(this.ls.l('GeneratingStarted'))),
-            switchMap(currencyId =>
-                this.reportsProxy.generate(<any>this.data.instanceType, this.data.instanceId, new GenerateInput({
-                    from: this.dateFrom && DateHelper.getDateWithoutTime(this.dateFrom),
-                    to: this.dateTo && DateHelper.getDateWithoutTime(this.dateTo),
-                    period: this.data.period,
-                    currencyId,
-                    businessEntityIds: this.selectedBusinessEntityIds,
-                    bankAccountIds: []
-                }))
-            )
+            switchMap(currencyId => {
+                let genOb = this.isSeparateGrouping ? this.selectedBusinessEntityIds.map(param =>
+                    this.reportsProxy.generate(<any>this.data.instanceType, this.data.instanceId, new GenerateInput({
+                        from: this.dateFrom && DateHelper.getDateWithoutTime(this.dateFrom),
+                        to: this.dateTo && DateHelper.getDateWithoutTime(this.dateTo),
+                        period: this.data.period,
+                        currencyId,
+                        businessEntityIds: [Number(param)],
+                        bankAccountIds: []
+                    }))) :
+                    this.reportsProxy.generate(<any>this.data.instanceType, this.data.instanceId, new GenerateInput({
+                        from: this.dateFrom && DateHelper.getDateWithoutTime(this.dateFrom),
+                        to: this.dateTo && DateHelper.getDateWithoutTime(this.dateTo),
+                        period: this.data.period,
+                        currencyId,
+                        businessEntityIds: this.selectedBusinessEntityIds,
+                        bankAccountIds: []
+                    }));
+                return forkJoin(genOb);
+            })
         ).subscribe(() => {
+            this.modalDialog.finishLoading();
             this.data.reportGenerated();
+            this.modalDialog.close(true);
             this.notify.info(this.ls.l('SuccessfullyGenerated'));
         });
-        this.modalDialog.close(true);
     }
 
     applyDateRange() {
@@ -137,7 +185,7 @@ export class GenerateReportDialogComponent implements OnInit {
     }
 
     onContentReady(event) {
-        this.onSelectionChanged({selectedRowKeys: event.component.getSelectedRowKeys()});
+        this.onSelectionChanged({ selectedRowKeys: event.component.getSelectedRowKeys() });
     }
 
     onSelectionChanged(event) {
--- src/app/cfo/reports/generate-report-dialog/generate-report-step.enum.ts
+++ src/app/cfo/reports/generate-report-dialog/generate-report-step.enum.ts
@@ -1,4 +1,5 @@
 export enum GenerateReportStep {
     Step1 = 1,
-    Step2 = 2
+    Step2 = 2,
+    Step3 = 3
 }
