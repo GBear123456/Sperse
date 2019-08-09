71099fc80e90902f31048441af55539b0cdc978b
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-08-09 20:04:44 +0300

    [SC-8906] added removing of doubled adjustment if it has already included in starting balance, added one time adjustment filtering instead of a lot filters in every calculateSummaryValue for starting and ending balances

--- src/app/cfo/cashflow/cashflow.component.ts
+++ src/app/cfo/cashflow/cashflow.component.ts
@@ -437,9 +437,8 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
             }
 
             if (this.cashflowService.isStartingBalanceDataColumn(cell, area)) {
-                let elements = this.cashflowService.adjustmentsList.filter(cashflowItem => {
-                    return cashflowItem.adjustmentType == AdjustmentType._0
-                        && (
+                let elements = this.cashflowService.zeroAdjustmentsList.filter(cashflowItem => {
+                    return (
                             cell.rowPath[1] === CategorizationPrefixes.AccountName + cashflowItem.accountId
                             || (
                                 cell.rowPath.length === 1
@@ -1364,6 +1363,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
                     this.cashflowService.adjustmentsList.splice(this.cashflowService.adjustmentsList.indexOf(item), 1);
                 }
             });
+            this.cashflowService.updateZeroAdjustmentsList();
         }
 
         transactions = this.getCashflowDataFromTransactions(transactions, false);
@@ -1557,6 +1557,7 @@ export class CashflowComponent extends CFOComponentBase implements OnInit, After
             return result;
         }, []);
 
+        this.cashflowService.updateZeroAdjustmentsList();
         this.transactionsTotal = +this.transactionsTotal.toFixed(2);
         this.transactionsAverage = this.transactionsAmount ? +(this.transactionsTotal / this.transactionsAmount).toFixed(2) : 0;
 
