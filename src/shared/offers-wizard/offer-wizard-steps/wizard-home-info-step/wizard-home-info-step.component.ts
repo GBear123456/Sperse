6bdfd9a5aa4617e77ac623bc7e104b6efb51b9d9
Denys Tkachuk <denys.tkachuk@sperse.com>
2019-08-09 14:10:56 +0300

    [SC-8855] added select for state field

--- src/shared/offers-wizard/offer-wizard-steps/wizard-home-info-step/wizard-home-info-step.component.ts
+++ src/shared/offers-wizard/offer-wizard-steps/wizard-home-info-step/wizard-home-info-step.component.ts
@@ -1,22 +1,43 @@
+/** Core imports */
 import { Component, OnInit } from '@angular/core';
+
+/** Third party imports */
+import { Store, select } from '@ngrx/store';
+import { RootStore, StatesStoreActions, StatesStoreSelectors } from '@root/store';
+
+/** Application imports */
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { InputStatusesService } from '@shared/utils/input-statuses.service';
 import { OffersWizardService } from '@shared/offers-wizard/offers-wizard.service';
+import { CountryStateDto } from '@shared/service-proxies/service-proxies';
 
 @Component({
-  selector: 'app-wizard-home-info-step',
-  templateUrl: './wizard-home-info-step.component.html',
-  styleUrls: ['./wizard-home-info-step.component.less']
+    selector: 'app-wizard-home-info-step',
+    templateUrl: './wizard-home-info-step.component.html',
+    styleUrls: ['./wizard-home-info-step.component.less']
 })
 export class WizardHomeInfoStepComponent implements OnInit {
+    countryCode = 'US';
+    states: CountryStateDto[];
 
-  constructor(
-      public ls: AppLocalizationService,
-      public inputStatusesService: InputStatusesService,
-      public offersWizardService: OffersWizardService
-  ) { }
+    constructor(
+        public ls: AppLocalizationService,
+        public inputStatusesService: InputStatusesService,
+        public offersWizardService: OffersWizardService,
+        private store$: Store<RootStore.State>
+    ) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.getStates();
+    }
 
+    getStates(): void {
+        this.store$.dispatch(new StatesStoreActions.LoadRequestAction(this.countryCode));
+        this.store$.pipe(select(StatesStoreSelectors.getState, { countryCode: this.countryCode }))
+            .subscribe(result => {
+                if (result) {
+                    this.states = result;
+                }
+            });
+    }
 }
