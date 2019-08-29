e1f7f2c2062773815826d810359cd308f64551cb
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-08-29 19:50:26 +0300

    [SC-9088] changed getUserForEdit request to store select(OrganizationUnitsStoreSelectors.getOrganizationUnits) to load all organization units

--- src/app/crm/contacts/lead-information/lead-information.component.ts
+++ src/app/crm/contacts/lead-information/lead-information.component.ts
@@ -2,28 +2,31 @@
 import { Component, OnInit, Injector, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 
 /** Third party imports */
-import { finalize } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
+import { finalize, filter, takeUntil } from 'rxjs/operators';
 import * as moment from 'moment-timezone';
 import startCase from 'lodash/startCase';
 import upperCase from 'lodash/upperCase';
-import find from 'lodash/find';
 
 /** Application imports */
 import { ContactGroup } from '@shared/AppEnums';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { ActivatedRoute } from '@angular/router';
 import {
-    ApplicationServiceProxy, LeadServiceProxy, LeadInfoDto, UserServiceProxy, UpdateContactOrganizationUnitInput,
-    ContactInfoDto, ContactServiceProxy, UpdateLeadInfoInput } from '@shared/service-proxies/service-proxies';
+    ApplicationServiceProxy, LeadServiceProxy, LeadInfoDto, UpdateContactOrganizationUnitInput,
+    ContactInfoDto, ContactServiceProxy, UpdateLeadInfoInput, OrganizationUnitDto
+} from '@shared/service-proxies/service-proxies';
 import { ContactsService } from '../contacts.service';
 import { AppConsts } from '@shared/AppConsts';
 import { AppPermissions } from '@shared/AppPermissions';
+import { CrmStore, OrganizationUnitsStoreActions, OrganizationUnitsStoreSelectors } from '@app/crm/store';
+import { LifecycleSubjectsService } from '@shared/common/lifecycle-subjects/lifecycle-subjects.service';
 
 @Component({
     selector: 'lead-information',
     templateUrl: './lead-information.component.html',
     styleUrls: ['./lead-information.component.less'],
-    providers: [ApplicationServiceProxy]
+    providers: [ ApplicationServiceProxy, LifecycleSubjectsService ]
 })
 export class LeadInformationComponent extends AppComponentBase implements OnInit, OnDestroy {
     @ViewChild('loaderWrapper') loaderWrapper: ElementRef;
@@ -32,7 +35,6 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
         leadInfo: LeadInfoDto
     };
     application: any;
-
     showApplicationAllowed = false;
     set selectedTabIndex(val: number) {
         if (this._selectedTabIndex = val)
@@ -42,7 +44,6 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
         return this._selectedTabIndex;
     }
 
-    private paramsSubscribe: any = [];
     private formatting = AppConsts.formatting;
     private _selectedTabIndex = 0;
     private readonly APP_TAB_INDEX = 1;
@@ -102,54 +103,63 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
     ];
 
     constructor(injector: Injector,
-        private _route: ActivatedRoute,
-        private _contactProxy: ContactServiceProxy,
-        private _leadService: LeadServiceProxy,
-        private _contactsService: ContactsService,
-        private _applicationProxy: ApplicationServiceProxy,
-        private _userService: UserServiceProxy
+        private route: ActivatedRoute,
+        private contactProxy: ContactServiceProxy,
+        private leadService: LeadServiceProxy,
+        private contactsService: ContactsService,
+        private applicationProxy: ApplicationServiceProxy,
+        private store$: Store<CrmStore.State>,
+        private lifeCycleService: LifecycleSubjectsService
     ) {
         super(injector);
 
-        this._contactsService.loadLeadInfo();
-        _contactsService.orgUnitsSaveSubscribe((data) => {
+        this.contactsService.loadLeadInfo();
+        contactsService.orgUnitsSaveSubscribe((data) => {
             let orgUnitId = data.length ? data[0] : undefined;
             if (orgUnitId && orgUnitId != this.data.leadInfo.organizationUnitId) {
                 this.data.leadInfo.organizationUnitId = orgUnitId;
-                _contactProxy.updateOrganizationUnit(new UpdateContactOrganizationUnitInput(
+                contactProxy.updateOrganizationUnit(new UpdateContactOrganizationUnitInput(
                     {contactId: this.data.contactInfo.id, organizationUnitId: orgUnitId})).subscribe();
             }
         }, this.constructor.name);
-
-        this._userService.getUserForEdit(undefined).subscribe(data => {
-            let orgUnit = find(data.allOrganizationUnits, {
-                id: this.data.leadInfo.organizationUnitId ||
-                    this.data.contactInfo.organizationUnitId
-            });
-            if (orgUnit) {
-                data.memberedOrganizationUnits = [orgUnit['code']];
-                setTimeout(() => this._contactsService.orgUnitsUpdate(data));
-            }
-        });
     }
 
     ngOnInit() {
-        this.data = this._contactProxy['data'];
-        this._contactsService.contactInfoSubscribe((contactInfo) => {
+        this.data = this.contactProxy['data'];
+        this.contactsService.contactInfoSubscribe((contactInfo) => {
             this.data.contactInfo = contactInfo;
             this.isEditAllowed = this.data.leadInfo && this.data.leadInfo.id &&
-                this._contactsService.checkCGPermission(contactInfo.groupId);
+                this.contactsService.checkCGPermission(contactInfo.groupId);
             this.showApplicationAllowed = this.isGranted(AppPermissions.PFMApplicationsViewApplications) &&
                 contactInfo.personContactInfo.userId && contactInfo.groupId == ContactGroup.Client;
+        });
+        this.loadOrganizationUnits();
+    }
+
+    private loadOrganizationUnits() {
+        this.store$.dispatch(new OrganizationUnitsStoreActions.LoadRequestAction(false));
+        this.store$.pipe(
+            select(OrganizationUnitsStoreSelectors.getOrganizationUnits),
+            takeUntil(this.lifeCycleService.destroy$),
+            filter(Boolean)
+        ).subscribe((organizationUnits: OrganizationUnitDto[]) => {
+            let data = { organizationUnits: organizationUnits };
+            const orgUnit = organizationUnits.find((organizationUnit: OrganizationUnitDto) => {
+                return organizationUnit.id === (this.data.leadInfo.organizationUnitId || this.data.contactInfo.organizationUnitId);
+            });
+            if (orgUnit) {
+                data['memberedOrganizationUnits'] = [orgUnit['code']];
+                setTimeout(() => this.contactsService.orgUnitsUpdate(data));
+            }
         });
     }
 
     loadApplication() {
         this.startLoading(false, this.loaderWrapper.nativeElement);
-        this._applicationProxy.getInitialMemberApplication(
+        this.applicationProxy.getInitialMemberApplication(
             this.data.contactInfo.personContactInfo.userId,
-            this.data.leadInfo.applicationId).pipe(finalize(() => this.finishLoading(false, this.loaderWrapper.nativeElement))).subscribe((responce) => {
-                this.application = responce;
+            this.data.leadInfo.applicationId).pipe(finalize(() => this.finishLoading(false, this.loaderWrapper.nativeElement))).subscribe((response) => {
+                this.application = response;
             }
         );
     }
@@ -163,14 +173,13 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
             return item.data;
 
         let field = item.name;
-        let result = {
+        return {
             id: (this.data && this.data.leadInfo) ? this.data.leadInfo.id : null,
             value: this.getPropValue(field),
             isEditDialogEnabled: false,
             lEntityName: field,
             lEditPlaceholder: this.l('EditValuePlaceholder')
         };
-        return result;
     }
 
     getPropValue(field) {
@@ -185,9 +194,9 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
     updateValue(value, item) {
         let fieldName = item.name;
         this.data.leadInfo[fieldName] = value;
-        this._leadService.updateLeadInfo(
+        this.leadService.updateLeadInfo(
             UpdateLeadInfoInput.fromJS(this.data.leadInfo)
-        ).subscribe(result => {});
+        ).subscribe();
     }
 
     getObjectKeys(obj) {
@@ -199,6 +208,6 @@ export class LeadInformationComponent extends AppComponentBase implements OnInit
     }
 
     ngOnDestroy() {
-        this._contactsService.unsubscribe(this.constructor.name);
+        this.contactsService.unsubscribe(this.constructor.name);
     }
 }
