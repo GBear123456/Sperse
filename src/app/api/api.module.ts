52ffcf1e468f93594c26311ed1230b188445fea2
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-08-29 18:53:33 +0300

    [SC-8994] added scroll to introduction section

--- src/app/api/api.module.ts
+++ src/app/api/api.module.ts
@@ -1,19 +1,24 @@
+/** Core imports */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+/** Third party imports */
+import { MatDialogModule } from '@angular/material/dialog';
+import { ClipboardModule } from 'ngx-clipboard';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
+import { DxTextBoxModule } from 'devextreme-angular/ui/text-box';
+import { DxDateBoxModule } from 'devextreme-angular/ui/date-box';
+import { DxValidatorModule } from 'devextreme-angular/ui/validator';
+import { DxScrollViewModule } from 'devextreme-angular/ui/scroll-view';
+import { DxValidationGroupModule } from 'devextreme-angular/ui/validation-group';
+
+/** Application imports */
 import { AppCommonModule } from '@app/shared/common/app-common.module';
 import { ApiRoutingModule } from './api-routing.module';
 import { SwaggerComponent } from './swagger/swagger.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 import { SetupStepComponent } from './shared/setup-steps/setup-steps.component';
 import { EditKeyDialog } from '@app/api/introduction/add-key-dialog/add-key-dialog.component';
-import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
-import { DxTextBoxModule } from 'devextreme-angular/ui/text-box';
-import { DxDateBoxModule } from 'devextreme-angular/ui/date-box';
-import { DxValidatorModule } from 'devextreme-angular/ui/validator';
-import { DxValidationGroupModule } from 'devextreme-angular/ui/validation-group';
-
-import { MatDialogModule } from '@angular/material/dialog';
-import { ClipboardModule } from 'ngx-clipboard';
 import { ApiWelcomeComponent } from './introduction/api-welcome/api-welcome.component';
 
 @NgModule({
@@ -26,6 +31,7 @@ import { ApiWelcomeComponent } from './introduction/api-welcome/api-welcome.comp
         DxDateBoxModule,
         DxValidatorModule,
         DxValidationGroupModule,
+        DxScrollViewModule,
         MatDialogModule,
         ClipboardModule
     ],
