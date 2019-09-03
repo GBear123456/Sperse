6afe3facdb689c6b91ad09d676dd622628686937
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-09-03 16:12:18 +0300

    [SC-8272] changed features strings to enum in admin.config.ts

--- src/app/admin/admin.config.ts
+++ src/app/admin/admin.config.ts
@@ -10,15 +10,15 @@ export class AdminConfig {
     localizationSource = 'Platform';
     navigation = [
         [ 'Dashboard', AppPermissions.AdministrationHostDashboard, 'icon-home', '/app/admin/hostDashboard' ],
-        [ 'Tenants', AppPermissions.Tenants, 'icon-globe', '/app/admin/tenants', 'Admin.Advanced' ],
+        [ 'Tenants', AppPermissions.Tenants, 'icon-globe', '/app/admin/tenants', AppFeatures.AdminAdvanced ],
         [ 'Roles', AppPermissions.AdministrationRoles, 'icon-briefcase', '/app/admin/roles', '' ],
         [ 'Users', AppPermissions.AdministrationUsers, 'icon-people', '/app/admin/users', '' ],
-        [ 'Languages', AppPermissions.AdministrationLanguages, 'icon-flag', '/app/admin/languages', 'Admin.Customizations' ],
-        [ 'AuditLogs', AppPermissions.AdministrationAuditLogs, 'icon-lock', '/app/admin/auditLogs', 'Admin.Advanced' ],
-        [ 'Maintenance', AppPermissions.AdministrationHostMaintenance, 'icon-wrench', '/app/admin/maintenance', 'Admin.Advanced' ],
-        [ 'Jobs', AppPermissions.AdministrationHangfireDashboard, 'icon-wrench', '/app/admin/jobs', 'Admin.Advanced' ],
-        [ 'Settings', AppPermissions.AdministrationHostSettings + '|' + AppPermissions.AdministrationTenantHosts, 'icon-settings', '/app/admin/hostSettings', 'Admin', '', '', 'host' ],
-        [ 'Settings', AppPermissions.AdministrationTenantSettings + '|' + AppPermissions.AdministrationTenantHosts, 'icon-settings', '/app/admin/tenantSettings', 'Admin', '', '', 'tenant' ],
+        [ 'Languages', AppPermissions.AdministrationLanguages, 'icon-flag', '/app/admin/languages', AppFeatures.AdminCustomizations ],
+        [ 'AuditLogs', AppPermissions.AdministrationAuditLogs, 'icon-lock', '/app/admin/auditLogs', AppFeatures.AdminAdvanced ],
+        [ 'Maintenance', AppPermissions.AdministrationHostMaintenance, 'icon-wrench', '/app/admin/maintenance', AppFeatures.AdminAdvanced ],
+        [ 'Jobs', AppPermissions.AdministrationHangfireDashboard, 'icon-wrench', '/app/admin/jobs', AppFeatures.AdminAdvanced ],
+        [ 'Settings', AppPermissions.AdministrationHostSettings + '|' + AppPermissions.AdministrationTenantHosts, 'icon-settings', '/app/admin/hostSettings', AppFeatures.Admin, '', '', 'host' ],
+        [ 'Settings', AppPermissions.AdministrationTenantSettings + '|' + AppPermissions.AdministrationTenantHosts, 'icon-settings', '/app/admin/tenantSettings', AppFeatures.Admin, '', '', 'tenant' ],
         [ 'Products', AppPermissions.Editions, 'icon-grid', '/app/admin/products' ]
     ];
 }
