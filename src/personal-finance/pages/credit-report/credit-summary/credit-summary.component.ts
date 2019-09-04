3b2c3e04f6d8ffd4d101f147d0b060a73c6c684c
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2019-09-04 15:13:58 +0300

    [SC-7522] Added detecting changes on push on some CR sections

--- src/personal-finance/pages/credit-report/credit-summary/credit-summary.component.ts
+++ src/personal-finance/pages/credit-report/credit-summary/credit-summary.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, Input, Injector } from '@angular/core';
+import { Component, OnInit, Input, Injector, ChangeDetectionStrategy } from '@angular/core';
 import { AppComponentBase } from '@shared/common/app-component-base';
 
 @Component({
     selector: 'app-credit-summary',
     templateUrl: './credit-summary.component.html',
-    styleUrls: ['./credit-summary.component.less']
+    styleUrls: ['./credit-summary.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreditSummaryComponent extends AppComponentBase implements OnInit {
     @Input() creditReport;
