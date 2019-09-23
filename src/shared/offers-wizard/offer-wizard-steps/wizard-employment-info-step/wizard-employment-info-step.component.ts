42dc7f7d20d77495ecbd86e46bb6f5df5e48cdba
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-09-23 19:52:46 +0300

    [SC-9066] added postal code handling to employment info step

--- src/shared/offers-wizard/offer-wizard-steps/wizard-employment-info-step/wizard-employment-info-step.component.ts
+++ src/shared/offers-wizard/offer-wizard-steps/wizard-employment-info-step/wizard-employment-info-step.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { InputStatusesService } from '@shared/utils/input-statuses.service';
 import { OffersWizardService } from '@shared/offers-wizard/offers-wizard.service';
+import { AppConsts } from '@shared/AppConsts';
+import { MaskPipe } from 'ngx-mask';
 
 @Component({
     selector: 'app-wizard-employment-info-step',
@@ -9,15 +11,28 @@ import { OffersWizardService } from '@shared/offers-wizard/offers-wizard.service
     styleUrls: ['./wizard-employment-info-step.component.less']
 })
 export class WizardEmploymentInfoStepComponent {
+    employerPostalCode = this.maskPipe.transform(
+        this.offersWizardService.submitApplicationProfileInput.employmentInformation.employerPostalCode,
+        AppConsts.masks.zipCodeLong
+    );
     phoneWork: string = this.offersWizardService.submitApplicationProfileInput.employmentInformation.phoneWork;
     constructor(
         public ls: AppLocalizationService,
         public inputStatusesService: InputStatusesService,
-        public offersWizardService: OffersWizardService
+        public offersWizardService: OffersWizardService,
+        private maskPipe: MaskPipe
     ) {}
 
     mobileChange(e) {
         this.offersWizardService.submitApplicationProfileInput.employmentInformation.phoneWork = this.offersWizardService.clearPhoneMask(e.value);
+    }
+
+    onPostalCodeInput(e) {
+        e.event.target.value = this.offersWizardService.transformPostalCode(e.event.target);
+    }
+
+    postalCodeChange(e) {
+        this.offersWizardService.submitApplicationProfileInput.employmentInformation.employerPostalCode = e.value.replace('-', '');
     }
 
 }
