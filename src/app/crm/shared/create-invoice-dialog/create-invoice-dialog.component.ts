bb3f12e873ad3795854a2df724dcef107fecd271
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-10-17 11:16:45 +0300

    fixed build error

--- src/app/crm/shared/create-invoice-dialog/create-invoice-dialog.component.ts
+++ src/app/crm/shared/create-invoice-dialog/create-invoice-dialog.component.ts
@@ -19,8 +19,17 @@ import { DateHelper } from '@shared/helpers/DateHelper';
 import { DialogService } from '@app/shared/common/dialogs/dialog.service';
 import { ContactGroup } from '@shared/AppEnums';
 import {
-    InvoiceServiceProxy, CreateInvoiceInput, UpdateInvoiceLineInput, UpdateInvoiceStatusInput, UpdateInvoiceInput, CustomerServiceProxy, InvoiceStatus,
-    CreateInvoiceLineInput, InvoiceSettingsInfoDto, CurrencyInfo
+    InvoiceServiceProxy,
+    CreateInvoiceInput,
+    UpdateInvoiceLineInput,
+    UpdateInvoiceStatusInput,
+    UpdateInvoiceInput,
+    CustomerServiceProxy,
+    InvoiceStatus,
+    CreateInvoiceLineInput,
+    InvoiceSettingsInfoDto,
+    CurrencyInfo,
+    InvoiceLineUnit
 } from '@shared/service-proxies/service-proxies';
 import { NotifyService } from '@abp/notify/notify.service';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
@@ -197,8 +206,9 @@ export class CreateInvoiceDialogComponent implements OnInit {
 
     saveOptionsInit() {
         let cacheKey = this._cacheHelper.getCacheKey(this.SAVE_OPTION_CACHE_KEY, this.constructor.name),
-            selectedOption = this.saveContextMenuItems[this._cacheService.exists(cacheKey) ? 
-                this._cacheService.get(cacheKey) : this.SAVE_OPTION_DEFAULT];
+            selectedOption = this.saveContextMenuItems[this._cacheService.exists(cacheKey)
+                ? this._cacheService.get(cacheKey)
+                : this.SAVE_OPTION_DEFAULT];
         selectedOption.selected = true;
         this.buttons[0].title = selectedOption.text;
         this.status = selectedOption.status;
@@ -251,7 +261,7 @@ export class CreateInvoiceDialogComponent implements OnInit {
                     id: row['id'],
                     quantity: row['Quantity'],
                     rate: row['Rate'],
-                    unitId: 'UT',
+                    unitId: InvoiceLineUnit.Unit,
                     description: row['Description'],
                     sortOrder: index
                 });
@@ -267,7 +277,7 @@ export class CreateInvoiceDialogComponent implements OnInit {
                 return new CreateInvoiceLineInput({
                     quantity: row['Quantity'],
                     rate: row['Rate'],
-                    unitId: 'UT',
+                    unitId: InvoiceLineUnit.Unit,
                     description: row['Description'],
                     sortOrder: index
                 });
@@ -475,7 +485,7 @@ export class CreateInvoiceDialogComponent implements OnInit {
         }).afterClosed().subscribe((data) => {
             if (data) {
                 this.contactId = data.id;
-                this.customer = [data.firstName, data.middleName, 
+                this.customer = [data.firstName, data.middleName,
                     data.lastName].filter(Boolean).join(' ');
                 this._changeDetectorRef.detectChanges();
             }
