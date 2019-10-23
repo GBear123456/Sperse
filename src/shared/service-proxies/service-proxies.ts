7a37baebea92c989b250ddaac4f3e52984432192
Yulia Shkliarenko <ys@sperse.com>
2019-10-23 18:01:43 +0300

    [SC-9131] Added Login as admin impersonation option

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -519,6 +519,61 @@ export class AccountServiceProxy {
     }
 
     /**
+     * @tenantId (optional) 
+     * @return Success
+     */
+    impersonateAsAdmin(tenantId: number | null | undefined): Observable<ImpersonateOutput> {
+        let url_ = this.baseUrl + "/api/services/Platform/Account/ImpersonateAsAdmin?";
+        if (tenantId !== undefined)
+            url_ += "tenantId=" + encodeURIComponent("" + tenantId) + "&"; 
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processImpersonateAsAdmin(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processImpersonateAsAdmin(<any>response_);
+                } catch (e) {
+                    return <Observable<ImpersonateOutput>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<ImpersonateOutput>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processImpersonateAsAdmin(response: HttpResponseBase): Observable<ImpersonateOutput> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = resultData200 ? ImpersonateOutput.fromJS(resultData200) : new ImpersonateOutput();
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<ImpersonateOutput>(<any>null);
+    }
+
+    /**
      * @return Success
      */
     backToImpersonator(): Observable<ImpersonateOutput> {
