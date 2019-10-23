dfead8f166b85db42091e4d9d6c25befbf50c3be
Oleh Stakhiv <veishen19@gmail.com>
2019-10-23 22:01:31 +0300

    [SC-9447] returned full name validation for all cases for create-client dialog

--- src/app/crm/shared/create-client-dialog/create-client-dialog.component.ts
+++ src/app/crm/shared/create-client-dialog/create-client-dialog.component.ts
@@ -318,10 +318,7 @@ export class CreateClientDialogComponent implements OnInit, OnDestroy {
             return this.notifyService.error(this.ls.l('NameFieldsValidationError'));
         }
 
-        /** Do not allow empty full name for lead creation and allow for contact */
-        if ((this.data.isInLeadMode || this.title)
-             && !ValidationHelper.ValidateName(this.title)
-        ) {
+        if (!ValidationHelper.ValidateName(this.title)) {
             this.isTitleValid = false;
             return this.notifyService.error(this.ls.l('FullNameIsNotValid'));
         }
