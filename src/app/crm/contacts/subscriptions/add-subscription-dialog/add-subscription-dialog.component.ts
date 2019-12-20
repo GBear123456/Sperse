98a0208d83c52bc375d9a16561fc9b36e47f969f
Oleh Stakhiv <veishen19@gmail.com>
2019-12-20 11:34:44 +0200

    [SC-10069] removed unused imports, fixed build error

--- src/app/crm/contacts/subscriptions/add-subscription-dialog/add-subscription-dialog.component.ts
+++ src/app/crm/contacts/subscriptions/add-subscription-dialog/add-subscription-dialog.component.ts
@@ -31,7 +31,7 @@ import { UserManagementService } from '@shared/common/layout/user-management-lis
 import { BankCodeServiceType } from '@root/bank-code/products/bank-code-service-type.enum';
 import { InvoicesService } from '@app/crm/contacts/invoices/invoices.service';
 import { DateHelper } from '@shared/helpers/DateHelper';
-import { CurrencyPipe, getCurrencySymbol } from '@angular/common';
+import { getCurrencySymbol } from '@angular/common';
 
 @Component({
     selector: 'add-subscription-dialog',
@@ -41,7 +41,6 @@ import { CurrencyPipe, getCurrencySymbol } from '@angular/common';
         '../../../../shared/common/styles/form.less',
         './add-subscription-dialog.component.less'
     ],
-    providers: [ CurrencyPipe ],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AddSubscriptionDialogComponent implements AfterViewInit, OnInit {
@@ -74,10 +73,9 @@ export class AddSubscriptionDialogComponent implements AfterViewInit, OnInit {
         private contactsService: ContactsService,
         private userManagementService: UserManagementService,
         private invoicesService: InvoicesService,
-        private currencyPipe: CurrencyPipe,
         public dialogRef: MatDialogRef<AddSubscriptionDialogComponent>,
         public ls: AppLocalizationService,
-        @Inject(MAT_DIALOG_DATA) private data: any
+        @Inject(MAT_DIALOG_DATA) public data: any
     ) {
         this.dialogRef.beforeClose().subscribe(() => {
             this.dialogRef.updatePosition({
