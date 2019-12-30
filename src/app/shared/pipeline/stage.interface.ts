bfe030de5c9408629e2a7799f719e16c63125e0e
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2019-12-30 13:30:21 +0200

    [SC-10168] decreased narrow columns width, added getting an old width after refresh

--- src/app/shared/pipeline/stage.interface.ts
+++ src/app/shared/pipeline/stage.interface.ts
@@ -1,4 +1,5 @@
 import { IStageDto } from '@shared/service-proxies/service-proxies';
+import { StageWidth } from '@app/shared/pipeline/stage-width.enum';
 
 export interface IStage extends IStageDto {
     entities: any[];
@@ -7,4 +8,5 @@ export interface IStage extends IStageDto {
     stageIndex: number;
     total: number;
     lastStageIndex: number;
+    width: StageWidth;
 }
