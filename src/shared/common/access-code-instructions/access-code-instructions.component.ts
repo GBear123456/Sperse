21469bbf06a55ead190f65b37199bd89012fa95c
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2020-01-09 17:44:57 +0200

    [SC-10316] updated tracking and affiliate links for access code instructions

--- src/shared/common/access-code-instructions/access-code-instructions.component.ts
+++ src/shared/common/access-code-instructions/access-code-instructions.component.ts
@@ -10,6 +10,7 @@ import { ClipboardService } from 'ngx-clipboard';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { ProfileService } from '@shared/common/profile-service/profile.service';
 import { NotifyService } from '@abp/notify/notify.service';
+import { environment } from '@root/environments/environment';
 
 @Component({
     selector: 'access-code-instructions',
@@ -21,10 +22,10 @@ import { NotifyService } from '@abp/notify/notify.service';
 export class AccessCodeInstructionsComponent {
     accessCode$: Observable<string> = this.profileService.accessCode$;
     trackingLink$: Observable<string> = this.accessCode$.pipe(
-        map((accessCode: string) => 'https://www.CrackMyCode.com/?ref=' + accessCode)
+        map((accessCode: string) => (environment.production ? 'https://www.CrackMyCode.com/' : 'https://bankpass.bankcode.com/') + accessCode)
     );
     affiliateLink$: Observable<string> = this.accessCode$.pipe(
-        map((accessCode: string) => 'https://www.CodebreakerTech.com/?ref=' + accessCode)
+        map((accessCode: string) => 'https://www.CodebreakerTech.com/' + accessCode)
     );
     constructor(
         private profileService: ProfileService,
