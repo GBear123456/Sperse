81dbc9e2d1e13698bf91cc5223ddca29cf1a545b
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2020-01-14 09:59:32 +0200

    [SC-10336] updated values at a glance on bank code home page

--- src/bank-code/dashboard/counters/counters.component.ts
+++ src/bank-code/dashboard/counters/counters.component.ts
@@ -1,12 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {AppLocalizationService} from '@app/shared/common/localization/app-localization.service';
+import { Component } from '@angular/core';
+import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 
 @Component({
     selector: 'bank-code-counters',
     templateUrl: './counters.component.html',
     styleUrls: ['./counters.component.less']
 })
-export class CountersComponent implements OnInit {
+export class CountersComponent {
     myCodesCracked = [
         { percent: 90, outerColor: '#004a81', innerColor: '#91bfdd', title: '90%', subtitle: '4230' },
         { percent: 40, outerColor: '#ac1f22', innerColor: '#ce767f', title: '40%', subtitle: '930' },
@@ -14,15 +14,11 @@ export class CountersComponent implements OnInit {
         { percent: 72, outerColor: '#1b6634', innerColor: '#87c796', title: '72%', subtitle: '4230' }
     ];
     codesCrackedAtAGlance = [
-        { percent: 85, outerColor: '#004a81', innerColor: '#91bfdd', title: '85%', subtitle: '4230' },
-        { percent: 63, outerColor: '#ac1f22', innerColor: '#ce767f', title: '63%', subtitle: '455' },
-        { percent: 77, outerColor: '#f09e1f', innerColor: '#ecd68a', title: '77%', subtitle: '6335' },
-        { percent: 57, outerColor: '#1b6634', innerColor: '#87c796', title: '57%', subtitle: '7433' }
+        { percent: 20.7, outerColor: '#004a81', innerColor: '#91bfdd', title: '20.7%', subtitle: '26408' },
+        { percent: 26.6, outerColor: '#ac1f22', innerColor: '#ce767f', title: '26.6%', subtitle: '33932' },
+        { percent: 36.1, outerColor: '#f09e1f', innerColor: '#ecd68a', title: '36.1%', subtitle: '46087' },
+        { percent: 16.5, outerColor: '#1b6634', innerColor: '#87c796', title: '16.5%', subtitle: '21067' }
     ];
-    constructor(public ls: AppLocalizationService) {
-    }
-
-    ngOnInit() {
-    }
+    constructor(public ls: AppLocalizationService) {}
 
 }
--- src/bank-code/dashboard/dashboard.component.ts
+++ src/bank-code/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 
 @Component({
