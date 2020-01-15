ab10a703df8807cbdef01bc2e30dd974f1053ffc
Oleh Stakhiv <oleg.stakhiv@sperse.com>
2020-01-15 18:03:35 +0200

    [SC-10293] removed unused variables

--- src/bank-code/products/bank-affiliate/bank-affiliate.component.ts
+++ src/bank-code/products/bank-affiliate/bank-affiliate.component.ts
@@ -22,7 +22,7 @@ import { ProductsService } from '@root/bank-code/products/products.service';
 export class BankAffiliateComponent implements AfterViewInit {
     dataIsLoading = true;
     hasSubscription$: Observable<boolean> = this.profileService.checkServiceSubscription(BankCodeServiceType.BANKAffiliate).pipe(
-        tap((hasSubscription) => setTimeout(() => {
+        tap(() => setTimeout(() => {
             this.changeDetectorRef.detectChanges();
         }))
     );
