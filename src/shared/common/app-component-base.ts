17099d131aa84ec0c6f2e08416d369b8581a1279
Volodymyr Peren <vp@sperse.com>
2020-03-05 12:32:40 +0200

    [SC-10921] - Hide Toolbar options for AdvicePeriod layout

--- src/shared/common/app-component-base.ts
+++ src/shared/common/app-component-base.ts
@@ -33,6 +33,7 @@ import { FullScreenService } from '@shared/common/fullscreen/fullscreen.service'
 import { FilterModel } from '@shared/filters/models/filter.model';
 import { TitleService } from '@shared/common/title/title.service';
 import { DataGridService } from '@app/shared/common/data-grid.service/data-grid.service';
+import { LayoutType } from '@shared/service-proxies/service-proxies';
 import { InstanceModel } from '@shared/cfo/instance.model';
 
 export abstract class AppComponentBase implements OnDestroy {
@@ -89,6 +90,7 @@ export abstract class AppComponentBase implements OnDestroy {
     public cacheHelper: CacheHelper;
     public loadingService: LoadingService;
     public defaultGridPagerConfig = DataGridService.defaultGridPagerConfig;
+    public showReload = true;
 
     constructor(
         private _injector: Injector
@@ -121,6 +123,7 @@ export abstract class AppComponentBase implements OnDestroy {
             .subscribe((isFullScreenMode: boolean) => {
                 this.isFullscreenMode = isFullScreenMode;
             });
+        this.showReload = this.appSession.tenant.customLayoutType != LayoutType.AdvicePeriod;
     }
 
     getRootComponent() {
