2fd7937324379caf009e10d26488fcf7b4f949db
Volodymyr Peren <vp@sperse.com>
2020-04-29 20:46:26 +0300

    [SC-11329] - Applied testing comments

--- src/app/shared/common/bank-code-letters/bank-code-decode/bank-code-decode.component.ts
+++ src/app/shared/common/bank-code-letters/bank-code-decode/bank-code-decode.component.ts
@@ -4,6 +4,9 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 /** Third party imports */
 
 /** Application imports */
+import { AppFeatures } from '@shared/AppFeatures';
+import { FeatureCheckerService } from '@abp/features/feature-checker.service';
+import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { ExternalServiceProxy, GetBankCodeInput, Dimensions } from '@shared/service-proxies/service-proxies';
 import { BankCodeLetter } from '@app/shared/common/bank-code-letters/bank-code-letter.enum';
 import { BankCodeService } from '@app/shared/common/bank-code/bank-code.service';
@@ -20,12 +23,15 @@ export class BankCodeDecodeComponent {
     @Input() content: string;
     @Output() onDecodeStart: EventEmitter<any> = new EventEmitter<any>();
     @Output() onDecodeFinish: EventEmitter<any> = new EventEmitter<any>();
+    bankCodeEnabled = this.features.isEnabled(AppFeatures.CRMBANKCode);
     bankCode: string;
 
     constructor(
         private notify: NotifyService,
         private externalProxy: ExternalServiceProxy,
-        public bankCodeService: BankCodeService
+        public features: FeatureCheckerService,
+        public bankCodeService: BankCodeService,
+        public ls: AppLocalizationService
     ) {}
 
     decode(event) {
@@ -40,6 +46,6 @@ export class BankCodeDecodeComponent {
                 this.onDecodeFinish.emit(error);
             });
         } else
-            return this.notify.warn('Content should be defined');
+            return this.notify.warn(this.ls.l('RequiredField', this.ls.l('Message')));
     }
 }
