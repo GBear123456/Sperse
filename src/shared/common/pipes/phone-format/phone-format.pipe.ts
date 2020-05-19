8a48a399ccff8cc8b9abb4fb141f17530d67eac6
Volodymyr Peren <vp@sperse.com>
2020-05-19 16:44:54 +0300

    [SC-11550] - Unknown country: AN console error on Export / Import

--- src/shared/common/pipes/phone-format/phone-format.pipe.ts
+++ src/shared/common/pipes/phone-format/phone-format.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { parse, formatNumber, ParsedNumber } from 'libphonenumber-js';
+import { parse, formatNumber, ParsedNumber, ParseNumberOptions } from 'libphonenumber-js';
 import { AppConsts } from '@shared/AppConsts';
 
 @Pipe({
@@ -9,11 +9,16 @@ export class PhoneFormatPipe implements PipeTransform {
     transform(value: string, parseOptions: any = {}): any {
         let phoneNumber = value;
         if (value) {
-            const defaultParseOptions = {
+            const defaultParseOptions: ParseNumberOptions = {
                 extended: true,
                 defaultCountry: AppConsts.defaultCountry
             };
-            const parsedNumber: ParsedNumber = parse(value, {...defaultParseOptions, ...parseOptions});
+            let parsedNumber: ParsedNumber;
+            try {
+                parsedNumber = parse(value, {...defaultParseOptions, ...parseOptions});
+            } catch (error) {
+                parsedNumber = parse(value);
+            }
             if (Object.keys(parsedNumber).length) {
                 if (parsedNumber.countryCallingCode === '1') {
                     phoneNumber = `+${parsedNumber.countryCallingCode} ${formatNumber(parsedNumber, 'National')}`;
