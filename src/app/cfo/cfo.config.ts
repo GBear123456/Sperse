e2d12cca0d461bab2eaa0262f4eb4a4cbc155892
Oleh Stakhiv <veishen19@gmail.com>
2020-05-20 20:17:10 +0300

    [SC-11409] removed dx icons from navigation

--- src/app/cfo/cfo.config.ts
+++ src/app/cfo/cfo.config.ts
@@ -12,38 +12,31 @@ export class CfoConfig implements ConfigInterface {
     localizationSource = 'CFO';
     navigation: ConfigNavigation[] = [
         {
-            icon: 'icon-home',
             route: '/app/cfo/:instance/start',
             alterRoutes: ['/app/cfo/:instance/business-entities', '/app/cfo/:instance/chart-of-accounts']
         },
         {
             text: 'Accounts',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/linkaccounts'
         },
         {
             text: 'Transactions',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/transactions'
         },
         {
             text: 'Cashflow',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/cashflow'
         },
         {
             text: 'Stats',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/stats'
         },
         {
             text: 'Statements',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/statements'
         },
         {
             text: 'Reports',
-            icon: 'icon-home',
             route: '/app/cfo/:instance/reports'
         }
     ];
