2096a2ec1d865b418c8292eb1f7c57034b008236
Oleh Stakhiv <os3@sperse.com>
2020-06-04 14:38:44 +0300

    [SC-11713] added filtering by SourceId to ContactSlice odata, made counter component dynamic, distinguish requests with source contact id and without

--- src/app/shared/common/bank-code/bank-code.service.ts
+++ src/app/shared/common/bank-code/bank-code.service.ts
@@ -12,83 +12,18 @@ import values from 'lodash/values';
 /** Application imports */
 import { BankCodeLetter } from '@app/shared/common/bank-code-letters/bank-code-letter.enum';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
-import { BankCodeGroup } from '@root/bank-code/products/bank-pass/bank-code-group.interface';
-import { ContactGroup, ContactStatus } from '@shared/AppEnums';
+import { ContactGroup } from '@shared/AppEnums';
 import { AppConsts } from '@shared/AppConsts';
 import { BankCodeTime } from '@app/shared/common/bank-code/bank-code-time.enum';
 import { GoalType } from '@app/shared/common/bank-code/goal-type.interface';
+import { FiltersService } from '@shared/filters/filters.service';
+import { BankCodeCount } from './bank-code-count.interface';
+import { BankCodeGroup } from "@root/bank-code/products/bank-pass/bank-code-group.interface";
+import { AppSessionService } from "@shared/common/session/app-session.service";
 
 @Injectable()
 export class BankCodeService {
     bankCodeBadges: number[] = [ 50, 100, 250, 500, 1000, 2500, 5000, 10000, 25000, 50000 ];
-    bankCodeClientsCount$: Observable<number> = this.getClientsBankCodesTotalCount().pipe(
-        startWith(0)
-    );
-    bankCodeLevel$: Observable<number> = this.bankCodeClientsCount$.pipe(
-        map((currentBankCodeClientsCount: number) => {
-            let level: number;
-            if (currentBankCodeClientsCount >= this.bankCodeBadges[this.bankCodeBadges.length - 1]) {
-                level = 10;
-            } else {
-                level = this.bankCodeBadges.findIndex((bankCodeBadgeCount: number) => {
-                    return currentBankCodeClientsCount < bankCodeBadgeCount;
-                });
-            }
-            return level;
-        }),
-        publishReplay(),
-        refCount()
-    );
-    goalTypes: GoalType[] = [
-        {
-            name: 'daily',
-            text: this.ls.l('Daily'),
-            number: 3,
-            currentNumber$: this.getBankCodesNumber(BankCodeTime.Daily),
-            innerColor: '#91bfdd',
-            outerColor: '#004a81'
-        },
-        {
-            name: 'weekly',
-            text: this.ls.l('Weekly'),
-            number: 20,
-            currentNumber$: this.getBankCodesNumber(BankCodeTime.Weekly),
-            innerColor: '#ce767f',
-            outerColor: '#ac1f22'
-        },
-        {
-            name: 'monthly',
-            text: this.ls.l('Monthly'),
-            number: 90,
-            currentNumber$: this.getBankCodesNumber(BankCodeTime.Monthly),
-            innerColor: '#ecd68a',
-            outerColor: '#f09e1e'
-        },
-        {
-            name: 'quarterly',
-            text: this.ls.l('Quarterly'),
-            number: 250,
-            currentNumber$: this.getBankCodesNumber(BankCodeTime.Quarterly),
-            innerColor: '#87c796',
-            outerColor: '#1b6634'
-        },
-        {
-            name: 'annual',
-            text: this.ls.l('Annual'),
-            number: 1000,
-            currentNumber$: this.getBankCodesNumber(BankCodeTime.Annual),
-            innerColor: '#c8c0e1',
-            outerColor: '#004a81'
-        },
-        {
-            name: 'lifetime',
-            text: this.ls.l('Lifetime'),
-            number: 25000,
-            currentNumber$: this.bankCodeClientsCount$,
-            innerColor: '#ddbcdb',
-            outerColor: '#b142ab'
-        }
-    ];
     bankCodeConfig = {
         [BankCodeLetter.A]: {
             name: 'action',
@@ -118,44 +53,132 @@ export class BankCodeService {
     readonly emptyBankCode = '????';
     reportsLink = 'https://www.codebreakertech.com/reports';
     partnerCode = location.href.indexOf('successfactory.com') >= 0 ? 'SF' : 'CB';
-    bankCodeGroupsCounts$ = this.getAvailableBankCodes().pipe(
-        map((bankCodes: { [bankCode: string]: number }) => {
-            let bankCodeGroups = {
-                'B': 0,
-                'A': 0,
-                'N': 0,
-                'K': 0
-            };
-            for (let bankCode in bankCodes) {
-                bankCodeGroups[bankCode[0]] += bankCodes[bankCode];
-            }
-            return values(bankCodeGroups);
-        })
+
+    /** ContactSlice request for current contact */
+    contactClientsBankCodeData$: Observable<any> = this.getClientsBankCodesData(this.getSourceFilters());
+    /** ContactSlice request for all contacts */
+    allContactClientsBankCodeData$: Observable<any> = this.getClientsBankCodesData();
+
+    /** Number of clients that current contact cracked */
+    contactClientsBankCodesTotalCount$: Observable<number> = this.getClientsBankCodesTotalCount(this.contactClientsBankCodeData$);
+    /** Number of all cracked bank codes */
+    allClientsBankCodesTotalCount$: Observable<number> = this.getClientsBankCodesTotalCount(this.allContactClientsBankCodeData$);
+
+    contactBankCodeClientsCount$: Observable<number> = this.contactClientsBankCodesTotalCount$.pipe(startWith(0));
+    allBankCodeClientsCount$: Observable<number> = this.allClientsBankCodesTotalCount$.pipe(startWith(0));
+
+    contactClientsBankCodes$: Observable<BankCodeGroup[]> = this.contactClientsBankCodeData$.pipe(
+        map((result: any) => result && result.data)
     );
-    bankCodesGroupsCountsWithPercents$ = this.bankCodeGroupsCounts$.pipe(
-        withLatestFrom(this.bankCodeClientsCount$),
-        map(([bankCodeGroupsCounts, total]: [number[], number]) => {
-            return bankCodeGroupsCounts.map((groupCount: number) => ({
-                percent: total ? groupCount / total * 100 : 0,
-                count: groupCount
-            }));
-        })
+    allClientsBankCodes$: Observable<BankCodeGroup[]> = this.allContactClientsBankCodeData$.pipe(
+        map((result: any) => result && result.data)
     );
-    bankCodeTotalCount$: Observable<string> = this.bankCodeClientsCount$.pipe(
+
+    contactAvailableBankCodes$: Observable<BankCodeCount> = this.getAvailableBankCodes(this.contactClientsBankCodes$);
+    allAvailableBankCodes$: Observable<BankCodeCount> = this.getAvailableBankCodes(this.allClientsBankCodes$);
+
+    contactBankCodeGroupsCounts$ = this.getBankCodeGroupsCounts$(this.contactAvailableBankCodes$);
+    allContactBankCodeGroupsCounts$ = this.getBankCodeGroupsCounts$(this.allAvailableBankCodes$);
+
+    contactBankCodesGroupsCountsWithPercents$ = this.getBankCodesGroupsCountsWithPercents(this.contactBankCodeGroupsCounts$, this.contactBankCodeClientsCount$);
+    allBankCodesGroupsCountsWithPercents$ = this.getBankCodesGroupsCountsWithPercents(this.allContactBankCodeGroupsCounts$, this.allBankCodeClientsCount$);
+
+    contactBankCodeTotalCount$: Observable<string> = this.contactBankCodeClientsCount$.pipe(
+        map((count) => count.toString())
+    );
+    allBankCodeTotalCount$: Observable<string> = this.allBankCodeClientsCount$.pipe(
         map((count) => count.toString())
     );
 
-    static getCustomerFilters() {
-        return [
-            { 'GroupId': { 'eq': ContactGroup.Client }},
-            { 'StatusId': { 'eq': ContactStatus.Active }}
-        ];
-    }
+    goalTypes: GoalType[] = [
+        {
+            name: 'daily',
+            text: this.ls.l('Daily'),
+            number: 3,
+            currentNumber$: this.getContactBankCodesNumber(BankCodeTime.Daily),
+            innerColor: '#91bfdd',
+            outerColor: '#004a81'
+        },
+        {
+            name: 'weekly',
+            text: this.ls.l('Weekly'),
+            number: 20,
+            currentNumber$: this.getContactBankCodesNumber(BankCodeTime.Weekly),
+            innerColor: '#ce767f',
+            outerColor: '#ac1f22'
+        },
+        {
+            name: 'monthly',
+            text: this.ls.l('Monthly'),
+            number: 90,
+            currentNumber$: this.getContactBankCodesNumber(BankCodeTime.Monthly),
+            innerColor: '#ecd68a',
+            outerColor: '#f09e1e'
+        },
+        {
+            name: 'quarterly',
+            text: this.ls.l('Quarterly'),
+            number: 250,
+            currentNumber$: this.getContactBankCodesNumber(BankCodeTime.Quarterly),
+            innerColor: '#87c796',
+            outerColor: '#1b6634'
+        },
+        {
+            name: 'annual',
+            text: this.ls.l('Annual'),
+            number: 1000,
+            currentNumber$: this.getContactBankCodesNumber(BankCodeTime.Annual),
+            innerColor: '#c8c0e1',
+            outerColor: '#004a81'
+        },
+        {
+            name: 'lifetime',
+            text: this.ls.l('Lifetime'),
+            number: 25000,
+            currentNumber$: this.contactBankCodeClientsCount$,
+            innerColor: '#ddbcdb',
+            outerColor: '#b142ab'
+        }
+    ];
+    bankCodeLevel$: Observable<number> = this.contactBankCodeClientsCount$.pipe(
+        map((currentBankCodeClientsCount: number) => {
+            let level: number;
+            if (currentBankCodeClientsCount >= this.bankCodeBadges[this.bankCodeBadges.length - 1]) {
+                level = 10;
+            } else {
+                level = this.bankCodeBadges.findIndex((bankCodeBadgeCount: number) => {
+                    return currentBankCodeClientsCount < bankCodeBadgeCount;
+                });
+            }
+            return level;
+        }),
+        publishReplay(),
+        refCount()
+    );
 
     constructor(
         private ls: AppLocalizationService,
-        private httpClient: HttpClient
+        private httpClient: HttpClient,
+        private appSessionService: AppSessionService
     ) {}
+
+    getSourceFilters() {
+        return this.appSessionService.user
+            ? [{ 'SourceContactId ': { 'eq': this.appSessionService.user.contactId }}]
+            : [];
+    }
+
+    private static getFilterFromTime(time: BankCodeTime) {
+        const currentDate = moment();
+        return [
+            {
+                ContactDate: {
+                    ge: currentDate.startOf(time.toString()).toDate(),
+                    le: currentDate.endOf(time.toString()).toDate()
+                }
+            }
+        ];
+    }
 
     getBankCodeReportLink(
         languageCode: string,
@@ -167,14 +190,60 @@ export class BankCodeService {
         return this.reportsLink + '/' + this.partnerCode + '/' + languageCode + '/' + reportsFolder + '/' + bankCode + '-' + reportType.toUpperCase() + '-REPORT' + resolution + '.pdf';
     }
 
-    getColorsByLetter(bankCodeLetter: BankCodeLetter) {
+    private getAvailableBankCodes(clientsBankCodesGroups$: Observable<BankCodeGroup[]>): Observable<BankCodeCount> {
+        return clientsBankCodesGroups$.pipe(
+            map((bankCodeGroups: BankCodeGroup[]) => {
+                let availableBankCodes = {};
+                bankCodeGroups.forEach((bankCodeGroup: BankCodeGroup) => {
+                    availableBankCodes[bankCodeGroup.key] = bankCodeGroup.count;
+                });
+                return availableBankCodes;
+            }),
+            publishReplay(),
+            refCount()
+        );
+    }
+
+    private getBankCodeGroupsCounts$(availableBankCodes$: Observable<BankCodeCount>): Observable<number[]> {
+        return availableBankCodes$.pipe(
+            map((bankCodes: BankCodeCount) => {
+                let bankCodeGroups = {
+                    'B': 0,
+                    'A': 0,
+                    'N': 0,
+                    'K': 0
+                };
+                for (let bankCode in bankCodes) {
+                    bankCodeGroups[bankCode[0]] += bankCodes[bankCode];
+                }
+                return values(bankCodeGroups);
+            })
+        );
+    }
+
+    private getBankCodesGroupsCountsWithPercents(
+        bankCodeGroupsCounts$: Observable<number[]>,
+        bankCodeClienstCount$: Observable<number>
+    ): Observable<{ percent: number, count: number }[]> {
+        return bankCodeGroupsCounts$.pipe(
+            withLatestFrom(bankCodeClienstCount$),
+            map(([bankCodeGroupsCounts, total]: [number[], number]) => {
+                return bankCodeGroupsCounts.map((groupCount: number) => ({
+                    percent: total ? groupCount / total * 100 : 0,
+                    count: groupCount
+                }));
+            })
+        );
+    }
+
+    getColorsByLetter(bankCodeLetter: BankCodeLetter): { color: string, background: string } {
         const colors = this.bankCodeConfig[bankCodeLetter];
         return colors
             ? { color: colors.color, background: colors.background }
             : null;
     }
 
-    getBackgroundColorByLetter(bankCodeLetter: BankCodeLetter) {
+    getBackgroundColorByLetter(bankCodeLetter: BankCodeLetter): string {
         return this.bankCodeConfig[bankCodeLetter] && this.bankCodeConfig[bankCodeLetter].background;
     }
 
@@ -190,7 +259,7 @@ export class BankCodeService {
         filters.push(
             { 'BankCode': { 'ne': null }},
             { 'BankCode': { 'ne': '' }},
-            ...BankCodeService.getCustomerFilters()
+            ...FiltersService.getCustomerFilters()
         );
         let filter = buildQuery({
             filter: filters
@@ -206,38 +275,20 @@ export class BankCodeService {
         );
     }
 
-    getClientsBankCodes(filters = []): Observable<BankCodeGroup[]> {
-        return this.getClientsBankCodesData(filters).pipe(
-            map((result: any) => result && result.data)
-        );
-    }
-
-    getClientsBankCodesTotalCount(filters = []): Observable<number> {
-        return this.getClientsBankCodesData(filters).pipe(
+    private getClientsBankCodesTotalCount(data$: Observable<any>): Observable<number> {
+        return data$.pipe(
             map((result: any) => result && result.totalCount)
         );
     }
 
-    getAvailableBankCodes(): Observable<{[bankCode: string]: number}> {
-        return this.getClientsBankCodes().pipe(
-            map((bankCodeGroups: BankCodeGroup[]) => {
-                let availableBankCodes = {};
-                bankCodeGroups.forEach((bankCodeGroup: BankCodeGroup) => {
-                    availableBankCodes[bankCodeGroup.key] = bankCodeGroup.count;
-                });
-                return availableBankCodes;
-            }),
-            publishReplay(),
-            refCount()
-        );
-    }
-
-    getBankCodesNumber(time?: BankCodeTime): Observable<number> {
-        let filter;
-        if (time) {
-            filter = this.getFilterFromTime(time);
-        }
-        return this.getClientsBankCodesTotalCount(filter).pipe(
+    private getContactBankCodesNumber(time?: BankCodeTime): Observable<number> {
+        return this.getClientsBankCodesData(
+            [
+                ...BankCodeService.getFilterFromTime(time),
+                ...this.getSourceFilters()
+            ]
+        ).pipe(
+            map((result: any) => result && result.totalCount),
             startWith(0)
         );
     }
@@ -255,16 +306,8 @@ export class BankCodeService {
         );
     }
 
-    private getFilterFromTime(time: BankCodeTime) {
-        const currentDate = moment();
-        return [
-            {
-                ContactDate: {
-                    ge: currentDate.startOf(time.toString()).toDate(),
-                    le: currentDate.endOf(time.toString()).toDate()
-                }
-            }
-        ];
+    getBankCodeColor(bankCode): string {
+        return bankCode ? this.getBackgroundColorByLetter(bankCode[0] as BankCodeLetter) : '#000';
     }
 
 }
--- src/bank-code/shared/common/counters/counters.component.ts
+++ src/bank-code/shared/common/counters/counters.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
-import range from 'lodash/range';
+import { CrackedCode } from './cracked-code.interface';
+import { CrackedNumber } from './cracked-number.interface';
+import { CrackedCodeColor } from './cracked-code-color.type';
 
 @Component({
     selector: 'bank-code-counters',
@@ -14,40 +16,44 @@ import range from 'lodash/range';
     providers: [ DecimalPipe ]
 })
 export class CountersComponent implements OnChanges {
-    @Input() crackedCodesNumbers: { percent: number, count: number }[];
-    @Input() totalCrackedCodesNumber: string;
-    myCodesCracked = [
-        { percent: 90, outerColor: '#004a81', innerColor: '#91bfdd', title: '90%', subtitle: '4230' },
-        { percent: 40, outerColor: '#ac1f22', innerColor: '#ce767f', title: '40%', subtitle: '930' },
-        { percent: 48, outerColor: '#f09e1f', innerColor: '#ecd68a', title: '48%', subtitle: '2200' },
-        { percent: 72, outerColor: '#1b6634', innerColor: '#87c796', title: '72%', subtitle: '4230' }
+    @Input() contactCrackedCodesNumbers: CrackedNumber[];
+    @Input() allCrackedCodesNumbers: CrackedNumber[];
+    @Input() contactTotalCrackedCodesNumbers: string;
+    @Input() allTotalCrackedCodesNumbers: string;
+    crackedCodesColors: CrackedCodeColor[] = [
+        { outerColor: '#004a81', innerColor: '#91bfdd' },
+        { outerColor: '#ac1f22', innerColor: '#ce767f' },
+        { outerColor: '#f09e1f', innerColor: '#ecd68a' },
+        { outerColor: '#1b6634', innerColor: '#87c796' }
     ];
-    codesCrackedAtAGlance = [
-        { percent: 20.7, outerColor: '#004a81', innerColor: '#91bfdd', title: '20.7%', subtitle: '26,408' },
-        { percent: 26.6, outerColor: '#ac1f22', innerColor: '#ce767f', title: '26.6%', subtitle: '33,932' },
-        { percent: 36.1, outerColor: '#f09e1f', innerColor: '#ecd68a', title: '36.1%', subtitle: '46,087' },
-        { percent: 16.5, outerColor: '#1b6634', innerColor: '#87c796', title: '16.5%', subtitle: '21,067' }
-    ];
-    range = range;
+    myCodesCracked: CrackedCode[];
+    codesCrackedAtAGlance: CrackedCode[];
     constructor(
         private decimalPipe: DecimalPipe,
         public ls: AppLocalizationService
     ) {}
 
     ngOnChanges(changes: SimpleChanges) {
-        if (changes && changes.crackedCodesNumbers && changes.crackedCodesNumbers.currentValue) {
-            this.myCodesCracked.forEach((crackedCode, index: number) => {
-                const value = changes.crackedCodesNumbers.currentValue[index];
-                crackedCode.percent = value.percent;
-                crackedCode.title = crackedCode.percent.toFixed() + '%';
-                crackedCode.subtitle = this.decimalPipe.transform(value.count);
-            });
+        if (changes) {
+            if (changes.contactCrackedCodesNumbers && changes.contactCrackedCodesNumbers.currentValue) {
+                this.myCodesCracked = this.getCodesCracked(changes.contactCrackedCodesNumbers.currentValue);
+            }
+            if (changes.allCrackedCodesNumbers && changes.allCrackedCodesNumbers.currentValue) {
+                this.codesCrackedAtAGlance = this.getCodesCracked(changes.allCrackedCodesNumbers.currentValue);
+            }
         }
     }
 
-    showComma(i: number): boolean {
-        const numberReverseIndex = this.totalCrackedCodesNumber.length - i;
-        return i !== 0 && numberReverseIndex !== this.totalCrackedCodesNumber.length && numberReverseIndex % 3 === 0;
+    private getCodesCracked(crackedNumbers: CrackedNumber[]) {
+        return this.crackedCodesColors.map((crackedCodeColor: CrackedCodeColor, index: number) => {
+            const value = crackedNumbers[index];
+            return {
+                ...crackedCodeColor,
+                percent: value.percent,
+                title: value.percent.toFixed() + '%',
+                subtitle: this.decimalPipe.transform(value.count)
+            };
+        });
     }
 
 }
