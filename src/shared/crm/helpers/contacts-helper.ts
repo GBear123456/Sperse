fed9206e3d054d3ebbdfed0422a0a2f396021f9a
Denys Tkachuk <denys.tkachuk@sperse.com>
2020-07-10 21:42:03 +0300

    [SC-12096] set checkbox selected by default, fixed tslint warnings

--- src/shared/crm/helpers/contacts-helper.ts
+++ src/shared/crm/helpers/contacts-helper.ts
@@ -2,20 +2,21 @@ import swal from 'sweetalert';
 import extend from 'lodash/extend';
 
 export class ContactsHelper {
-    static showConfirmMessage(text: string, actionText: string, 
-        callback: (confirmed: boolean, force: boolean) => void, 
+    static showConfirmMessage(text: string, actionText: string,
+        callback: (confirmed: boolean, force: boolean) => void,
         canForceAction: boolean, title?: string) {
         let content, input;
         if (canForceAction) {
-            content = document.createElement("div");
-            content.className = "checkbox-container";
+            content = document.createElement('div');
+            content.className = 'checkbox-container';
 
-            input = document.createElement("input");
-            input.type = "checkbox";
-            input.id = "modal-checkbox";
+            input = document.createElement('input');
+            input.type = 'checkbox';
+            input.id = 'modal-checkbox';
+            input.checked = true;
             content.appendChild(input);
 
-            let label = document.createElement("label");
+            let label = document.createElement('label');
             label.htmlFor = 'modal-checkbox';
             const labelTextElement = document.createTextNode(actionText);
             label.appendChild(labelTextElement);
