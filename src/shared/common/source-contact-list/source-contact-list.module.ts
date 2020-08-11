273d919f345341c54f918de8966053993b55757b
Volodymyr Peren <vp@sperse.com>
2020-08-11 13:56:30 +0300

    [SC-12381] - Add Owner tab to Source Contacts drop-down list

--- src/shared/common/source-contact-list/source-contact-list.module.ts
+++ src/shared/common/source-contact-list/source-contact-list.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { SourceContactListComponent } from './source-contact-list.component';
 import { StaticListModule } from '@app/shared/common/static-list/static-list.module';
+import { OrgUnitsTreeModule } from '@shared/common/organization-units-tree/organization-units-tree.module';
 
 @NgModule({
-    imports: [ StaticListModule ],
+    imports: [
+        StaticListModule,
+        OrgUnitsTreeModule
+    ],
     exports: [ SourceContactListComponent ],
     declarations: [ SourceContactListComponent ]
 })
