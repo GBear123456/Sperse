94163700a8e62028f070b33f025e84ed881bb143
Oleh Stakhiv <os3@sperse.com>
2020-09-09 23:11:47 +0300

    [SC-12746] added check of access to contact before navigating to contact details

--- src/app/crm/contacts/personal-details/personal-details-dialog/personal-details-dialog.component.ts
+++ src/app/crm/contacts/personal-details/personal-details-dialog/personal-details-dialog.component.ts
@@ -557,15 +557,21 @@ export class PersonalDetailsDialogComponent implements OnInit, AfterViewInit, On
 
     openContactDetails(contactId: number) {
         if (contactId) {
-            /** Clear data source to avoid wrong navigating after opening the contact */
-            this.itemDetailsService.setItemsSource(
-                this.contactsService.getCurrentItemType(this.route.snapshot.queryParams),
-                null
-            );
-            setTimeout(() => this.router.navigate(
-                ['app/crm/contact', contactId],
-                { queryParams: this.route.snapshot.queryParams }
-            ));
+            this.contactProxy.isContactInfoAvailable(contactId).subscribe((isContactInfoAvailable: boolean) => {
+                if (isContactInfoAvailable) {
+                    /** Clear data source to avoid wrong navigating after opening the contact */
+                    this.itemDetailsService.setItemsSource(
+                        this.contactsService.getCurrentItemType(this.route.snapshot.queryParams),
+                        null
+                    );
+                    setTimeout(() => this.router.navigate(
+                        ['app/crm/contact', contactId],
+                        { queryParams: this.route.snapshot.queryParams }
+                    ));
+                } else {
+                    this.notifyService.error(this.ls.l('NoPermissionError'))
+                }
+            });
         }
     }
 
