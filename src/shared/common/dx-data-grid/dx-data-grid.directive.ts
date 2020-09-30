7cefa9dbd5f253763b627242b2d72c0f47879121
Volodymyr Peren <vp@sperse.com>
2020-09-30 12:45:28 +0300

    [SC-12956, SC-12991] - Updated grids date handling logic

--- src/shared/common/dx-data-grid/dx-data-grid.directive.ts
+++ src/shared/common/dx-data-grid/dx-data-grid.directive.ts
@@ -1,8 +1,11 @@
 /** Core imports */
-import { Directive, AfterViewInit, OnDestroy, Renderer2 } from '@angular/core';
+import { Directive, OnInit, OnDestroy, Renderer2 } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 /** Third party imports */
+import { AppConsts } from '@shared/AppConsts';
 import { ClipboardService } from 'ngx-clipboard';
+import { DateHelper } from '@shared/helpers/DateHelper';
 import { NotifyService } from '@abp/notify/notify.service';
 import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
 import { on } from 'devextreme/events';
@@ -11,11 +14,13 @@ import { on } from 'devextreme/events';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 
 @Directive({
-  selector: 'dx-data-grid'
+    selector: 'dx-data-grid',
+    providers: [ DatePipe ]
 })
-export class DxDataGridDirective implements AfterViewInit, OnDestroy {
+export class DxDataGridDirective implements OnInit, OnDestroy {
     private clipboardIcon;
     private subscriptions = [];
+    private timezone = DateHelper.getUserTimezone();
     private copyToClipboard = (event) => {
         this.clipboardService.copyFromContent(event.target.parentNode.innerText.trim());
         this.notifyService.info(this.ls.l('SavedToClipboard'));
@@ -25,6 +30,7 @@ export class DxDataGridDirective implements AfterViewInit, OnDestroy {
     }
 
     constructor(
+        private datePipe: DatePipe,
         private renderer: Renderer2,
         private ls: AppLocalizationService,
         private notifyService: NotifyService,
@@ -32,56 +38,114 @@ export class DxDataGridDirective implements AfterViewInit, OnDestroy {
         private clipboardService: ClipboardService
     ) {
         this.clipboardIcon = this.renderer.createElement('i');
-        this.clipboardIcon.addEventListener('click', this.copyToClipboard);
+        this.clipboardIcon.addEventListener('click', this.copyToClipboard, true);
         this.renderer.addClass(this.clipboardIcon, 'save-to-clipboard');
     }
 
-    ngAfterViewInit() {
-        this.subscriptions.push(this.component.onCellHoverChanged.subscribe(event => {
-            if (event.rowType == 'data' && event.eventType == 'mouseover') {
-                if (event.cellElement.classList.contains('clipboard-holder'))
-                    this.appendClipboardIcon(event.cellElement);
-                else
-                    this.appendClipboardIcon(event.cellElement.querySelector('.clipboard-holder'));
-            }
-        }),
-        this.component.onContentReady.subscribe(event => {
-            let dataSource = event.component.getDataSource();
-            if (!dataSource || !dataSource.group())
-                event.element.classList.remove('show-group-panel');
-        }),
-        this.component.onCellPrepared.subscribe(event => {
-            if (event.rowType == 'header') {
-                on(event.cellElement, 'dxdragstart', { timeout: 1000 }, () => {
-                    event.element.classList.add('show-group-panel');
-                });
-                on(event.cellElement, 'dxdragend', { timeout: 0 }, () => {
-                    setTimeout(() => {
-                        if (!event.component.getDataSource().group())
-                            event.element.classList.remove('show-group-panel');
-                    }, 100);
-                });
-            }
-        }));
-        this.component.onRowPrepared.subscribe(event => {
-            if (event.rowType === 'group') {
-                const cellsSelectorsToHide = ['.dx-command-select', '.dx-command-edit'];
-                let columnsNumberToAdd = 0;
-                cellsSelectorsToHide.forEach((cellSelector: string) => {
-                    const cell = event.rowElement.querySelector(cellSelector);
-                    if (cell) {
-                        cell.style.display = 'none';
-                        columnsNumberToAdd += 1;
+    ngOnInit() {
+        this.subscriptions.push(
+            this.component.onInitialized.subscribe(event => {
+                this.checkInitDateCellColumn(event.component);
+            }),
+            this.component.onOptionChanged.subscribe(event => {
+                if (event.name == 'dataSource')
+                    setTimeout(() => this.checkInitDateCellColumn(event.component));
+            }),
+            this.component.onCellHoverChanged.subscribe(event => {
+                if (event.rowType == 'data') {
+                    if (event.eventType == 'mouseover') {
+                        if (event.column.name == 'hiddenTime') {
+                            let text = event.cellElement.querySelector('span');
+                            if (!text) {
+                                event.cellElement.innerHTML = '';
+                                text = this.renderer.createElement('span');
+                                this.renderer.appendChild(event.cellElement, text);
+                            }
+                            text.innerText = event.value || '';
+                        }
+                        if (event.cellElement.classList.contains('clipboard-holder'))
+                            this.appendClipboardIcon(event.cellElement);
+                        else
+                            this.appendClipboardIcon(event.cellElement.querySelector('.clipboard-holder'));
                     }
-                });
-                if (columnsNumberToAdd) {
-                    const lastColumn = event.rowElement.querySelector('td[colspan]');
-                    if (lastColumn) {
-                        lastColumn.colSpan = +lastColumn.colSpan + columnsNumberToAdd;
+                    if (event.eventType == 'mouseout') {
+                        if (event.column.name == 'hiddenTime') {
+                            let text = event.cellElement.querySelector('span');
+                            text.innerText = event.value ? event.value.split(' ').shift() : '';
+                        }
                     }
                 }
+            }),
+            this.component.onContentReady.subscribe(event => {
+                let dataSource = event.component.getDataSource();
+                if (!dataSource || !dataSource.group())
+                    event.element.classList.remove('show-group-panel');
+            }),
+            this.component.onCellPrepared.subscribe(event => {
+                if (event.rowType == 'header') {
+                    on(event.cellElement, 'dxdragstart', { timeout: 1000 }, () => {
+                        event.element.classList.add('show-group-panel');
+                    });
+                    on(event.cellElement, 'dxdragend', { timeout: 0 }, () => {
+                        setTimeout(() => {
+                            if (!event.component.getDataSource().group())
+                                event.element.classList.remove('show-group-panel');
+                        }, 100);
+                    });
+                }
+            }),
+            this.component.onRowPrepared.subscribe(event => {
+                if (event.rowType === 'group') {
+                    const cellsSelectorsToHide = ['.dx-command-select', '.dx-command-edit'];
+                    let columnsNumberToAdd = 0;
+                    cellsSelectorsToHide.forEach((cellSelector: string) => {
+                        const cell = event.rowElement.querySelector(cellSelector);
+                        if (cell) {
+                            cell.style.display = 'none';
+                            columnsNumberToAdd += 1;
+                        }
+                    });
+                    if (columnsNumberToAdd) {
+                        const lastColumn = event.rowElement.querySelector('td[colspan]');
+                        if (lastColumn) {
+                            lastColumn.colSpan = +lastColumn.colSpan + columnsNumberToAdd;
+                        }
+                    }
+                }
+            })
+        );
+    }
+
+    checkInitDateCellColumn(component) {
+        component.option('columns').forEach(column => {
+            if (column.cellTemplate == 'dateCell') {
+                this.initDateCellColumn(column, component);
             }
-        })
+        });
+    }
+
+    initDateCellColumn(column, component) {
+        component.columnOption(column.dataField, 'name', 'hiddenTime');
+        component.columnOption(column.dataField, 'minWidth', '180px');
+        component.columnOption(column.dataField, 'cellTemplate', undefined);
+        component.columnOption(column.dataField, 'cssClass', column.cssClass + ' clipboard-holder');
+        component.columnOption(column.dataField, 'calculateDisplayValue', (data) => {
+            return this.getDateFormated(data[column.dataField]);
+        });
+        component.columnOption(column.dataField, 'calculateGroupValue', (data) => {
+            return this.getDateFormated(data[column.dataField]);
+        });
+        component.columnOption(column.dataField, 'calculateCellValue', (data) => {
+            return this.getDateFormated(data[column.dataField], false);
+        });
+    }
+
+    getDateFormated(value: string, withoutTime = true) {
+        let date = value && this.datePipe.transform(
+            value, AppConsts.formatting.dateTime, this.timezone);
+        if (withoutTime)
+            date = date && date.split(' ').shift();
+        return date || '';
     }
 
     appendClipboardIcon(elm) {
