d37e82f7b4d63bdb43e0570f45f54744d35c7088
Volodymyr Peren <vp@sperse.com>
2020-11-24 20:13:04 +0200

    [SC-6623] - Expire date should be passed with time

--- src/app/api/introduction/add-key-dialog/add-key-dialog.component.ts
+++ src/app/api/introduction/add-key-dialog/add-key-dialog.component.ts
@@ -7,6 +7,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 /** Application imports */
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { GenerateApiKeyInput } from '@shared/service-proxies/service-proxies';
+import { DateHelper } from '@shared/helpers/DateHelper';
 
 @Component({
     templateUrl: 'add-key-dialog.component.html',
@@ -15,18 +16,28 @@ import { GenerateApiKeyInput } from '@shared/service-proxies/service-proxies';
 export class EditKeyDialog extends AppComponentBase {
     isValid = false;
     validator: any;
+    minCalendarDate = DateHelper.addTimezoneOffset(new Date(), true);
     maxCalendarDate = new Date(2038, 0, 19);
-    model: GenerateApiKeyInput = new GenerateApiKeyInput();
+    model: GenerateApiKeyInput = new GenerateApiKeyInput({
+        name: '',
+        expirationDate: this.minCalendarDate
+    });
+
+    private readonly ONE_HOUR_MILISECONDS = 3600000;
 
     constructor(injector: Injector,
         @Inject(MAT_DIALOG_DATA) public data: any,
-        public dialogRef: MatDialogRef<EditKeyDialog>) {
+        public dialogRef: MatDialogRef<EditKeyDialog>
+    ) {
         super(injector);
+        this.model.expirationDate.setTime(this.minCalendarDate.getTime() + this.ONE_HOUR_MILISECONDS);
     }
 
     onSave(event) {
-        if (this.validator.validate().isValid)
+        if (this.validator.validate().isValid) {
+            this.model.expirationDate = DateHelper.removeTimezoneOffset(this.model.expirationDate, true);
             this.dialogRef.close(this.model);
+        }
     }
 
     initValidationGroup(event) {
