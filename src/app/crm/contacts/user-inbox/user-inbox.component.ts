d76be756670f12855ff495e53803352e93c3ac56
Roman Mudryk <roman.mudryk@sperse.com>
2020-11-24 18:39:41 +0200

    [SC-13494] generated proxy

--- src/app/crm/contacts/user-inbox/user-inbox.component.ts
+++ src/app/crm/contacts/user-inbox/user-inbox.component.ts
@@ -19,7 +19,7 @@ import { LoadingService } from '@shared/common/loading-service/loading.service';
 import { ProfileService } from '@shared/common/profile-service/profile.service';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
 import { CommunicationMessageDeliveryType, ContactCommunicationServiceProxy, AttachmentDto,
-    CommunicationMessageSendingStatus, MessageDto, ContactInfoDto } from '@shared/service-proxies/service-proxies';
+    CommunicationMessageSendingStatus, MessageDto, ContactInfoDto, FileInfo } from '@shared/service-proxies/service-proxies';
 import { ContactsService } from '../contacts.service';
 import { AppPermissionService } from '@shared/common/auth/permission.service';
 
@@ -481,7 +481,12 @@ export class UserInboxComponent implements OnDestroy {
                 bcc: undefined,
                 subject: 'Re: ' + this.activeMessage.subject,
                 body: this.instantMessageText,
-                attachments: this.instantMessageAttachments.map(item => item.id)
+                attachments: this.instantMessageAttachments.map(item => {
+                    return new FileInfo({
+                        id: item.id,
+                        name: item.name
+                    });
+                })
             }) : this.contactsService.sendSMS({
                 contactId: this.contactId,
                 parentId: parentId,
