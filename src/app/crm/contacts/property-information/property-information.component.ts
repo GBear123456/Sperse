4f0aff78294cbacdf657d69b08f441f22e9091bd
Volodymyr Peren <vp@sperse.com>
2020-12-03 18:50:04 +0200

    [SC-13639] - Support Property Name

--- src/app/crm/contacts/property-information/property-information.component.ts
+++ src/app/crm/contacts/property-information/property-information.component.ts
@@ -76,7 +76,7 @@ export class PropertyInformationComponent implements OnInit {
             this.initialProperty = property;
             this.savePropertyInfo(property);
             this.changeDetectorRef.detectChanges();
-        })
+        });
     }
 
     savePropertyInfo(property: PropertyDto) {
@@ -98,7 +98,7 @@ export class PropertyInformationComponent implements OnInit {
                 id: null
             },
             property.address.countryId)
-        ]
+        ];
     }
 
     updateAddress({ address, dialogData }: AddressUpdate) {
