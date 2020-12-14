82a5e4c6fc260c21f068356e085f64d8f550a8ac
Oleh Stakhiv <os3@sperse.com>
2020-12-14 14:33:58 +0200

    [SC-13676] added closing of dialog after 'Cancel' button click, added creating of syncAccount after getting of start stage from saltEdge events

--- src/shared/common/account-connector-dialog/salt-edge/salt-edge.component.ts
+++ src/shared/common/account-connector-dialog/salt-edge/salt-edge.component.ts
@@ -9,7 +9,12 @@ import {
 import { CFOService } from '@shared/cfo/cfo.service';
 import { filter, first, switchMap } from 'rxjs/operators';
 import { SyncTypeIds } from '@shared/AppEnums';
-import { GetConnectionInfoOutput, SyncServiceProxy } from '@shared/service-proxies/service-proxies';
+import {
+    CreateSyncAccountInput,
+    GetConnectionInfoOutput,
+    SyncAccountServiceProxy,
+    SyncServiceProxy
+} from '@shared/service-proxies/service-proxies';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { NotifyService } from '@abp/notify/notify.service';
 import { SynchProgressService } from '@shared/cfo/bank-accounts/helpers/synch-progress.service';
@@ -22,6 +27,7 @@ import { SynchProgressService } from '@shared/cfo/bank-accounts/helpers/synch-pr
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SaltEdgeComponent implements OnInit {
+    @Output() onClose: EventEmitter<any> = new EventEmitter<any>();
     @Output() onComplete: EventEmitter<any> = new EventEmitter<any>();
     connectUrl: SafeResourceUrl;
     constructor(
@@ -31,7 +37,8 @@ export class SaltEdgeComponent implements OnInit {
         private domSanitizer: DomSanitizer,
         private window: Window,
         private notifyService: NotifyService,
-        private syncProgressService: SynchProgressService
+        private syncProgressService: SynchProgressService,
+        private syncAccountServiceProxy: SyncAccountServiceProxy
     ) {}
 
     ngOnInit() {
@@ -41,9 +48,25 @@ export class SaltEdgeComponent implements OnInit {
 
     providerEventsHandler = (event: MessageEvent) => {
         if (event.data) {
+            if (event.data === 'cancel') {
+                this.onClose.emit();
+                return;
+            }
             const response = JSON.parse(event.data);
+            if (response.data.api_stage === 'start') {
+                this.syncAccountServiceProxy.create(
+                    this.cfoService.instanceType,
+                    this.cfoService.instanceId,
+                    new CreateSyncAccountInput({
+                        isSyncBankAccountsEnabled: true,
+                        typeId: SyncTypeIds.SaltEdge,
+                        publicToken: undefined,
+                        syncAccountRef: response.data.connection_id
+                    })
+                ).subscribe()
+            }
             if (response.data.stage === 'success') {
-                this.notifyService.success('Successfully Connected')
+                this.notifyService.success('Successfully Connected');
                 this.onComplete.emit();
                 this.syncProgressService.runSynchProgress().subscribe();
             }
