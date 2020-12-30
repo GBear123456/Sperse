a2694515e16f4ab9e7789719c2597dcb2364efb5
Oleh Stakhiv <os3@sperse.com>
2020-12-30 20:06:19 +0200

    fixed build error due to , removed extending of AppComponentBase in some places, added missing formatting property to some components where property is using in template

--- src/account/layouts/lend-space/lend-space-layout.component.ts
+++ src/account/layouts/lend-space/lend-space-layout.component.ts
@@ -1,12 +1,10 @@
 /** Core imports */
-import { Component, Injector, Renderer2 } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 
 /** Application imports */
-import { AppComponentBase } from '@shared/common/app-component-base';
 import * as moment from 'moment';
-import {LayoutType} from '@shared/service-proxies/service-proxies';
-import {AppSessionService} from '@shared/common/session/app-session.service';
+import { AppSessionService } from '@shared/common/session/app-session.service';
 
 @Component({
     templateUrl: './lend-space-layout.component.html',
@@ -14,19 +12,16 @@ import {AppSessionService} from '@shared/common/session/app-session.service';
         './lend-space-layout.component.less'
     ]
 })
-export class LendSpaceLayoutComponent extends AppComponentBase {
+export class LendSpaceLayoutComponent {
     currentDate = new Date();
     currentYear: number = moment().year();
     previousUrl: string;
 
     constructor(
-        injector: Injector,
         private router: Router,
-        private _appSession: AppSessionService,
+        private appSession: AppSessionService,
         private renderer: Renderer2
     ) {
-        super(injector);
-
         if (!this.previousUrl) {
             (this.router.url.indexOf('?') != -1) ?
                 this.previousUrl = this.router.url.substring(0, this.router.url.indexOf('?')).split('/').pop() :
