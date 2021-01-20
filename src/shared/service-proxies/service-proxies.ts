867edce681cfc955a67ff26f63da8b7730e7c6b9
Roman Mudryk <roman.mudryk@sperse.com>
2021-01-20 21:02:23 +0200

    [SC-13859] Implemented API method for editing product

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -26595,6 +26595,58 @@ export class ProductServiceProxy {
     }
 
     /**
+     * @body (optional) 
+     * @return Success
+     */
+    updateProduct(body: UpdateProductInput | null | undefined): Observable<void> {
+        let url_ = this.baseUrl + "/api/services/CRM/Product/UpdateProduct";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(body);
+
+        let options_ : any = {
+            body: content_,
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+            })
+        };
+
+        return this.http.request("put", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processUpdateProduct(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processUpdateProduct(<any>response_);
+                } catch (e) {
+                    return <Observable<void>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<void>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processUpdateProduct(response: HttpResponseBase): Observable<void> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return _observableOf<void>(<any>null);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<void>(<any>null);
+    }
+
+    /**
      * @return Success
      */
     getProudctGroups(): Observable<ProductGroupInfo[]> {
@@ -71813,10 +71865,6 @@ export interface IProductInfo {
     rate: number | undefined;
 }
 
-export enum ProductType {
-    Subscription = "Subscription", 
-}
-
 export class ProductServiceInfo implements IProductServiceInfo {
     memberServiceId!: number | undefined;
     memberServiceLevelId!: number | undefined;
@@ -71906,15 +71954,19 @@ export interface IProductSubscriptionOptionInfo {
     trialDayCount: number | undefined;
 }
 
+export enum ProductType {
+    Subscription = "Subscription", 
+}
+
 export class CreateProductInput implements ICreateProductInput {
+    productServices!: ProductServiceInfo[] | undefined;
+    productSubscriptionOptions!: ProductSubscriptionOptionInfo[] | undefined;
     code!: string;
     name!: string;
     description!: string | undefined;
     groupId!: number | undefined;
     type!: ProductType | undefined;
     price!: number | undefined;
-    productServices!: ProductServiceInfo[] | undefined;
-    productSubscriptionOptions!: ProductSubscriptionOptionInfo[] | undefined;
 
     constructor(data?: ICreateProductInput) {
         if (data) {
@@ -71927,12 +71979,6 @@ export class CreateProductInput implements ICreateProductInput {
 
     init(data?: any) {
         if (data) {
-            this.code = data["code"];
-            this.name = data["name"];
-            this.description = data["description"];
-            this.groupId = data["groupId"];
-            this.type = data["type"];
-            this.price = data["price"];
             if (data["productServices"] && data["productServices"].constructor === Array) {
                 this.productServices = [];
                 for (let item of data["productServices"])
@@ -71943,6 +71989,12 @@ export class CreateProductInput implements ICreateProductInput {
                 for (let item of data["productSubscriptionOptions"])
                     this.productSubscriptionOptions.push(ProductSubscriptionOptionInfo.fromJS(item));
             }
+            this.code = data["code"];
+            this.name = data["name"];
+            this.description = data["description"];
+            this.groupId = data["groupId"];
+            this.type = data["type"];
+            this.price = data["price"];
         }
     }
 
@@ -71955,12 +72007,6 @@ export class CreateProductInput implements ICreateProductInput {
 
     toJSON(data?: any) {
         data = typeof data === 'object' ? data : {};
-        data["code"] = this.code;
-        data["name"] = this.name;
-        data["description"] = this.description;
-        data["groupId"] = this.groupId;
-        data["type"] = this.type;
-        data["price"] = this.price;
         if (this.productServices && this.productServices.constructor === Array) {
             data["productServices"] = [];
             for (let item of this.productServices)
@@ -71971,19 +72017,25 @@ export class CreateProductInput implements ICreateProductInput {
             for (let item of this.productSubscriptionOptions)
                 data["productSubscriptionOptions"].push(item.toJSON());
         }
+        data["code"] = this.code;
+        data["name"] = this.name;
+        data["description"] = this.description;
+        data["groupId"] = this.groupId;
+        data["type"] = this.type;
+        data["price"] = this.price;
         return data; 
     }
 }
 
 export interface ICreateProductInput {
+    productServices: ProductServiceInfo[] | undefined;
+    productSubscriptionOptions: ProductSubscriptionOptionInfo[] | undefined;
     code: string;
     name: string;
     description: string | undefined;
     groupId: number | undefined;
     type: ProductType | undefined;
     price: number | undefined;
-    productServices: ProductServiceInfo[] | undefined;
-    productSubscriptionOptions: ProductSubscriptionOptionInfo[] | undefined;
 }
 
 export class CreateProductOutput implements ICreateProductOutput {
@@ -72020,6 +72072,66 @@ export class CreateProductOutput implements ICreateProductOutput {
 
 export interface ICreateProductOutput {
     productId: number | undefined;
+}
+
+export class UpdateProductInput implements IUpdateProductInput {
+    id!: number;
+    code!: string;
+    name!: string;
+    description!: string | undefined;
+    groupId!: number | undefined;
+    type!: ProductType | undefined;
+    price!: number | undefined;
+
+    constructor(data?: IUpdateProductInput) {
+        if (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property))
+                    (<any>this)[property] = (<any>data)[property];
+            }
+        }
+    }
+
+    init(data?: any) {
+        if (data) {
+            this.id = data["id"];
+            this.code = data["code"];
+            this.name = data["name"];
+            this.description = data["description"];
+            this.groupId = data["groupId"];
+            this.type = data["type"];
+            this.price = data["price"];
+        }
+    }
+
+    static fromJS(data: any): UpdateProductInput {
+        data = typeof data === 'object' ? data : {};
+        let result = new UpdateProductInput();
+        result.init(data);
+        return result;
+    }
+
+    toJSON(data?: any) {
+        data = typeof data === 'object' ? data : {};
+        data["id"] = this.id;
+        data["code"] = this.code;
+        data["name"] = this.name;
+        data["description"] = this.description;
+        data["groupId"] = this.groupId;
+        data["type"] = this.type;
+        data["price"] = this.price;
+        return data; 
+    }
+}
+
+export interface IUpdateProductInput {
+    id: number;
+    code: string;
+    name: string;
+    description: string | undefined;
+    groupId: number | undefined;
+    type: ProductType | undefined;
+    price: number | undefined;
 }
 
 export class ProductGroupInfo implements IProductGroupInfo {
