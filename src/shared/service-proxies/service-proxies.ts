990f43e6a008644bb174a5898d9e2bfa43c19425
Roman Mudryk <roman.mudryk@sperse.com>
2021-02-12 20:34:54 +0200

    [SC-13970] Implemented User Information permission. Implemented LoginAsUser API method

--- src/shared/service-proxies/service-proxies.ts
+++ src/shared/service-proxies/service-proxies.ts
@@ -12285,6 +12285,62 @@ export class ContactUserServiceProxy {
     /**
      * @return Success
      */
+    loginAsUser(userId: number): Observable<LoginAsUserOutput> {
+        let url_ = this.baseUrl + "/api/services/CRM/ContactUser/LoginAsUser?";
+        if (userId === undefined || userId === null)
+            throw new Error("The parameter 'userId' must be defined and cannot be null.");
+        else
+            url_ += "userId=" + encodeURIComponent("" + userId) + "&"; 
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Content-Type": "application/json", 
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("post", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processLoginAsUser(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processLoginAsUser(<any>response_);
+                } catch (e) {
+                    return <Observable<LoginAsUserOutput>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<LoginAsUserOutput>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processLoginAsUser(response: HttpResponseBase): Observable<LoginAsUserOutput> {
+        const status = response.status;
+        const responseBlob = 
+            response instanceof HttpResponse ? response.body : 
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }};
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = resultData200 ? LoginAsUserOutput.fromJS(resultData200) : new LoginAsUserOutput();
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<LoginAsUserOutput>(<any>null);
+    }
+
+    /**
+     * @return Success
+     */
     requestAutoLoginToken(userKey: string): Observable<string> {
         let url_ = this.baseUrl + "/api/services/CRM/ContactUser/RequestAutoLoginToken?";
         if (userKey === undefined || userKey === null)
@@ -55294,6 +55350,42 @@ export class UpdateContactTagInput implements IUpdateContactTagInput {
 export interface IUpdateContactTagInput {
     id: number;
     name: string;
+}
+
+export class LoginAsUserOutput implements ILoginAsUserOutput {
+    impersonationToken!: string | undefined;
+
+    constructor(data?: ILoginAsUserOutput) {
+        if (data) {
+            for (var property in data) {
+                if (data.hasOwnProperty(property))
+                    (<any>this)[property] = (<any>data)[property];
+            }
+        }
+    }
+
+    init(data?: any) {
+        if (data) {
+            this.impersonationToken = data["impersonationToken"];
+        }
+    }
+
+    static fromJS(data: any): LoginAsUserOutput {
+        data = typeof data === 'object' ? data : {};
+        let result = new LoginAsUserOutput();
+        result.init(data);
+        return result;
+    }
+
+    toJSON(data?: any) {
+        data = typeof data === 'object' ? data : {};
+        data["impersonationToken"] = this.impersonationToken;
+        return data; 
+    }
+}
+
+export interface ILoginAsUserOutput {
+    impersonationToken: string | undefined;
 }
 
 export class CountryDto implements ICountryDto {
