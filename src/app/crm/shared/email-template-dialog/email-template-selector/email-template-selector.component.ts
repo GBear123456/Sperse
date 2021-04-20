05865d8468e3be0a1f9fb102c5d0c3c589d3b2ba
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2021-04-20 11:58:21 +0300

    [SC-14320] Removed System Default from Invoices; Changed title

--- src/app/crm/shared/email-template-dialog/email-template-selector/email-template-selector.component.ts
+++ src/app/crm/shared/email-template-dialog/email-template-selector/email-template-selector.component.ts
@@ -24,6 +24,7 @@ export class EmailTemplateSelectorComponent {
     internalTemplateId: number;
     systemDefaultId = -1;
 
+    @Input() title: string = this.ls.l("UserRegistrationEmail");
     @Input() templateType: EmailTemplateType;
     @Input() showSystemDefault: boolean = true;
     @Input()
