6d32a13fc0fc71c2d49514548b241bb012dfc090
Volodymyr Peren <vp@sperse.com>
2021-05-24 14:04:13 +0300

    [SC-14545] - 'Back' and 'Next' buttons might lose functionality after certain steps

--- src/app/cfo/reports/generate-report-dialog/generate-report-dialog.component.ts
+++ src/app/cfo/reports/generate-report-dialog/generate-report-dialog.component.ts
@@ -294,11 +294,13 @@ export class GenerateReportDialogComponent implements OnInit {
             this.buttons[this.BACK_BTN_INDEX].disabled = false;
         } else if (this.currentStep == GenerateReportStep.ReportTemplate) {
             this.title = this.ls.l('SelectReportTemplate');
+            this.buttons[this.NEXT_BTN_INDEX].disabled = false;
             this.buttons[this.BACK_BTN_INDEX].disabled = true;
         } else if (this.currentStep == GenerateReportStep.Calendar) {
             this.title = this.currentConfig.showYearCalendar ? this.ls.l('Select') + ' ' + this.ls.l('Year') :
                 this.ls.l(this.currentConfig.showSingleDateCalendar ? 'SelectDate' : 'SelectDateRange');
             this.buttons[this.BACK_BTN_INDEX].disabled = false;
+            this.buttons[this.NEXT_BTN_INDEX].disabled = false;
         } else if (this.currentStep == GenerateReportStep.Final) {
             this.title = this.ls.l('ReportGenerationOptions');
             this.buttons = null;
