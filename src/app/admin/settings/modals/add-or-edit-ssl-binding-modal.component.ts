669e0c41b71fd2b3c6cc18652b24c45fec50c64e
Volodymyr Peren <vp@sperse.com>
2021-07-15 13:16:16 +0300

    [SC-14789] - Applied testing comment

--- src/app/admin/settings/modals/add-or-edit-ssl-binding-modal.component.ts
+++ src/app/admin/settings/modals/add-or-edit-ssl-binding-modal.component.ts
@@ -97,7 +97,7 @@ export class AddOrEditSSLBindingModalComponent {
         if (this.editing) {
             this.model = new UpdateSslBindingInput({
                 id: data.item.id,
-                organizationUnitId: data.item.organizationUnitId,
+                organizationUnitId: data.item.organizationUnitId || -1,
                 sslCertificateId: data.item.sslCertificateId,
                 isActive: data.item.isActive
             }); // ...data.item added a lot of no needed items
