71dc24ac7ad4bf04b60442db84ead627bc8958ea
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2021-11-01 20:22:10 +0200

    [SC-15258] Changed Mark As Paid behaviuor according to invoice status; Fixed dialog styles

--- src/app/crm/contacts/invoices/mark-paid-dialog/mark-paid-dialog.component.ts
+++ src/app/crm/contacts/invoices/mark-paid-dialog/mark-paid-dialog.component.ts
@@ -11,7 +11,7 @@ import { finalize, first, filter } from 'rxjs/operators';
 import { AppConsts } from '@shared/AppConsts';
 import { DateHelper } from '@shared/helpers/DateHelper';
 import { AppLocalizationService } from '@app/shared/common/localization/app-localization.service';
-import { InvoiceServiceProxy, AddBankCardPaymentInput, InvoiceSettings, PaymentTransactionType, PaymentServiceProxy } from '@shared/service-proxies/service-proxies';
+import { InvoiceServiceProxy, AddBankCardPaymentInput, InvoiceSettings, PaymentTransactionType, PaymentServiceProxy, InvoiceStatus } from '@shared/service-proxies/service-proxies';
 import { LoadingService } from '@shared/common/loading-service/loading.service';
 import { InvoicesService } from '@app/crm/contacts/invoices/invoices.service';
 
@@ -23,7 +23,7 @@ export class MarkAsPaidDialogComponent {
     date = DateHelper.addTimezoneOffset(new Date(), true);
     amount = this.data.invoice.Amount;
     currency = '$';
-    transactionTypes = [PaymentTransactionType.Sale, PaymentTransactionType.Refund, PaymentTransactionType.Chargeback];
+    transactionTypes = [];
     model: AddBankCardPaymentInput = new AddBankCardPaymentInput();
     paymentProviders$ = this.paymentProxy.getPaymentProviders();
     PaymentTransactionType = PaymentTransactionType;
@@ -42,10 +42,20 @@ export class MarkAsPaidDialogComponent {
             this.currency = getCurrencySymbol(res.currency, 'wide')
         });
 
-        this.model.invoiceId = this.data.invoice.InvoiceId;
-        this.model.invoiceNumber = this.data.invoice.InvoiceNumber;
-        this.model.orderStage = this.data.invoice.OrderStage;
-        this.model.transactionType = this.transactionTypes[0];
+        this.initTransactionTypes(data.invoice.InvoiceStatus);
+
+        this.model.invoiceId = data.invoice.InvoiceId;
+        this.model.invoiceNumber = data.invoice.InvoiceNumber;
+        this.model.orderStage = data.invoice.OrderStage;
+        this.model.transactionType = data.invoice.InvoiceStatus == InvoiceStatus.Paid ? PaymentTransactionType.Refund : PaymentTransactionType.Sale;
+    }
+
+    initTransactionTypes(invoiceStatus: InvoiceStatus): void {
+        this.transactionTypes = [
+            { text: PaymentTransactionType.Sale, disabled: invoiceStatus == InvoiceStatus.Paid },
+            { text: PaymentTransactionType.Refund, disabled: invoiceStatus != InvoiceStatus.Paid && invoiceStatus != InvoiceStatus.PartiallyPaid },
+            { text: PaymentTransactionType.Chargeback, disabled: invoiceStatus != InvoiceStatus.Paid && invoiceStatus != InvoiceStatus.PartiallyPaid }
+        ]
     }
 
     allowDigitsOnly(event, exceptions = []) {
