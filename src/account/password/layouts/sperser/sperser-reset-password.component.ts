9e63407ae14792bb21358d90d822d8f5c7a5b67a
Denys Tkachuk <denys.tkachuk@utechcorp.com>
2021-12-13 00:37:36 +0200

    [SC-15401] applied test comments

--- src/account/password/layouts/sperser/sperser-reset-password.component.ts
+++ src/account/password/layouts/sperser/sperser-reset-password.component.ts
@@ -1,5 +1,5 @@
 /** Core imports */
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 /** Application imports */
 import { accountModuleAnimation } from '@shared/animations/routerTransition';
@@ -7,13 +7,31 @@ import { HostResetPasswordComponent } from '../host/host-reset-password.componen
 
 @Component({
     templateUrl: './sperser-reset-password.component.html',
-    styleUrls: [ './sperser-password.component.less' ],
+    styleUrls: ['./sperser-password.component.less'],
     animations: [accountModuleAnimation()]
 })
-export class SperserResetPasswordComponent extends HostResetPasswordComponent {
+export class SperserResetPasswordComponent extends HostResetPasswordComponent implements OnInit {
+    public showPasswordComplexity: boolean;
+    public minHeight: number;
+
     showHidePass(input) {
         let native = input.valueAccessor._elementRef.nativeElement,
             visible = native.type == 'text';
         native.type = visible ? 'password' : 'text';
+    }
+
+    ngOnInit(): void {
+        this.minHeight = 0;
+        let lines = 1;
+        for (const item in this.passwordComplexitySetting) {
+            if (item !== 'init' && item !== 'toJSON' && this.passwordComplexitySetting[item]) {
+                lines++;
+            }
+        }
+        this.minHeight = lines * 29.2 + 30;
+    }
+
+    onFocus(): void {
+        this.showPasswordComplexity = true;
     }
 }
