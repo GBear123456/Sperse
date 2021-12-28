e3d1c5c7c8792799ac58c2171a4972bac6960257
Volodymyr Peren <vp@sperse.com>
2021-12-28 17:25:19 +0200

    [SC-15505] - Fix Column's name for Subcontact

--- src/app/crm/contacts/operations-widget/operations-widget.component.ts
+++ src/app/crm/contacts/operations-widget/operations-widget.component.ts
@@ -556,7 +556,7 @@ export class OperationsWidgetComponent extends AppComponentBase implements After
     }
 
     updateActiveGroups(status?: GroupStatus) {
-        if (this.contactGroups) {
+        if (this.contactGroups && this.contactInfo) {
             this.statuses = [];
             this.contactGroups.forEach(group => {
                 if (this.permission.checkCGPermission([group.id], ''))
