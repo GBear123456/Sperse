0e2725ff579faa79d9e8c1da37a95f9ff12f93b5
Volodymyr Peren <vp@sperse.com>
2022-05-27 13:43:21 +0300

    [SC-16089] - Fix showing editions in Edit Tenant dialog

--- src/app/admin/tenants/create-tenant-modal/create-tenant-modal.component.ts
+++ src/app/admin/tenants/create-tenant-modal/create-tenant-modal.component.ts
@@ -30,7 +30,7 @@ import { AppLocalizationService } from '@app/shared/common/localization/app-loca
 import { NotifyService } from 'abp-ng2-module';
 import { IDialogButton } from '@shared/common/dialogs/modal/dialog-button.interface';
 import { ModalDialogComponent } from '@shared/common/dialogs/modal/modal-dialog.component';
-import { ModulesEditionsSelectComponent } from '../modules-edtions-select.component.ts/modules-editions-select.component';
+import { ModulesEditionsSelectComponent } from '../modules-edtions-select/modules-editions-select.component';
 
 //!!VP should be reimplemnted to use Dx text box instead of inputs
 @Component({
