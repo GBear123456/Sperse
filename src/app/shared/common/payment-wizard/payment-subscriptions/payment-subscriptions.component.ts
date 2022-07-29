b234d2c80c051c3f97bad9df004308d2fb063aad
Volodymyr Peren <vp@sperse.com>
2022-07-29 18:16:52 +0300

    [SC-16245] - Implement current subscriptions list in Payment Dialog context

--- src/app/shared/common/payment-wizard/payment-subscriptions/payment-subscriptions.component.ts
+++ src/app/shared/common/payment-wizard/payment-subscriptions/payment-subscriptions.component.ts
@@ -6,8 +6,16 @@ import {
     Injector
 } from '@angular/core';
 
+/** Third party imports */
+import { MatDialog } from '@angular/material/dialog';
+import { finalize } from 'rxjs/operators';
+
 /** Application imports */
-import { ProductServiceProxy, ProductInfo } from '@shared/service-proxies/service-proxies';
+import { 
+    CancelOrderSubscriptionInput, 
+    OrderSubscriptionServiceProxy 
+} from '@shared/service-proxies/service-proxies';
+import { CancelSubscriptionDialogComponent } from '@app/crm/contacts/subscriptions/cancel-subscription-dialog/cancel-subscription-dialog.component';
 import { AppComponentBase } from '@shared/common/app-component-base';
 import { AppService } from '@app/app.service';
 import { AppConsts } from '@shared/AppConsts';
@@ -16,33 +24,41 @@ import { AppConsts } from '@shared/AppConsts';
     selector: 'payment-subscriptions',
     templateUrl: './payment-subscriptions.component.html',
     styleUrls: ['./payment-subscriptions.component.less'],
+    providers: [OrderSubscriptionServiceProxy],
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PaymentSubscriptionsComponent extends AppComponentBase {
     formatting = AppConsts.formatting;
-    products: ProductInfo[] = [];
 
     constructor(
         injector: Injector,
         public appService: AppService,
-        private productServiceProxy: ProductServiceProxy,
+        private dialog: MatDialog,
+        private subscriptionProxy: OrderSubscriptionServiceProxy,
         private changeDetectionRef: ChangeDetectorRef
     ) {
         super(injector);
-        this.productServiceProxy.getSubscriptionProductsByGroupName(
-            'signup'
-        ).subscribe((products: ProductInfo[]) => {
-            this.products = products;
-            this.changeDetectionRef.detectChanges();
-            console.log(this.products);
-        });
-    }
-
-    getProductImage(data) {
-        let product = this.products.find(product => product.id == data.productId);
-        return product && product.imageUrl || 'assets/common/images/product.png';
     }
 
     cancelSubscription(data) {
+        this.dialog.open(CancelSubscriptionDialogComponent, {
+            width: '400px',
+            data: {
+                title: this.l('CancelBillingConfirm')
+            }
+        }).afterClosed().subscribe(result => {
+            if (result) {
+                this.startLoading();
+                this.subscriptionProxy
+                    .cancel(new CancelOrderSubscriptionInput({
+                        subscriptionId: data.id,
+                        cancelationReason: result.cancellationReason
+                    })).pipe(finalize(() => this.finishLoading())).subscribe(() => {
+                        data.statusId = 'C';
+                        abp.notify.success(this.l('Cancelled'));
+                        this.changeDetectionRef.detectChanges();
+                    });
+            }
+        });
     }
 }
