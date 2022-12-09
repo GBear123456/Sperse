273ea191e139cbe6bbe0edbce26092024570b011
Volodymyr Peren <vp@sperse.com>
2022-12-09 14:49:55 +0200

    [SC-16557] - Show Contact Activity Logs on Contact details

--- src/app/crm/contacts/activity-logs/activity-logs.component.ts
+++ src/app/crm/contacts/activity-logs/activity-logs.component.ts
@@ -5,15 +5,20 @@ import { Component, Injector, ViewChild } from '@angular/core';
 import DataSource from 'devextreme/data/data_source';
 import ODataStore from 'devextreme/data/odata/store';
 import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
+import startCase from 'lodash/startCase';
+import { first } from 'rxjs/operators';
 
 /** Application imports */
-import { AppComponentBase } from '@shared/common/app-component-base';
 import { AppConsts } from '@shared/AppConsts';
-import { UserServiceProxy } from '@shared/service-proxies/service-proxies';
+import { AppPermissions } from '@shared/AppPermissions';
 import { KeysEnum } from '@shared/common/keys.enum/keys.enum';
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { UserServiceProxy, ContactServiceProxy, ContactActivityLogInfo, 
+    ContactActivityLogType } from '@shared/service-proxies/service-proxies';
 import { ActivityLogsFields } from '@app/crm/contacts/activity-logs/activity-logs-fields.enum';
 import { ActivityLogsDto } from '@app/crm/contacts/activity-logs/activity-logs-dto.interface';
 import { DataGridService } from '@app/shared/common/data-grid.service/data-grid.service';
+import { ContactsService } from '../contacts.service';
 
 @Component({
     selector: 'activity-logs',
@@ -22,16 +27,22 @@ import { DataGridService } from '@app/shared/common/data-grid.service/data-grid.
 })
 export class ActivityLogsComponent extends AppComponentBase {
     @ViewChild(DxDataGridComponent) dataGrid: DxDataGridComponent;
-    dataSource: any;
-    private readonly dataSourceURI = 'PfmOfferRequest';
     readonly activityLogsFields: KeysEnum<ActivityLogsDto> = ActivityLogsFields;
+    hasPFMEnabled = this.permission.isGranted(AppPermissions.PFMApplications);
+    startCase = startCase;
+
+    private readonly dataSourceURI = 'PfmOfferRequest';
+    contactDataSource: ContactActivityLogInfo[];
+    pfmDataSource: any;
 
     constructor(
         injector: Injector,
-        private userService: UserServiceProxy
+        private userService: UserServiceProxy,
+        private contactsService: ContactsService,
+        private contactServiceProxy: ContactServiceProxy
     ) {
         super(injector);
-        this.dataSource = new DataSource({
+        this.pfmDataSource = new DataSource({
             store: new ODataStore({
                 url: this.getODataUrl(this.dataSourceURI),
                 version: AppConsts.ODataVersion,
@@ -46,10 +57,18 @@ export class ActivityLogsComponent extends AppComponentBase {
             }),
             filter: [ 'ApplicantUserId', '=', +this.userService['data'].userId ]
         });
+
+        this.contactsService.contactInfo$.pipe(first()).subscribe(contactInfo => {
+            this.contactServiceProxy.getActivityLogs(
+                contactInfo.id
+            ).subscribe((logs: ContactActivityLogInfo[]) => {
+                console.log(contactInfo.id, logs);
+            });
+        });
     }
 
-    onToolbarPreparing($event) {
-        $event.toolbarOptions.items.push({
+    onToolbarPreparing(event) {
+        event.toolbarOptions.items.push({
             location: 'before',
             template: 'title'
         });
