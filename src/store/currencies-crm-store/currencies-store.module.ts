b872f0d88f168fcecd51c12904470f05c24fe1a6
Dmytro Shkliarenko <dmytro.shkliarenko@sperse.com>
2024-01-08 20:08:08 +0200

    [SC-17347] Added CRM currency store

---
+++ src/store/currencies-crm-store/currencies-store.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { currenciesReducer } from './reducer';
+import { CurrenciesCrmStoreEffects } from './effects';
+import { EffectsModule } from '@node_modules/@ngrx/effects';
+import { CurrencyServiceProxy } from '@shared/service-proxies/service-proxies';
+
+@NgModule({
+    imports: [
+        StoreModule.forFeature('currencies-crm', currenciesReducer),
+        EffectsModule.forFeature([CurrenciesCrmStoreEffects ])
+    ],
+    providers: [CurrenciesCrmStoreEffects, CurrencyServiceProxy ]
+})
+export class CurrenciesCrmStoreModule {}
