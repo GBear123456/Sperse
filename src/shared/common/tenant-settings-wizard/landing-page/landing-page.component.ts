49518e43f0d584c970d7141b9938ad12f2218269
OlegZdolyny <oz@sperse.com>
2024-10-23 16:42:34 +0300

    [SC-17914] - Fixed WooCommerce value

--- src/shared/common/tenant-settings-wizard/landing-page/landing-page.component.ts
+++ src/shared/common/tenant-settings-wizard/landing-page/landing-page.component.ts
@@ -98,7 +98,7 @@ export class LandingPageComponent implements ITenantSettingsStepComponent {
             text: item
         };
     });
-    checkoutThemes = ['Default', 'Classic', 'WooСommerce'];
+    checkoutThemes = ['Default', 'Classic', 'WooCommerce'];
 
     checkoutFields = [
         'FirstName', 'LastName', 'Email', 'PhoneNumber', 'Shipping', 'Billing', 'DateOfBirth', 'Company',
