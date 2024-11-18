5ca18e98e0524fc254147491197b97f8204ee3ad
OlegZdolyny <oz@sperse.com>
2024-11-18 14:12:10 +0200

    [SC-17947] - disabled NextJS for Prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,7 +13,7 @@ export const environment = {
     host: 'sperseprodapiservice.azurewebsites.net',
     asuid: '98E5F7681BB2243E6A883B2D1AF5138AE73D2BB9927709C3D5F8B66E746BA48A',
     portalCdn: 'sperseportalproduicdn.azureedge.net',
-    showNextJSPortal: true,
+    showNextJSPortal: false,
     showAngularPortal: true,
 
     /* Custom tenant domains */
