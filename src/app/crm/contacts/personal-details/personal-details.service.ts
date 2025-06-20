68d3eaeb267b62591b1e7cc811007215124fee6b
Angel98518 <daniel98518@gmail.com>
2025-06-20 11:58:23 +0800

    feat: updated contact overview

--- src/app/crm/contacts/personal-details/personal-details.service.ts
+++ src/app/crm/contacts/personal-details/personal-details.service.ts
@@ -19,19 +19,19 @@ export class PersonalDetailsService {
 
      togglePersonalDetailsDialog(dialogId: string = 'personal-details-dialog', open = true): void {
          if (!this.layoutService.showModernLayout) {
-             let dialog = this.dialog.getDialogById(dialogId);
-             if (!dialog) {
-                 if (open)
-                     dialog = this.dialog.open(PersonalDetailsDialogComponent, {
-                         id: dialogId,
-                         panelClass: ['slider'],
-                         disableClose: true,
-                         hasBackdrop: false,
-                         closeOnNavigation: true,
-                         data: {}
-                     });
-             } else if (!open)
-                 dialog.close();
+            //  let dialog = this.dialog.getDialogById(dialogId);
+            //  if (!dialog) {
+            //      if (open)
+            //          dialog = this.dialog.open(PersonalDetailsDialogComponent, {
+            //              id: dialogId,
+            //              panelClass: ['slider'],
+            //              disableClose: true,
+            //              hasBackdrop: false,
+            //              closeOnNavigation: true,
+            //              data: {}
+            //          });
+            //  } else if (!open)
+            //      dialog.close();
          }
     }
 }
