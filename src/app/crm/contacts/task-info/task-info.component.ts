252a6b5d1e7b64284433f552e0e6c15a65eddeb9
tiger1016 <grant@sperse.com>
2025-07-01 13:32:25 -0400

    [SC-17536] Integrated apis for header section in overview tab.

--- src/app/crm/contacts/task-info/task-info.component.ts
+++ src/app/crm/contacts/task-info/task-info.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { takeUntil } from "rxjs/operators";
+
+import { ContactActivityLogInfo, ContactServiceProxy, DocumentServiceProxy } from "@shared/service-proxies/service-proxies";
+import { ActivityServiceProxy } from "@shared/service-proxies/service-proxies";
+import { ContactsService } from "../contacts.service";
+import { LifecycleSubjectsService } from "@shared/common/lifecycle-subjects/lifecycle-subjects.service";
 import { AppLocalizationService } from "@app/shared/common/localization/app-localization.service";
+import { LeadInfoDto } from "@shared/service-proxies/service-proxies";
+import { DocumentInfo } from "@shared/service-proxies/service-proxies";
 
 @Component({
     selector: "task-info",
@@ -7,7 +15,48 @@ import { AppLocalizationService } from "@app/shared/common/localization/app-loca
     styleUrls: ["./task-info.component.less"],
 })
 export class TaskInfoComponent implements OnInit {
-    constructor(public ls: AppLocalizationService) {}
+    leadId: number;
+    contactIds: number[];
+    logs: ContactActivityLogInfo[];
+    files: DocumentInfo[];
 
-    ngOnInit(): void {}
+    constructor(
+        private contactServiceProxy: ContactServiceProxy,
+        private lifeCycleService: LifecycleSubjectsService,
+        public ls: AppLocalizationService,
+        private documentProxy: DocumentServiceProxy,
+        public activityProxy: ActivityServiceProxy,
+        public contactsService: ContactsService
+    ) { }
+
+    ngOnInit(): void {
+        this.contactsService.leadInfo$
+            .pipe(takeUntil(this.lifeCycleService.destroy$))
+            .subscribe((lead: LeadInfoDto) => {
+                this.leadId = lead && lead.id;
+            });
+
+        this.contactServiceProxy
+            .getSourceContacts("", this.leadId, true, 100)
+            .pipe(takeUntil(this.lifeCycleService.destroy$))
+            .subscribe((contacts) => {
+                this.contactIds = contacts.map((contact) => contact.id);
+            });
+
+        this.contactsService.contactInfo$.subscribe(contactInfo => {
+            this.contactServiceProxy.getActivityLogs(
+                contactInfo.id
+            ).subscribe((logs: ContactActivityLogInfo[]) => {
+                this.logs = logs;
+            });
+
+            this.documentProxy.getAll(
+                contactInfo.id
+            ).subscribe((files: DocumentInfo[]) => {
+                this.files = files;
+            })
+
+        });
+
+    }
 }
