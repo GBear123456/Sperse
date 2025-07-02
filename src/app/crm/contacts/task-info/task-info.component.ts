b6d92f758a6ca72e6a09c46d7229c9be0b32c0fd
tiger1016 <grant@sperse.com>
2025-07-02 04:55:35 -0400

    [SC-17536] Update api integrations for the statistic section in overview.

--- src/app/crm/contacts/task-info/task-info.component.ts
+++ src/app/crm/contacts/task-info/task-info.component.ts
@@ -1,13 +1,70 @@
 import { Component, OnInit } from "@angular/core";
+import { takeUntil } from "rxjs/operators";
+
+import { ContactActivityLogInfo, ContactBalanceBaseDto, ContactServiceProxy, CreditBalanceServiceProxy, DocumentServiceProxy } from "@shared/service-proxies/service-proxies";
+import { ActivityServiceProxy } from "@shared/service-proxies/service-proxies";
+import { ContactsService } from "../contacts.service";
+import { LifecycleSubjectsService } from "@shared/common/lifecycle-subjects/lifecycle-subjects.service";
 import { AppLocalizationService } from "@app/shared/common/localization/app-localization.service";
+import { LeadInfoDto } from "@shared/service-proxies/service-proxies";
+import { DocumentInfo } from "@shared/service-proxies/service-proxies";
 
 @Component({
     selector: "task-info",
     templateUrl: "./task-info.component.html",
     styleUrls: ["./task-info.component.less"],
+    providers: [CreditBalanceServiceProxy]
 })
 export class TaskInfoComponent implements OnInit {
-    constructor(public ls: AppLocalizationService) {}
+    leadId: number;
+    balance: number;
+    contactIds: number[];
+    logs: ContactActivityLogInfo[];
+    files: DocumentInfo[];
 
-    ngOnInit(): void {}
+    constructor(
+        private contactServiceProxy: ContactServiceProxy,
+        private lifeCycleService: LifecycleSubjectsService,
+        private creditBalanceProxy: CreditBalanceServiceProxy,
+        public ls: AppLocalizationService,
+        private documentProxy: DocumentServiceProxy,
+        public activityProxy: ActivityServiceProxy,
+        public contactsService: ContactsService
+    ) { }
+
+    ngOnInit(): void {
+        this.contactsService.leadInfo$
+            .pipe(takeUntil(this.lifeCycleService.destroy$))
+            .subscribe((lead: LeadInfoDto) => {
+                this.leadId = lead && lead.id;
+            });
+
+        this.contactServiceProxy
+            .getSourceContacts("", this.leadId, true, 100)
+            .pipe(takeUntil(this.lifeCycleService.destroy$))
+            .subscribe((contacts) => {
+                this.contactIds = contacts.map((contact) => contact.id);
+            });
+
+        this.contactsService.contactInfo$.subscribe(contactInfo => {
+            this.contactServiceProxy.getActivityLogs(
+                contactInfo.id
+            ).subscribe((logs: ContactActivityLogInfo[]) => {
+                this.logs = logs;
+            });
+
+            this.documentProxy.getAll(
+                contactInfo.id
+            ).subscribe((files: DocumentInfo[]) => {
+                this.files = files;
+            })
+
+            this.creditBalanceProxy.getContactBalance(
+                contactInfo.id
+            ).subscribe((result: ContactBalanceBaseDto) => {
+                this.balance = result.balance;
+            })
+        });
+
+    }
 }
