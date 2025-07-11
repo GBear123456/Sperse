e89b3bd470e435cbf9876b998beae4a16dd6dd95
happysmile1012 <maru.lancers57@gmail.com>
2025-07-12 04:08:45 +0530

    "Document": Implemented header UI

---
+++ src/app/crm/contacts/documents/documents-dto.interface.ts
@@ -0,0 +1,12 @@
+export enum DocumentTabsFilter {
+    All = 'All',
+    Images = 'Images',
+    Videos = 'Videos',
+    PDFs = 'PDFs',
+    Invoices = 'Invoices'
+}
+
+export enum DocumentViewTypeFilter {
+    List = "List",
+    Grid = "Grid"
+}
--- src/app/shared/common/toolbar/toolbar.component.ts
+++ src/app/shared/common/toolbar/toolbar.component.ts
@@ -39,6 +39,7 @@ export class ToolBarComponent implements OnDestroy, OnInit {
     @Input() titleTemplate: TemplateRef<any>;
     @Input() isDisabled = false;
     @Input() width = "100%";
+    @Input() toolbarClass!: string;
     _config: ToolbarGroupModel[];
     @Input()
     set config(config: ToolbarGroupModel[]) {
@@ -389,6 +390,19 @@ export class ToolBarComponent implements OnDestroy, OnInit {
                 text: this.ls.l("Actions"),
                 hint: this.ls.l("Actions"),
             },
+            extension: {
+                hint: this.ls.l("ByExt"),
+                text: this.ls.l("ByExt"),
+                icon: this.getImgURI("arrow-down-icon"),
+            },
+            list: {
+                hint: this.ls.l("List"),
+                icon: this.getImgURI("bullet-list-69"),
+            },
+            grid: {
+                hint: this.ls.l("Grid"),
+                icon: this.getImgURI("blocks"),
+            }
         };
     }
 
