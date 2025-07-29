d2921516cf47ce6b6b317920b98553ac755d0959
alexey1001 <alexey.code1002@gmail.com>
2025-07-30 00:32:43 +0300

    added mail template functions: create, list

--- src/app/crm/shared/create-mail-template-modal/create-mail-template-data.interface.ts
+++ src/app/crm/shared/create-mail-template-modal/create-mail-template-data.interface.ts
@@ -1,6 +1,16 @@
+import {
+    FileInfo,
+    EmailTemplateType,
+} from "@shared/service-proxies/service-proxies";
+
 export interface CreateEmailTemplateData {
     title: string;
     previewText: string;
     body: string;
     subject: string;
+    type: EmailTemplateType;
+    id?: number | null;
+    cc: string[] | undefined;
+    bcc: string[] | undefined;
+    attachments: FileInfo[] | undefined;
 }
