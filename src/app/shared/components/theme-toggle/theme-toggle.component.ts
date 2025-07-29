2ca2c678ff75b44d8c49b6c259351ad6a5794d18
codevet210 <sharp.blader2050@gmail.com>
2025-07-29 03:15:05 -0300

    update crm dashboard

--- src/app/shared/components/theme-toggle/theme-toggle.component.ts
+++ src/app/shared/components/theme-toggle/theme-toggle.component.ts
@@ -1,71 +1,39 @@
-import { Component, OnInit } from "@angular/core";
-import { ThemeService } from "../../services/theme.service";
-import { Observable } from "rxjs";
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { Observable } from 'rxjs';
 
 @Component({
-    selector: "app-theme-toggle",
-    template: `
-        <button
-            class="theme-toggle"
-            (click)="toggleTheme()"
-            [title]="
-                (isDark$ | async)
-                    ? 'Switch to light theme'
-                    : 'Switch to dark theme'
-            "
-        >
-            <ng-container *ngIf="!(isDark$ | async)">
-                <lucide-icon name="sun"></lucide-icon>
-            </ng-container>
-            <ng-container *ngIf="isDark$ | async">
-                <lucide-icon name="moon"></lucide-icon>
-            </ng-container>
-        </button>
+  selector: 'app-theme-toggle',
+  templateUrl: './theme-toggle.component.html',
+  styles: [
+    `
+      .theme-toggle {
+        all: unset;
+        background: none;
+        border: none;
+        cursor: pointer;
+        transition: transform 0.3s ease;
+        display: block;
+        color: var(--text-color);
+        width: 50px;
+        height: 25px;
+      }
+      .theme-toggle:hover {
+        transform: scale(1.1);
+      }
     `,
-    styles: [
-        `
-            .theme-toggle {
-                background: none;
-                border: none;
-                padding: 8px;
-                cursor: pointer;
-                transition: transform 0.3s ease;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                color: var(--text-color);
-            }
-            .theme-toggle:hover {
-                transform: scale(1.1);
-            }
-            :host {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-            :host ::ng-deep lucide-icon {
-                width: 24px;
-                height: 24px;
-                display: block;
-            }
-            :host ::ng-deep lucide-icon svg {
-                width: 100%;
-                height: 100%;
-                color: currentColor;
-            }
-        `,
-    ],
+  ],
 })
 export class ThemeToggleComponent implements OnInit {
-    isDark$: Observable<boolean>;
+  isDark$: Observable<boolean>;
 
-    constructor(private themeService: ThemeService) {
-        this.isDark$ = this.themeService.isDarkTheme$;
-    }
+  constructor(private themeService: ThemeService) {
+    this.isDark$ = this.themeService.isDarkTheme$;
+  }
 
-    ngOnInit() {}
+  ngOnInit() {}
 
-    toggleTheme() {
-        this.themeService.toggleTheme();
-    }
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 }
