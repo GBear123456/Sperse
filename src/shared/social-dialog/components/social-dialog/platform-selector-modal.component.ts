0c14483c11fe45e0ff905e4119e12563fed9335c
tiger1016 <grant@sperse.com>
2025-08-20 15:50:17 -0400

    [SC-18329] complete Social link modal UI

--- src/shared/social-dialog/components/social-dialog/platform-selector-modal.component.ts
+++ src/shared/social-dialog/components/social-dialog/platform-selector-modal.component.ts
@@ -4,7 +4,7 @@ export interface Platform {
   id: string;
   name: string;
   icon: string;
-  color: string;
+  color?: string;
   urlPrefix?: string;
 }
 
@@ -24,102 +24,255 @@ export class PlatformSelectorModalComponent {
 
   platforms: Platform[] = [
     {
-      id: 'facebook',
-      name: 'Facebook',
-      icon: 'assets/images/platforms/facebook.svg',
-      color: '#1877f2'
+      id: 'apple',
+      name: 'Apple',
+      icon: 'assets/images/platforms/apple.svg'
     },
     {
-      id: 'twitter',
-      name: 'Twitter',
-      icon: 'assets/images/platforms/twitter.svg',
-      color: '#1da1f2'
-    },
-    {
-      id: 'linkedin',
-      name: 'LinkedIn',
-      icon: 'assets/images/platforms/linkedin.svg',
-      color: '#0077b5'
-    },
-    {
-      id: 'instagram',
-      name: 'Instagram',
-      icon: 'assets/images/platforms/instagram.svg',
-      color: '#e4405f'
-    },
-    {
-      id: 'youtube',
-      name: 'YouTube',
-      icon: 'assets/images/platforms/youtube.svg',
-      color: '#ff0000'
-    },
-    {
-      id: 'tiktok',
-      name: 'TikTok',
-      icon: 'assets/images/platforms/tiktok.svg',
-      color: '#000000'
-    },
-    {
-      id: 'whatsapp',
-      name: 'WhatsApp',
-      icon: 'assets/images/platforms/whatsapp.svg',
-      color: '#25d366'
-    },
-    {
-      id: 'telegram',
-      name: 'Telegram',
-      icon: 'assets/images/platforms/telegram.svg',
-      color: '#0088cc'
-    },
-    {
-      id: 'discord',
-      name: 'Discord',
-      icon: 'assets/images/platforms/discord.svg',
-      color: '#5865f2'
-    },
-    {
-      id: 'website',
-      name: 'Website',
-      icon: 'assets/images/platforms/website.svg',
-      color: '#6366f1'
-    },
-    {
-      id: 'calendly',
-      name: 'Calendly',
-      icon: 'assets/images/platforms/calendly.svg',
-      color: '#006bff'
-    },
-    {
-      id: 'cal.com',
-      name: 'Cal.com',
-      icon: 'assets/images/platforms/cal.svg',
-      color: '#000000'
-    },
-    {
-      id: 'crunchbase',
-      name: 'Crunchbase',
-      icon: 'assets/images/platforms/crunchbase.svg',
-      color: '#8B5CF6'
-    },
-    {
-      id: 'angellist',
+      id: 'angel',
       name: 'AngelList',
-      icon: 'assets/images/platforms/angellist.svg',
-      color: '#8B5CF6'
+      icon: 'assets/images/platforms/angel.svg'
     },
     {
       id: 'bbb.org',
       name: 'BBB.org',
-      icon: 'assets/images/platforms/bbb.svg',
-      color: '#007BFF'
+      icon: 'assets/images/platforms/bbb.svg'
     },
     {
-      id: 'bluesky',
-      name: 'BlueSky',
-      icon: 'assets/images/platforms/bluesky.svg',
-      color: '#007BFF'
+      id: 'cb',
+      name: 'Crunchbase',
+      icon: 'assets/images/platforms/cb.svg'
+    },
+    {
+      id: 'cal.com',
+      name: 'Cal.com',
+      icon: 'assets/images/platforms/cal.svg'
+    },
+    {
+      id: 'calendly',
+      name: 'Calendly',
+      icon: 'assets/images/platforms/calendly.svg'
+    },
+    {
+      id: 'discord',
+      name: 'Discord',
+      icon: 'assets/images/platforms/discord.svg'
+    },
+    {
+      id: 'dribbble',
+      name: 'Dribbble',
+      icon: 'assets/images/platforms/dribbble.svg'
+    },
+    {
+      id: 'facebook',
+      name: 'Facebook',
+      icon: 'assets/images/platforms/facebook.svg'
+    },
+    {
+      id: 'github',
+      name: 'GitHub',
+      icon: 'assets/images/platforms/github.svg'
+    },
+    {
+      id: 'glassdoor',
+      name: 'Glassdoor',
+      icon: 'assets/images/platforms/glassdoor.svg'
+    },
+    {
+      id: 'google',
+      name: 'Google',
+      icon: 'assets/images/platforms/google.svg'
+    },
+    {
+      id: 'instagram',
+      name: 'Instagram',
+      icon: 'assets/images/platforms/instagram.svg'
+    },
+    {
+      id: 'linkedin',
+      name: 'LinkedIn',
+      icon: 'assets/images/platforms/linkedin.svg'
+    },
+    {
+      id: 'messenger',
+      name: 'Messenger',
+      icon: 'assets/images/platforms/messenger.svg'
+    },
+    {
+      id: 'opencorporates',
+      name: "Opencorporates",
+      icon: 'assets/images/platforms/opencorporates.svg'
+    },
+    {
+      id: 'opensea',
+      name: 'OpenSea',
+      icon: 'assets/images/platforms/opensea.svg'
+    },
+    {
+      id: 'pinterest',
+      name: 'Pinterest',
+      icon: 'assets/images/platforms/pinterest.svg'
+    },
+    // {
+    //   id: 'podcast',
+    //   name: 'Podcast',
+    //   icon: 'assets/images/platforms/podcast.svg'
+    // },
+    {
+      id: 'reddit',
+      name: 'Reddit',
+      icon: 'assets/images/platforms/reddit.svg'
+    },
+    {
+      id: 'rss',
+      name: 'RSS',
+      icon: 'assets/images/platforms/rss.svg'
+    },
+    {
+      id: 'skype',
+      name: 'Skype',
+      icon: 'assets/images/platforms/skype.svg'
+    },
+    {
+      id: 'slack',
+      name: 'Slack',
+      icon: 'assets/images/platforms/slack.svg'
+    },
+    {
+      id: 'snapchat',
+      name: 'Snapchat',
+      icon: 'assets/images/platforms/snapchat.svg'
+    },
+    {
+      id: 'soundcloud',
+      name: 'SoundCloud',
+      icon: 'assets/images/platforms/cloud.svg'
+    },
+    {
+      id: 'substack',
+      name: 'Substack',
+      icon: 'assets/images/platforms/substack.svg'
+    },
+    {
+      id: 'teams',
+      name: 'Microsoft Teams',
+      icon: 'assets/images/platforms/teams.svg'
+    },
+    {
+      id: 'telegram',
+      name: 'Telegram',
+      icon: 'assets/images/platforms/telegram.svg'
+    },
+    {
+      id: 'threads',
+      name: 'Threads',
+      icon: 'assets/images/platforms/threads.svg'
+    },
+    {
+      id: 'tiktok',
+      name: 'TikTok',
+      icon: 'assets/images/platforms/tiktok.svg'
+    },
+    {
+      id: 'trustpilot',
+      name: 'Trustpilot',
+      icon: 'assets/images/platforms/trustpilot.svg'
+    },
+    {
+      id: 'tweach',
+      name: 'Tweach',
+      icon: 'assets/images/platforms/tweach.svg'
+    },
+    {
+      id: 'viber',
+      name: 'Viber',
+      icon: 'assets/images/platforms/viber.svg'
+    },
+    {
+      id: 'vimeo',
+      name: 'Vimeo',
+      icon: 'assets/images/platforms/vimeo.svg'
+    },
+    // {
+    //   id: 'website',
+    //   name: 'Website',
+    //   icon: 'assets/images/platforms/website.svg'
+    // },
+    {
+      id: 'wechat',
+      name: 'WeChat',
+      icon: 'assets/images/platforms/wechat.svg'
+    },
+    {
+      id: 'whatsapp',
+      name: 'WhatsApp',
+      icon: 'assets/images/platforms/whatsapp.svg'
+    },
+    {
+      id: 'x.com',
+      name: 'X.com',
+      icon: 'assets/images/platforms/x.svg'
+    },
+    {
+      id: 'yelp',
+      name: 'Yelp',
+      icon: 'assets/images/platforms/yelp.svg'
+    },
+    {
+      id: 'youtube',
+      name: 'YouTube',
+      icon: 'assets/images/platforms/Youtube.svg'
+    },
+    {
+      id: 'zoom',
+      name: 'Zoom',
+      icon: 'assets/images/platforms/zoom.svg'
     }
   ];
+
+  getSocialAppBackground(platform: string): string {
+    switch (platform.toLowerCase()) {
+      case "facebook": return "social-bg-facebook";
+      case "instagram": return "social-bg-instagram";
+      case "x.com": return "social-bg-x";
+      case "linkedin": return "social-bg-linkedin";
+      case "youtube": return "social-bg-youtube";
+      case "tiktok": return "social-bg-tiktok";
+      case "discord": return "social-bg-discord";
+      case "github": return "social-bg-github";
+      case "whatsapp": return "social-bg-whatsapp";
+      case "telegram": return "social-bg-telegram";
+      case "pinterest": return "social-bg-pinterest";
+      case "reddit": return "social-bg-reddit";
+      case "snapchat": return "social-bg-snapchat";
+      case "tweach": return "social-bg-tweach";
+      case "dribbble": return "social-bg-dribbble";
+      case "soundcloud": return "social-bg-soundcloud";
+      case "skype": return "social-bg-skype";
+      case "zoom": return "social-bg-zoom";
+      case "vimeo": return "social-bg-vimeo";
+      case "threads": return "social-bg-threads";
+      case "cal.com": return "social-bg-cal";
+      case "calendly": return "social-bg-calendly";
+      case "podcast": return "social-bg-podcast";
+      case "rss": return "social-bg-rss";
+      case "slack": return "social-bg-slack";
+      case "substack": return "social-bg-substack";
+      case "yelp": return "social-bg-yelp";
+      case "website": return "social-bg-website";
+      case "apple": return "social-bg-apple";
+      case "viber": return "social-bg-viber";
+      case "messenger": return "social-bg-messenger";
+      case "wechat": return "social-bg-wechat";
+      case "google": return "social-bg-google";
+      case "onlinecourse": return "social-bg-onlinecourse";
+      case "teams": return "social-bg-teams";
+      case "opensea": return "social-bg-opensea";
+      case "cb": return "social-bg-cb";
+      case "angel": return "social-bg-angel";
+      default: return "social-bg-default";
+    }
+  }
 
   get filteredPlatforms(): Platform[] {
     if (!this.searchTerm) {
@@ -132,25 +285,21 @@ export class PlatformSelectorModalComponent {
 
   getPlatformUrlPrefix(platform: string): string {
     switch (platform.toLowerCase()) {
-      case "angellist": return "https://angel.co/u/";
+      case "angel": return "https://angel.co/u/";
       case "apple": return "https://www.apple.com/";
-      case "bbb.org": return "https://www.bbb.org/";
       case "cal.com": return "https://cal.com/";
       case "calendly": return "https://calendly.com/";
-      case "crunchbase": return "https://www.crunchbase.com/person/";
+      case "cb": return "https://www.crunchbase.com/person/";
       case "discord": return "https://discord.gg/";
       case "dribbble": return "https://dribbble.com/";
       case "facebook": return "https://www.facebook.com/";
       case "github": return "https://github.com/";
       case "glassdoor": return "https://www.glassdoor.com/Overview/";
-      case "google reviews": return "https://www.google.com/maps/place/";
+      case "google": return "https://www.google.com/";
       case "instagram": return "https://www.instagram.com/";
       case "linkedin": return "https://www.linkedin.com/in/";
       case "messenger": return "https://m.me/";
-      case "nav": return "https://www.nav.com/company/";
-      case "opencorporates": return "https://opencorporates.com/companies/";
       case "opensea": return "https://opensea.io/";
-      case "paypal.me": return "https://paypal.me/";
       case "pinterest": return "https://www.pinterest.com/";
       case "podcast": return "https://";
       case "rss": return "https://";
@@ -163,10 +312,8 @@ export class PlatformSelectorModalComponent {
       case "telegram": return "https://t.me/";
       case "threads": return "https://www.threads.net/@";
       case "tiktok": return "https://www.tiktok.com/@";
-      case "trustpilot": return "https://www.trustpilot.com/review/";
-      case "twitch": return "https://twitch.tv/";
-      case "up": return "https://upapp.us/";
-      case "upscroll": return "https://upscroll.com/";
+      case "trustpilot": return "https://www.trustpilot.com/";
+      case "tweach": return "https://tweach.com/";
       case "viber": return "https://chats.viber.com/";
       case "vimeo": return "https://vimeo.com/";
       case "wechat": return "https://www.wechat.com/";
@@ -176,8 +323,8 @@ export class PlatformSelectorModalComponent {
       case "yelp": return "https://www.yelp.com/biz/";
       case "youtube": return "https://www.youtube.com/@";
       case "zoom": return "https://zoom.us/my/";
-      case "your website": return "https://";
-      case "other link": return "https://";
+      case "website": return "https://";
+      case "opencorporates": return 'https://opencorporates.com/';
       default: return "https://";
     }
   }
@@ -202,10 +349,7 @@ export class PlatformSelectorModalComponent {
     this.modalClosed.emit();
   }
 
-  toggleTheme(): void {
-    // Theme toggle functionality
-    console.log('Theme toggled');
-  }
+ 
 
   toggleViewMode(): void {
     this.isGridView = !this.isGridView;
