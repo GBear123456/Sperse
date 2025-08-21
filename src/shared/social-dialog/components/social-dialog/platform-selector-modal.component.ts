af3326c2a3d678f95b4fc2f37003152875704b8f
tiger1016 <grant@sperse.com>
2025-08-20 23:36:23 -0400

    [SC-18329] added functionality

--- src/shared/social-dialog/components/social-dialog/platform-selector-modal.component.ts
+++ src/shared/social-dialog/components/social-dialog/platform-selector-modal.component.ts
@@ -1,12 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Platform } from './platform.interface';
+import { platforms } from './platforms.data';
 
-export interface Platform {
-  id: string;
-  name: string;
-  icon: string;
-  color?: string;
-  urlPrefix?: string;
-}
 
 @Component({
   selector: 'app-platform-selector-modal',
@@ -21,214 +16,9 @@ export class PlatformSelectorModalComponent {
   searchTerm: string = '';
   selectedPlatformId: string = '';
   isGridView: boolean = false;
+  platforms: Platform[] = platforms;
 
-  platforms: Platform[] = [
-    {
-      id: 'apple',
-      name: 'Apple',
-      icon: 'assets/images/platforms/apple.svg'
-    },
-    {
-      id: 'angel',
-      name: 'AngelList',
-      icon: 'assets/images/platforms/angel.svg'
-    },
-    {
-      id: 'bbb.org',
-      name: 'BBB.org',
-      icon: 'assets/images/platforms/bbb.svg'
-    },
-    {
-      id: 'cb',
-      name: 'Crunchbase',
-      icon: 'assets/images/platforms/cb.svg'
-    },
-    {
-      id: 'cal.com',
-      name: 'Cal.com',
-      icon: 'assets/images/platforms/cal.svg'
-    },
-    {
-      id: 'calendly',
-      name: 'Calendly',
-      icon: 'assets/images/platforms/calendly.svg'
-    },
-    {
-      id: 'discord',
-      name: 'Discord',
-      icon: 'assets/images/platforms/discord.svg'
-    },
-    {
-      id: 'dribbble',
-      name: 'Dribbble',
-      icon: 'assets/images/platforms/dribbble.svg'
-    },
-    {
-      id: 'facebook',
-      name: 'Facebook',
-      icon: 'assets/images/platforms/facebook.svg'
-    },
-    {
-      id: 'github',
-      name: 'GitHub',
-      icon: 'assets/images/platforms/github.svg'
-    },
-    {
-      id: 'glassdoor',
-      name: 'Glassdoor',
-      icon: 'assets/images/platforms/glassdoor.svg'
-    },
-    {
-      id: 'google',
-      name: 'Google',
-      icon: 'assets/images/platforms/google.svg'
-    },
-    {
-      id: 'instagram',
-      name: 'Instagram',
-      icon: 'assets/images/platforms/instagram.svg'
-    },
-    {
-      id: 'linkedin',
-      name: 'LinkedIn',
-      icon: 'assets/images/platforms/linkedin.svg'
-    },
-    {
-      id: 'messenger',
-      name: 'Messenger',
-      icon: 'assets/images/platforms/messenger.svg'
-    },
-    {
-      id: 'opencorporates',
-      name: "Opencorporates",
-      icon: 'assets/images/platforms/opencorporates.svg'
-    },
-    {
-      id: 'opensea',
-      name: 'OpenSea',
-      icon: 'assets/images/platforms/opensea.svg'
-    },
-    {
-      id: 'pinterest',
-      name: 'Pinterest',
-      icon: 'assets/images/platforms/pinterest.svg'
-    },
-    // {
-    //   id: 'podcast',
-    //   name: 'Podcast',
-    //   icon: 'assets/images/platforms/podcast.svg'
-    // },
-    {
-      id: 'reddit',
-      name: 'Reddit',
-      icon: 'assets/images/platforms/reddit.svg'
-    },
-    {
-      id: 'rss',
-      name: 'RSS',
-      icon: 'assets/images/platforms/rss.svg'
-    },
-    {
-      id: 'skype',
-      name: 'Skype',
-      icon: 'assets/images/platforms/skype.svg'
-    },
-    {
-      id: 'slack',
-      name: 'Slack',
-      icon: 'assets/images/platforms/slack.svg'
-    },
-    {
-      id: 'snapchat',
-      name: 'Snapchat',
-      icon: 'assets/images/platforms/snapchat.svg'
-    },
-    {
-      id: 'soundcloud',
-      name: 'SoundCloud',
-      icon: 'assets/images/platforms/cloud.svg'
-    },
-    {
-      id: 'substack',
-      name: 'Substack',
-      icon: 'assets/images/platforms/substack.svg'
-    },
-    {
-      id: 'teams',
-      name: 'Microsoft Teams',
-      icon: 'assets/images/platforms/teams.svg'
-    },
-    {
-      id: 'telegram',
-      name: 'Telegram',
-      icon: 'assets/images/platforms/telegram.svg'
-    },
-    {
-      id: 'threads',
-      name: 'Threads',
-      icon: 'assets/images/platforms/threads.svg'
-    },
-    {
-      id: 'tiktok',
-      name: 'TikTok',
-      icon: 'assets/images/platforms/tiktok.svg'
-    },
-    {
-      id: 'trustpilot',
-      name: 'Trustpilot',
-      icon: 'assets/images/platforms/trustpilot.svg'
-    },
-    {
-      id: 'tweach',
-      name: 'Tweach',
-      icon: 'assets/images/platforms/tweach.svg'
-    },
-    {
-      id: 'viber',
-      name: 'Viber',
-      icon: 'assets/images/platforms/viber.svg'
-    },
-    {
-      id: 'vimeo',
-      name: 'Vimeo',
-      icon: 'assets/images/platforms/vimeo.svg'
-    },
-    // {
-    //   id: 'website',
-    //   name: 'Website',
-    //   icon: 'assets/images/platforms/website.svg'
-    // },
-    {
-      id: 'wechat',
-      name: 'WeChat',
-      icon: 'assets/images/platforms/wechat.svg'
-    },
-    {
-      id: 'whatsapp',
-      name: 'WhatsApp',
-      icon: 'assets/images/platforms/whatsapp.svg'
-    },
-    {
-      id: 'x.com',
-      name: 'X.com',
-      icon: 'assets/images/platforms/x.svg'
-    },
-    {
-      id: 'yelp',
-      name: 'Yelp',
-      icon: 'assets/images/platforms/yelp.svg'
-    },
-    {
-      id: 'youtube',
-      name: 'YouTube',
-      icon: 'assets/images/platforms/Youtube.svg'
-    },
-    {
-      id: 'zoom',
-      name: 'Zoom',
-      icon: 'assets/images/platforms/zoom.svg'
-    }
-  ];
+  
 
   getSocialAppBackground(platform: string): string {
     switch (platform.toLowerCase()) {
@@ -334,14 +124,43 @@ export class PlatformSelectorModalComponent {
     // Get the URL prefix for the selected platform
     const urlPrefix = this.getPlatformUrlPrefix(platform.name);
     
-    // Create a platform object with the URL prefix
+    // Create a platform object with the URL prefix and color
     const platformWithUrl = {
       ...platform,
-      urlPrefix: urlPrefix
+      urlPrefix: urlPrefix,
+      color: this.getPlatformColor(platform.name)
     };
     
     this.platformSelected.emit(platformWithUrl);
     this.closeModal();
+  }
+
+  // Get platform color for consistent styling
+  private getPlatformColor(platformName: string): string {
+    const platformColors: { [key: string]: string } = {
+      'Facebook': '#1877f2',
+      'Twitter': '#1da1f2',
+      'X.com': '#000000',
+      'LinkedIn': '#0077b5',
+      'Instagram': '#e4405f',
+      'YouTube': '#ff0000',
+      'TikTok': '#000000',
+      'WhatsApp': '#25d366',
+      'Telegram': '#0088cc',
+      'Discord': '#5865f2',
+      'GitHub': '#333333',
+      'Reddit': '#ff4500',
+      'Snapchat': '#fffc00',
+      'Pinterest': '#bd081c',
+      'Skype': '#00aff0',
+      'Zoom': '#2d8cff',
+      'Vimeo': '#1ab7ea',
+      'SoundCloud': '#ff7700',
+      'Twitch': '#9146ff',
+      'Steam': '#171a21'
+    };
+    
+    return platformColors[platformName] || '#6366f1';
   }
 
   closeModal(): void {
