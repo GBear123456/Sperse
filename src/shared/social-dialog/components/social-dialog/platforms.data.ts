d76fdd45731711a008f94063a48a99d339e360e3
alexey1002 <alexey.code1002@gmail.com>
2025-09-19 17:30:19 +0300

    [SC-18329] added functionality

---
+++ src/shared/social-dialog/components/social-dialog/platforms.data.ts
@@ -0,0 +1,199 @@
+import { Platform } from './platform.interface';
+
+export const platforms: Platform[] = [
+  {
+    id: 'apple',
+    name: 'Apple',
+    icon: 'assets/images/platforms/apple.svg'
+  },
+  {
+    id: 'angel',
+    name: 'AngelList',
+    icon: 'assets/images/platforms/angel.svg'
+  },
+  {
+    id: 'bbb.org',
+    name: 'BBB.org',
+    icon: 'assets/images/platforms/bbb.svg'
+  },
+  {
+    id: 'cb',
+    name: 'Crunchbase',
+    icon: 'assets/images/platforms/cb.svg'
+  },
+  {
+    id: 'cal.com',
+    name: 'Cal.com',
+    icon: 'assets/images/platforms/cal.svg'
+  },
+  {
+    id: 'calendly',
+    name: 'Calendly',
+    icon: 'assets/images/platforms/calendly.svg'
+  },
+  {
+    id: 'discord',
+    name: 'Discord',
+    icon: 'assets/images/platforms/discord.svg'
+  },
+  {
+    id: 'dribbble',
+    name: 'Dribbble',
+    icon: 'assets/images/platforms/dribbble.svg'
+  },
+  {
+    id: 'facebook',
+    name: 'Facebook',
+    icon: 'assets/images/platforms/facebook.svg'
+  },
+  {
+    id: 'github',
+    name: 'GitHub',
+    icon: 'assets/images/platforms/github.svg'
+  },
+  {
+    id: 'glassdoor',
+    name: 'Glassdoor',
+    icon: 'assets/images/platforms/glassdoor.svg'
+  },
+  {
+    id: 'google',
+    name: 'Google',
+    icon: 'assets/images/platforms/google.svg'
+  },
+  {
+    id: 'instagram',
+    name: 'Instagram',
+    icon: 'assets/images/platforms/instagram.svg'
+  },
+  {
+    id: 'linkedin',
+    name: 'LinkedIn',
+    icon: 'assets/images/platforms/linkedin.svg'
+  },
+  {
+    id: 'messenger',
+    name: 'Messenger',
+    icon: 'assets/images/platforms/messenger.svg'
+  },
+  {
+    id: 'opencorporates',
+    name: "Opencorporates",
+    icon: 'assets/images/platforms/opencorporates.svg'
+  },
+  {
+    id: 'opensea',
+    name: 'OpenSea',
+    icon: 'assets/images/platforms/opensea.svg'
+  },
+  {
+    id: 'pinterest',
+    name: 'Pinterest',
+    icon: 'assets/images/platforms/pinterest.svg'
+  },
+  {
+    id: 'reddit',
+    name: 'Reddit',
+    icon: 'assets/images/platforms/reddit.svg'
+  },
+  {
+    id: 'rss',
+    name: 'RSS',
+    icon: 'assets/images/platforms/rss.svg'
+  },
+  {
+    id: 'skype',
+    name: 'Skype',
+    icon: 'assets/images/platforms/skype.svg'
+  },
+  {
+    id: 'slack',
+    name: 'Slack',
+    icon: 'assets/images/platforms/slack.svg'
+  },
+  {
+    id: 'snapchat',
+    name: 'Snapchat',
+    icon: 'assets/images/platforms/snapchat.svg'
+  },
+  {
+    id: 'soundcloud',
+    name: 'SoundCloud',
+    icon: 'assets/images/platforms/cloud.svg'
+  },
+  {
+    id: 'substack',
+    name: 'Substack',
+    icon: 'assets/images/platforms/substack.svg'
+  },
+  {
+    id: 'teams',
+    name: 'Microsoft Teams',
+    icon: 'assets/images/platforms/teams.svg'
+  },
+  {
+    id: 'telegram',
+    name: 'Telegram',
+    icon: 'assets/images/platforms/telegram.svg'
+  },
+  {
+    id: 'threads',
+    name: 'Threads',
+    icon: 'assets/images/platforms/threads.svg'
+  },
+  {
+    id: 'tiktok',
+    name: 'TikTok',
+    icon: 'assets/images/platforms/tiktok.svg'
+  },
+  {
+    id: 'trustpilot',
+    name: 'Trustpilot',
+    icon: 'assets/images/platforms/trustpilot.svg'
+  },
+  {
+    id: 'tweach',
+    name: 'Tweach',
+    icon: 'assets/images/platforms/tweach.svg'
+  },
+  {
+    id: 'viber',
+    name: 'Viber',
+    icon: 'assets/images/platforms/viber.svg'
+  },
+  {
+    id: 'vimeo',
+    name: 'Vimeo',
+    icon: 'assets/images/platforms/vimeo.svg'
+  },
+  {
+    id: 'wechat',
+    name: 'WeChat',
+    icon: 'assets/images/platforms/wechat.svg'
+  },
+  {
+    id: 'whatsapp',
+    name: 'WhatsApp',
+    icon: 'assets/images/platforms/whatsapp.svg'
+  },
+  {
+    id: 'x.com',
+    name: 'X.com',
+    icon: 'assets/images/platforms/x.svg'
+  },
+  {
+    id: 'yelp',
+    name: 'Yelp',
+    icon: 'assets/images/platforms/yelp.svg'
+  },
+  {
+    id: 'youtube',
+    name: 'YouTube',
+    icon: 'assets/images/platforms/Youtube.svg'
+  },
+  {
+    id: 'zoom',
+    name: 'Zoom',
+    icon: 'assets/images/platforms/zoom.svg'
+  }
+];
